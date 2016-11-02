e55198d79fedc36f3bffd23786760f096e4f57c7
Gabriel Volonte <tampas@gmail.com>
2016-11-02 18:10:33 -0300

    borro testing

--- testing/javaScript01.js
+++
@@ -1,9 +0,0 @@
-function cuadrado(cuadradoValue){
-    return cuadradoValue * cuadradoValue;
-}
-
-function hipotenusa(a, b){
-    return Math.sqrt(cuadrado(a) + cuadrado(b));
-}
-
-resultado.innerHTML=hipotenusa(3,4);
