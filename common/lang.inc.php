37026ebffd99d2ccf09c73df5c4ae381cf8e8eae
Gabriel Volonte <tampas@gmail.com>
2016-11-02 02:31:22 -0300

    agrego gettext a lang.inc

---
+++ common/lang.inc.php
@@ -0,0 +1,16 @@
+<ul class="lang pRel horizontalBT unstyledAT">
+    <li><a href="#"><img src="/clientes/frare/img/fl_es.svg" alt="<?php echo _("Navegar el sitio en idioma español"); ?>"></a></li>
+    <li><a href="#"><img src="/clientes/frare/img/fl_uk.svg" alt="<?php echo _("Switch to English version"); ?>"></a></li>
+</ul>
+
+<?php
+# Gettext
+/*
+
+msgid "Navegar el sitio en idioma espa&ntilde;ol"
+msgstr "Navegar el sitio en idioma espa&ntilde;ol"
+
+msgid "Switch to English version"
+msgstr "Switch to English version"ç+
+
+*/
