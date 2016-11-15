fabff33c915ec2cd4a6f11197da0b92c2dfa6953
Gabriel Volonte <tampas@gmail.com>
2016-11-15 00:00:27 -0300

    agrego directorio /archivos/ en oldframework

--- oldframework/descargas/logos.php
+++ oldframework/descargas/logos.php
@@ -1,7 +1,7 @@
 <div class="clearfix ptl bg bts mtl">
     <h2>Logos</h2>
     <ul class="bull">
-        <li class="clearfix mbs">Logo Original (para impresión)<a href="/nuevo/archivos/logo_escondida_olivos.zip" class="dd displayBlock floatRight bgBlue pvs phm bold white upperCase alignCenter">Descargar</a></li>
-        <li class="clearfix mbs">Logo (formato JPG/uso digital)<a href="/nuevo/archivos/logo_escondida_olivos_jpg.zip" class="dd displayBlock floatRight bgBlue pvs phm bold white upperCase alignCenter">Descargar</a></li>
+        <li class="clearfix mbs">Logo Original (para impresión)<a href="/nuevo/oldframework/archivos/logo.zip" class="dd displayBlock floatRight bgBlue pvs phm bold white upperCase alignCenter">Descargar</a></li>
+        <li class="clearfix mbs">Logo (formato JPG/uso digital)<a href="/nuevo/oldframework/archivos/logo_jpg.zip" class="dd displayBlock floatRight bgBlue pvs phm bold white upperCase alignCenter">Descargar</a></li>
     </ul>
 </div>
