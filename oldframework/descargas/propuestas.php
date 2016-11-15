adb79da784878bcc96f83b6ae9189b54ea77e4dc
Gabriel Volonte <tampas@gmail.com>
2016-11-15 00:01:46 -0300

    ajusto /oldframework/descargas/

--- oldframework/descargas/propuestas.php
+++ oldframework/descargas/propuestas.php
@@ -2,6 +2,6 @@
     <h2>Revista Propuestas &amp; Servicios</h2>
     <p>Pautas editoriales 2010</p>
     <ul class="bull">
-        <li class="clearfix mbs">Aviso (1 página) <a href="/nuevo/archivos/aviso-oli_pys-p_simple.zip" class="dd displayBlock floatRight bgBlue pvs phm bold white upperCase alignCenter">Descargar</a></li>
+        <li class="clearfix mbs">Aviso (1 página) <a href="/nuevo/oldframework/archivos/logo.zip" class="dd displayBlock floatRight bgBlue pvs phm bold white upperCase alignCenter">Descargar</a></li>
     </ul>
 </div>
--- oldframework/js/agenda.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $( ".listadoTxt .a" ).load( "/nuevo/agenda/01.php" );
-    $( ".listadoTxt .b" ).load( "/nuevo/agenda/02.php" );
-    $( ".listadoTxt .c" ).load( "/nuevo/agenda/03.php" );
-});
