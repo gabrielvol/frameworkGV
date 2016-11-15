ff1e5614eef49ecdc32aaaa49aceb076ef51c82c
Gabriel Volonte <tampas@gmail.com>
2016-11-14 22:26:22 -0300

    ajusto gulp.js

--- gulp.js
+++ gulp.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
+var pleeease = require('gulp-pleeease');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('css', function () {
+    gulp.src('./src/*.css')
+    .pipe(pleeease({
+        out: 'out.min.css'
+    }))
+    .pipe(gulp.dest('./dest'));
 });
