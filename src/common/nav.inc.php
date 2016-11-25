bed67faeec54625126f28156fde1f9335f233d26
Gabriel Volonte <tampas@gmail.com>
2016-11-25 15:28:32 -0300

    ajustes en menú responsive

--- src/common/nav.inc.php
+++ src/common/nav.inc.php
@@ -1,5 +1,5 @@
 <nav id="nav" class="mainNav container">
-    <a href="javascript:void(0)" class="hamb displayNone displayBlockBT alignCenter"><?php echo _("Menu"); ?></a>
+    <a href="javascript:void(0)" class="hamb displayNone displayBlockBT alignCenter upperCase"><?php echo _("Men&uacute;"); ?></a>
     <ul class="mainMenu displayNoneBT unstyled horizontalAT clearfix">
         <li class="<?php if($navSelected == 1) echo " selected" ?>"><a href="/nuevo/" class="displayBlock"><?php echo _("Inicio"); ?></a></li>
     </ul>
