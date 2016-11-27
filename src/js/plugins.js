0846400366b868f0e5e563932cd00b4ca78fcecf
Gabriel Volonte <tampas@gmail.com>
2016-11-27 17:53:29 -0300

    ajuste en hamburguer toggle

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     //Hamburger Menu
     $(".hamb").click(function () {
-        $(this).closest("#nav").find(".mainMenu").removeClass('displayNoneBT');
+        $(this).closest("#nav").find(".mainMenu").toggleClass('displayNoneBT');
     });
     $('.mainMenu a').click(function () {
         $(this).closest(".mainMenu").addClass('displayNoneBT');
