a4dab2e100d7eacc56695b389c93cfa45e1ed4db
Gabriel Volonte <tampas@gmail.com>
2016-12-10 19:08:53 -0300

    ajustes en nav

--- src/common/nav.inc.php
+++ src/common/nav.inc.php
@@ -1,4 +1,4 @@
-<nav id="nav" class="mainNav container">
+<nav id="nav" class="mainNav container pRel">
     <a href="javascript:void(0)" class="hamb displayNone displayBlockBT alignCenter upperCase"><?php echo _("Men&uacute;"); ?></a>
     <ul class="mainMenu displayNoneBT unstyled horizontalAT clearfix">
         <li class="<?php if($navSelected == 1) echo " selected" ?>"><a href="/nuevo/" class="displayBlock"><?php echo _("Inicio"); ?></a></li>
