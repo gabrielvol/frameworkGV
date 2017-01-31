2294196b4b42a32d894ac67a518a9edfcc253b48
Gabriel Volonte <tampas@gmail.com>
2017-01-31 19:25:32 -0300

    agrego color para menu hamburguer clicked

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     //Hamburger Menu
     $(".hamb").click(function () {
+        $(this).toggleClass('clicked');
         $(this).closest("#nav").find(".mainMenu").toggleClass('displayNoneBT');
     });
     $('.mainMenu a').click(function () {
