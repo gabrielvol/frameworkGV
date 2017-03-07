f3bef42ddb43f77e916a70ce688aa2587df8423a
Gabriel Volonte <tampas@gmail.com>
2017-03-07 20:21:14 -0300

    agrego bandera de brasil a lang.inc y gettext correspondiente

--- src/common/lang.inc.php
+++ src/common/lang.inc.php
@@ -1,16 +1,20 @@
-<ul class="lang pRel horizontalBT unstyledAT">
-    <li><a href="#"><img src="/clientes/frare/img/fl_es.svg" alt="<?php echo _("Navegar el sitio en idioma español"); ?>"></a></li>
+<ul class="lang pRel horizontalBT unstyledAT clearfix">
+    <li><a href="#"><img src="/clientes/frare/img/fl_es.svg" alt="<?php echo _("Navegar el sitio en idioma espanol"); ?>"></a></li>
     <li><a href="#"><img src="/clientes/frare/img/fl_uk.svg" alt="<?php echo _("Switch to English version"); ?>"></a></li>
+    <li><a href="#"><img src="/clientes/frare/img/fl_br.svg" alt="<?php echo _("Navegar no site em Portugues"); ?>"></a></li>
 </ul>
 
 <?php
 # Gettext
 /*
 
-msgid "Navegar el sitio en idioma espa&ntilde;ol"
+msgid "Navegar el sitio en idioma espanol"
 msgstr "Navegar el sitio en idioma espa&ntilde;ol"
 
 msgid "Switch to English version"
-msgstr "Switch to English version"ç+
+msgstr "Switch to English version"
+
+msgid "Navegar no site em Portugues"
+msgstr "Navegar no site em Portugu&ecirc;s"
 
 */
