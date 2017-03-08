b75d6261970b3de6ab07e5dd9daa7d29133d8bfc
Gabriel Volonte <tampas@gmail.com>
2017-03-08 20:06:18 -0300

    agrego vínculos con gettext en logoHeader

--- src/common/header.inc.php
+++ src/common/header.inc.php
@@ -3,14 +3,34 @@
 <div class="pAbs" id="topGlobal"></div>
 <header id="header" class="">
     <?php    
+        //Normal
         if($index == 1){
             echo '<h1 class="logoHeader"><a href="/nuevo/" class="pRel displayBlock man"><img src="/nuevo/img/logo.svg" alt="'._("Logo de ").'" /></a></h1>';
+            
+        //Con gettext
+        if($index == 1){
+            echo '<h1 class="logoHeader"><a href="/nuevo/';
+            if($idioma == 'pt_BR'){ echo '?l=pt_BR'; } else if($idioma == 'en_GB'){ echo '?l=en_GB'; } else { echo '?l=es_AR'; }
+            echo '" class="pRel displayBlock man"><img src="/nuevo/img/logo.svg" alt="'._("Logo de ").'" /></a></h1>';
+            
+        //Framework
         }else if($test == 1){
-            echo '<h1 class="logoHeadern"><a href="/nuevo/oldframework/" class="pRel displayBlock man"><img src="/nuevo/img/logo.svg" alt="'._("Logo de ").'" /></a></h1>';
+            echo '<h1 class="logoHeader"><a href="/nuevo/oldframework/" class="pRel displayBlock man"><img src="/nuevo/img/logo.svg" alt="'._("Logo de ").'" /></a></h1>';
+            
+        //Sección
         }else if($seccion == 1){
             echo '<a href="/nuevo/" class="logoHeader displayBlock"><img src="/nuevo/img/logo.svg" alt="'._("Logo de ").'" /></a>';
+            
+        //Normal ancla
         }else{
             echo '<a href="/nuevo/" class="logoHeader displayBlock"><img src="/nuevo/img/logo.svg" alt="'._("Logo de ").'" /></a>';
+        }
+            
+        //Ancla con gettext
+        }else{
+            echo '<a href="/nuevo/';
+            if($idioma == 'pt_BR'){ echo '?l=pt_BR'; } else if($idioma == 'en_GB'){ echo '?l=en_GB'; } else { echo '?l=es_AR'; }
+            echo '" class="logoHeader displayBlock"><img src="/nuevo/img/logo.svg" alt="'._("Logo de ").'" /></a>';
         }
     ?>
     
