8804af253e006f08aa525685c576c8ce905e8de7
Gabriel Volonte <tampas@gmail.com>
2017-03-08 18:48:36 -0300

    correcciones en acentos de gettext

--- src/common/lang.inc.php
+++ src/common/lang.inc.php
@@ -1,20 +1,20 @@
 <ul class="lang pRel horizontalBT unstyledAT clearfix">
-    <li><a href="/nuevo/"><img src="/nuevo/img/fl_es.svg" alt="<?php echo _("Navegar el sitio en idioma espanol"); ?>"></a></li>
+    <li><a href="/nuevo/"><img src="/nuevo/img/fl_es.svg" alt="<?php echo _("Navegar el sitio en idioma espa&ntilde;ol"); ?>"></a></li>
     <li><a href="/nuevo/"><img src="/nuevo/img/fl_uk.svg" alt="<?php echo _("Switch to English version"); ?>"></a></li>
-    <li><a href="/nuevo/"><img src="/nuevo/img/fl_br.svg" alt="<?php echo _("Navegar no site em Portugues"); ?>"></a></li>
+    <li><a href="/nuevo/"><img src="/nuevo/img/fl_br.svg" alt="<?php echo _("Navegar no site em Portugu&ecirc;s"); ?>"></a></li>
 </ul>
 
 <?php
 # Gettext
 /*
 
-msgid "Navegar el sitio en idioma espanol"
+msgid "Navegar el sitio en idioma espa&ntilde;ol"
 msgstr "Navegar el sitio en idioma espa&ntilde;ol"
 
 msgid "Switch to English version"
 msgstr "Switch to English version"
 
-msgid "Navegar no site em Portugues"
+msgid "Navegar no site em Portugu&ecirc;s"
 msgstr "Navegar no site em Portugu&ecirc;s"
 
 */
