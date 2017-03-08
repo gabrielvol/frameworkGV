d2c0cfa1c7dbabd94789746994841cf5a7fc3b69
Gabriel Volonte <tampas@gmail.com>
2017-03-08 20:12:04 -0300

    ajustes en gettext.inc.php

--- src/common/gettext.inc.php
+++ src/common/gettext.inc.php
@@ -1,27 +1,24 @@
 <?php
 # Gettext
-/*
 
- switch ($_GET["idioma"]) {
-  case 1:
-    $idioma = 'es_AR';
-    break;
-  case 2:
-    $idioma = 'it_IT';
-    break;
-  case 3:
-    $idioma = 'en_GB';
-    break;
-}
-// Define el idioma
-putenv("LANG=$idioma");
-setlocale(LC_ALL, $idioma);
+    switch ($_GET["idioma"]) {
+        case 1:
+            $idioma = 'es_AR';
+            break;
+        case 2:
+            $idioma = 'it_IT';
+            break;
+        case 3:
+            $idioma = 'en_GB';
+            break;
+    }
+    // Define el idioma
+    putenv("LANG=$idioma");
+    setlocale(LC_ALL, $idioma);
 
-// Define la ubicación de los ficheros de traducción
-bindtextdomain("messages", "locale");
-textdomain("messages");
-
-*/
+    // Define la ubicación de los ficheros de traducción
+    bindtextdomain("messages", "locale");
+    textdomain("messages");
 
 /*
 
