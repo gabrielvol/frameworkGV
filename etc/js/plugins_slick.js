b8ace32648257317430f5673fa6e8d8cb9f62069
Gabriel Volonte <tampas@gmail.com>
2017-03-13 16:25:06 -0300

    agrego js de slick en /etc/js/

---
+++ etc/js/plugins_slick.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    /* slick slider */
+    $('.slick').slick({
+        infinite: true,
+        dots: true,
+        arrows: false,            
+        fade: true,
+
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        lazyLoad: 'ondemand',
+
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 500,
+
+        centerMode: true,
+        centerPadding: '60px',
+
+        variableWidth: true,
+        adaptiveHeight: true,
+
+        responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        ]
+    });
+});
