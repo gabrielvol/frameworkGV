cab6c56d1d8c525f02852b07429986297685549c
Gabriel Volonte <tampas@gmail.com>
2017-05-08 15:02:47 -0300

    ajustes en h1/logo dinámico

--- src/common/header.inc.php
+++ src/common/header.inc.php
@@ -2,56 +2,46 @@
 <a href="#main" id="go-main-content"><?php echo _("Ir al contenido principal de esta pagina"); ?>.</a>
 <div class="pAbs" id="topGlobal"></div>
 <header id="header" class="">
-    <?php    
-        //Normal
-        if($index == 1){
-            echo '<h1 class="logoHeader"><a href="/nuevo/" class="pRel displayBlock man"><img src="/nuevo/img/logo.svg" alt="'._("Logo de ").'" /></a></h1>';
-            
-        //Con gettext
-        if($index == 1){
-            echo '<h1 class="logoHeader"><a href="/nuevo/';
-                if($idioma == 'pt_BR'){
-                    echo '?l=pt_BR';
-                } else if($idioma == 'en_GB') {
-                    echo '?l=en_GB';
-                } else {
-                    echo '?l=es_AR';
-                }
-            echo '" class="pRel displayBlock man"><img src="/nuevo/img/logo.svg" alt="'._("Logo de ").'" /></a></h1>';
-            
-        //Framework
-        }else if($test == 1) {
+    // Normal
+    <?php if($index == 1){ echo '<h1 class="logoHeader">'; } ?>
+    <a href="/clientes/anibal/" class="pRel displayBlock man <?php if($int == 1){ echo 'logoHeader'; } ?>">
+        <img src="/clientes/anibal/img/logo.png" alt="<?php echo _('Logo de'); ?>" />
+    </a>
+    <?php if($index == 1){ echo '</h1>'; } ?>    
+    
+    // Con Gettext
+    <?php if($index == 1){ echo '<h1 class="logoHeader">'; } ?>
+    <a href="/clientes/anibal/
+        <?php if($idioma == 'pt_BR'){
+            echo '?l=pt_BR';
+        } else if($idioma == 'en_GB') {
+            echo '?l=en_GB';
+        } else {
+            echo '?l=es_AR';
+        } ?>
+    " class="pRel displayBlock man <?php if($int == 1){ echo 'logoHeader'; } ?>">
+        <img src="/clientes/anibal/img/logo.png" alt="<?php echo _('Logo de'); ?>" />
+    </a>
+    <?php if($index == 1){ echo '</h1>'; } ?>
+        
+    
+    
+    // Framework
+    <?php if($test == 1) {
             echo '<h1 class="logoHeader"><a href="/nuevo/oldframework/" class="pRel displayBlock man"><img src="/nuevo/img/logo.svg" alt="'._("Logo de ").'" /></a></h1>';
             
         //Sección
         } else if($seccion == 1) {
             echo '<a href="/nuevo/" class="logoHeader displayBlock"><img src="/nuevo/img/logo.svg" alt="'._("Logo de ").'" /></a>';
-            
-        //Normal ancla
-        } else {
-            echo '<a href="/nuevo/" class="logoHeader displayBlock"><img src="/nuevo/img/logo.svg" alt="'._("Logo de ").'" /></a>';
-        }
-            
-        //Ancla con gettext
-        } else {
-            echo '<a href="/nuevo/';
-                if($idioma == 'pt_BR') {
-                    echo '?l=pt_BR';
-                } else if($idioma == 'en_GB') {
-                    echo '?l=en_GB';
-                } else {
-                    echo '?l=es_AR';
-                }
-            echo '" class="logoHeader displayBlock"><img src="/nuevo/img/logo.svg" alt="'._("Logo de ").'" /></a>';
         }
     ?>
     
     <?php
         if($index == 1){
             include('nav.inc.php');
-        }else if($test == 1){
+        } else if ($test == 1){
             include($_SERVER['DOCUMENT_ROOT'] . '/nuevo/oldframework/common/nav.inc.php');
-        }else{
+        } else {
             include('nav.inc.php');
         }
     ?>
