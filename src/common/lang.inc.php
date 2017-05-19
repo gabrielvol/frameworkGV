e38a591089445cdc087ada7fe6492e1ef72344ca
Gabriel Volonte <tampas@gmail.com>
2017-05-19 15:00:43 -0300

    ajustes en lista de idiomas

--- src/common/lang.inc.php
+++ src/common/lang.inc.php
@@ -6,15 +6,15 @@
 
 <ul class="lang pRel horizontal clearfix">
     <li>
-        <a href="/nuevo/?l=es_AR" class="displayBlock pRel">
+        <a href="/nuevo/?l=es_AR" class="displayBlock mask">
             <img src="/nuevo/img/fl_es.svg" alt="<?php echo $text_es; ?>" title="<?php echo $text_es; ?>">
         </a></li>
     <li>
-        <a href="/nuevo/?l=en_GB" class="displayBlock pRel">
+        <a href="/nuevo/?l=en_GB" class="displayBlock mask">
             <img src="/nuevo/img/fl_uk.svg" alt="<?php echo $text_en; ?>" title="<?php echo $text_en; ?>">
         </a></li>
     <li>
-        <a href="/nuevo/?l=pt_BR" class="displayBlock pRel">
+        <a href="/nuevo/?l=pt_BR" class="displayBlock mask">
             <img src="/nuevo/img/fl_br.svg" alt="<?php echo $text_pt; ?>" title="<?php echo $text_pt; ?>">
         </a></li>
 </ul>
