67c9b40f5c3ecda91ec376d00ee7aad126136b67
Gabriel Volonte <tampas@gmail.com>
2017-08-08 00:42:32 -0300

    agrego txt para redes sociales

--- src/common/social.inc.php
+++ src/common/social.inc.php
@@ -29,51 +29,9 @@
             <?php echo _("Mir&aacute; nuestro perfil en LinkedIn"); ?>
             <img src="/img/soc_li.svg" class="svg displayBlock" alt="<?php echo _('&Iacute;cono de LinkedIn'); ?>">
         </a></li>
+    <li class="sp displayBlock mhaBT indentedText" title="<?php echo _('Escuchanos en Spotify'); ?>">
+            <?php echo _("Escuchanos en Spotify"); ?>
+        <a href="https://play.spotify.com/artist/4X5957TE7Z36De4cytdkjr?play=true&utm_source=open.spotify.com&utm_medium=open" target="_blank" alt="Escuchanos en Spotify">
+            <img src="/img/soc_li.svg" class="svg displayBlock" alt="<?php echo _('&Iacute;cono de Spotify'); ?>">            
+        </a></li>
 </ul>
-
-<?php
-# Textos gettext de Redes Sociales
-/*
-msgid "Seguinos en Facebook"
-msgstr "Seguinos en Facebook"
-
-msgid "&Iacute;cono de Facebook"
-msgstr "&Iacute;cono de Facebook"
-
-msgid "Compartir en Facebook"
-msgstr "Compartir en Facebook"
-
-msgid "Seguinos en Twitter"
-msgstr "Seguinos en Twitter"
-
-msgid "&Iacute;cono de Twitter"
-msgstr "&Iacute;cono de Twitter"
-
-msgid "Compartir en Twitter"
-msgstr "Compartir en Twitter"
-
-msgid "Seguinos en Instagram"
-msgstr "Seguinos en Instagram"
-
-msgid "&Iacute;cono de Instagram"
-msgstr "&Iacute;cono de Instagram"
-
-msgid "Seguinos en Pinterest"
-msgstr "Seguinos en Pinterest"
-
-msgid "&Iacute;cono de Pinterest"
-msgstr "&Iacute;cono de Pinterest"
-
-msgid "Mir&aacute; Nuestro Canal en YouTube"
-msgstr "Mir&aacute; Nuestro Canal en YouTube"
-
-msgid "&Iacute;cono de Youtube"
-msgstr "&Iacute;cono de Youtube"
-
-msgid "Mir&aacute; nuestro perfil en LinkedIn"
-msgstr "Mir&aacute; nuestro perfil en LinkedIn"
-
-msgid "&Iacute;cono de LinkedIn"
-msgstr "&Iacute;cono de LinkedIn"
-
-*/
