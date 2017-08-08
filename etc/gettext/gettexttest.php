0a00382af79334a1d6047b4837770277ce5042a6
Gabriel Volonte <tampas@gmail.com>
2017-08-07 21:22:47 -0300

    agrego gettext test

---
+++ etc/gettext/gettexttest.php
@@ -0,0 +1,14 @@
+<?php
+//
+// test if gettext extension is installed with php
+//
+
+if (!function_exists("gettext"))
+{
+    echo "gettext is not installed\n";
+}
+else
+{
+    echo "gettext is supported\n";
+}
+?>
