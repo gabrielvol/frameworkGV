db8ec2557d322fdc8159ad05903168e8d160c5e4
Gabriel Volonte <tampas@gmail.com>
2017-08-24 18:38:23 -0300

    ajustes en redes sociales

--- src/common/social.inc.php
+++ src/common/social.inc.php
@@ -1,37 +1,37 @@
 <ul class="social horizontal clearfix">
     <li class="fb">
-        <a href="/nuevo/" class="fb displayBlock mhaBT indentedText" title="<?php echo _('Seguinos en Facebook'); ?>">
+        <a href="/nuevo/" class="fb displayBlock hoverGrow mhaBT indentedText" title="<?php echo _('Seguinos en Facebook'); ?>">
             <?php echo _("Seguinos en Facebook"); ?>
             <img src="/img/soc_fb.svg" class="svg displayBlock" alt="<?php echo _('&Iacute;cono de Facebook'); ?>">
         </a></li>
     <li class="tw">
-        <a href="/nuevo/" class="tw displayBlock mhaBT indentedText" title="<?php echo _('Seguinos en Twitter'); ?>">
+        <a href="/nuevo/" class="tw displayBlock hoverGrow mhaBT indentedText" title="<?php echo _('Seguinos en Twitter'); ?>">
             <?php echo _("Seguinos en Twitter"); ?>
             <img src="/img/soc_tw.svg" class="svg displayBlock" alt="<?php echo _('&Iacute;cono de Twitter'); ?>">
         </a></li>
     <li class="ig">
-        <a href="/nuevo/" class="ig displayBlock mhaBT indentedText" title="<?php echo _('Seguinos en Instagram'); ?>">
+        <a href="/nuevo/" class="ig displayBlock hoverGrow mhaBT indentedText" title="<?php echo _('Seguinos en Instagram'); ?>">
             <?php echo _("Seguinos en Instagram"); ?>
             <img src="/img/soc_ig.svg" class="svg displayBlock" alt="<?php echo _('&Iacute;cono de Instagram'); ?>">
         </a></li>
     <li class="pi">
-        <a href="/nuevo/" class="pi displayBlock mhaBT indentedText" title="<?php echo _('Seguinos en Pinterest'); ?>">
+        <a href="/nuevo/" class="pi displayBlock hoverGrow mhaBT indentedText" title="<?php echo _('Seguinos en Pinterest'); ?>">
             <?php echo _("Seguinos en Pinterest"); ?>
             <img src="/img/soc_pi.svg" class="svg displayBlock" alt="<?php echo _('&Iacute;cono de Pinterest'); ?>">
         </a></li>
     <li class="yt">
-        <a href="/nuevo/" class="yt displayBlock mhaBT indentedText" title="<?php echo _('Mir&aacute; Nuestro Canal en YouTube'); ?>">
+        <a href="/nuevo/" class="yt displayBlock hoverGrow mhaBT indentedText" title="<?php echo _('Mir&aacute; Nuestro Canal en YouTube'); ?>">
             <?php echo _("Mir&aacute; Nuestro Canal en YouTube"); ?>
             <img src="/img/soc_yt.svg" class="svg displayBlock" alt="<?php echo _('&Iacute;cono de YouTube'); ?>">
         </a></li>
     <li class="li">
-        <a href="/nuevo/" class="li displayBlock mhaBT indentedText" title="<?php echo _('Mir&aacute; nuestro perfil en LinkedIn'); ?>">
+        <a href="/nuevo/" class="li displayBlock hoverGrow mhaBT indentedText" title="<?php echo _('Mir&aacute; nuestro perfil en LinkedIn'); ?>">
             <?php echo _("Mir&aacute; nuestro perfil en LinkedIn"); ?>
             <img src="/img/soc_li.svg" class="svg displayBlock" alt="<?php echo _('&Iacute;cono de LinkedIn'); ?>">
         </a></li>
-    <li class="sp displayBlock mhaBT indentedText" title="<?php echo _('Escuchanos en Spotify'); ?>">
+    <li class="sp displayBlock hoverGrow mhaBT indentedText" title="<?php echo _('Escuchanos en Spotify'); ?>">
             <?php echo _("Escuchanos en Spotify"); ?>
-        <a href="https://play.spotify.com/artist/4X5957TE7Z36De4cytdkjr?play=true&utm_source=open.spotify.com&utm_medium=open" target="_blank" alt="Escuchanos en Spotify">
+        <a href="https://play.spotify.com/artist/ARTISTKEY?play=true&utm_source=open.spotify.com&utm_medium=open" target="_blank" alt="Escuchanos en Spotify">
             <img src="/img/soc_li.svg" class="svg displayBlock" alt="<?php echo _('&Iacute;cono de Spotify'); ?>">            
         </a></li>
 </ul>
