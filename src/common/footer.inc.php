7cf4b6daf62a12f1e4b2b3ff557b88da3a0da328
Gabriel Volonte <tampas@gmail.com>
2017-09-04 23:00:15 -0300

    ajuste en copyright de footer

--- src/common/footer.inc.php
+++ src/common/footer.inc.php
@@ -1,10 +1,12 @@
 <footer id="footer" class="">
-    &copy; <?php
-        $fromYear = 2008;
-        $thisYear = (int)date('Y');
-        echo $fromYear . (($fromYear != $thisYear) ? '-' . $thisYear : '');?> Company.
-
-    <?php echo date("Y"); ?>
+    <p class="copyright white upperCase">
+        &copy;
+        <?php
+            $fromYear = 2010;
+            $thisYear = (int)date('Y');
+            echo $fromYear . (($fromYear != $thisYear) ? ' - ' . $thisYear : ''); 
+        ?> Company y Asociados
+    </p>
         
     <img src="/nuevo/img/logo.svg" class="logoFooter displayBlock" alt="<?php echo _("Logo de"); ?>">
     <?php include('social.inc.php'); ?>
