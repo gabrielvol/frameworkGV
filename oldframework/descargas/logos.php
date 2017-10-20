c77c258d4a6f4b1dfebe971469f74c57ff56a53f
Gabriel Volonte <tampas@gmail.com>
2017-10-20 15:04:55 -0300

    reestructuración framework

--- oldframework/descargas/logos.php
+++
@@ -1,7 +0,0 @@
-<div class="clearfix ptl bg bts mtl">
-    <h2>Logos</h2>
-    <ul class="bull">
-        <li class="clearfix mbs">Logo Original (para impresión)<a href="/nuevo/oldframework/archivos/logo.zip" class="dd displayBlock floatRight bgBlue pvs phm bold white upperCase alignCenter">Descargar</a></li>
-        <li class="clearfix mbs">Logo (formato JPG/uso digital)<a href="/nuevo/oldframework/archivos/logo_jpg.zip" class="dd displayBlock floatRight bgBlue pvs phm bold white upperCase alignCenter">Descargar</a></li>
-    </ul>
-</div>
