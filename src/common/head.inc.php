45e4f3bb1d2758870860cb2e24be1a76ac283392
Gabriel Volonte <tampas@gmail.com>
2017-11-05 18:43:06 -0300

    actualizo head, agrego humans.txt

--- src/common/head.inc.php
+++ src/common/head.inc.php
@@ -1,16 +1,73 @@
 <!doctype html>
 <html lang="es-AR">
 <head>
-<meta charset="utf-8"/>
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge,chrome=1">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="robots" content="index,follow">
+<meta name="generator" content="NetBeans IDE">
+<meta name="rating" content="General">
+<meta name="geo.region" content="AR">
+
+<!-- Google No Translate -->
+<meta name="google" content="notranslate">
+
 <title><?php echo $title ?></title>
 <title><?php if($idioma == 'en_GB'){ echo $titleEN; } else if($idioma == 'pt_BR'){ echo $titleBR; } else { echo $title; } ?></title>
+
 <meta name="description" content="<?php echo $mdesc ?>"/>
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="description" content="<?php if($idioma == 'en_GB'){ echo $mdescEN; } else if($idioma == 'pt_BR'){ echo $mdescBR; } else { echo $mdesc; } ?>"/>
 
 <link href="/nuevo/css/styles.css" rel="stylesheet">
+<link rel="author" href="humans.txt">
 
+<!-- Copyright -->
+<link rel="license" href="copyright.html">
+
+<!-- Idiomas -->
+<link rel="alternate" href="https://es.example.com/" hreflang="es">
+
+<!-- Información personal -->
+<link rel="me" href="https://google.com/profiles/thenextweb" type="text/html">
+<link rel="me" href="mailto:name@example.com">
+<link rel="me" href="sms:+15035550125">
+
+<!-- Archivo -->
+<link rel="archives" href="http://example.com/archives/">
+
+<!-- Notifies a URL when you link to it on your document -->
+<link rel="webmention" href="http://example.com/webmention">
+
+<!-- Facebook Open Graph -->
+<meta property="fb:app_id" content="123456789">
+<meta property="og:url" content="http://example.com/page.html">
+<meta property="og:type" content="website">
+<meta property="og:title" content="Sitio web GabrielVolonte">
+<meta property="og:image" content="http://example.com/image.jpg">
+<meta property="og:description" content="Descriptio Here">
+<meta property="og:site_name" content="SiteName">
+<meta property="og:locale" content="es_AR">
+
+<!-- Twitter Card -->
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="@siteAccount">
+<meta name="twitter:creator" content="@individualAccount">
+<meta name="twitter:url" content="http://example.com/page.html">
+<meta name="twitter:title" content="ContentTitle">
+<meta name="twitter:description" content="ContentDescriptionLessThan200characters">
+<meta name="twitter:image" content="http://example.com/image.jpg">
+
+<!-- Google+ / Schema.org -->
+<link href="https://plus.google.com/+YourPage" rel="publisher">
+<meta itemprop="name" content="Content Title">
+<meta itemprop="description" content="Content description less than 200 characters">
+<meta itemprop="image" content="http://example.com/image.jpg">
+
+<!-- Pinterest -->
+<meta name="pinterest" content="nopin" description="Sorry, you can't save from my website!">
+
+<!-- Favicon -->
 <link rel="apple-touch-icon" sizes="180x180" href="/nuevo/common/apple-touch-icon.png">
 <link rel="icon" type="image/png" href="/nuevo/common/favicon-32x32.png" sizes="32x32">
 <link rel="icon" type="image/png" href="/nuevo/common/favicon-16x16.png" sizes="16x16">
@@ -18,12 +75,32 @@
 <link rel="mask-icon" href="/nuevo/common/safari-pinned-tab.svg" color="#000">
 <meta name="theme-color" content="#ffffff">
 
+<!-- Google Android -->
+<!-- Add to home screen -->
+<meta name="mobile-web-app-capable" content="yes">
+<!-- More info: https://developer.chrome.com/multidevice/android/installtohomescreen -->
+<!-- Android app deep linking -->
+<meta name="google-play-app" content="app-id=package-name">
+<link rel="alternate" href="android-app://package-name/http/url-sample.com">
+
+<!-- App Links -->
+<!-- iOS -->
+<meta property="al:ios:url" content="applinks://docs">
+<meta property="al:ios:app_store_id" content="12345">
+<meta property="al:ios:app_name" content="App Links">
+<!-- Android -->
+<meta property="al:android:url" content="applinks://docs">
+<meta property="al:android:app_name" content="App Links">
+<meta property="al:android:package" content="org.applinks">
+<!-- Web Fallback -->
+<meta property="al:web:url" content="http://applinks.org/documentation">
+
+<!-- Jquery, Modernizr, HTML5shiv -->
 <script type="text/javascript" src="/nuevo/js/jQuery3.2.1.min.js"></script>
 <script type="text/javascript" src="/nuevo/js/modernizr2.8.3.js"></script>
 <script type="text/javascript" src="/nuevo/js/html5shiv.min.js"></script>
-<!--<script type='text/javascript' src='http://getfirebug.com/releases/lite/1.2/firebug-lite-compressed.js'></script>-->
 
-<!--
+<!-- Google Analytics -->
 <script type="text/javascript">
 
   var _gaq = _gaq || [];
@@ -37,4 +114,3 @@
   })();
 
 </script>
--->
