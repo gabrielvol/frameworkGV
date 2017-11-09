ec3ff50fd5626eecfe19c9e27baf5e304462715f
Gabriel Volonte <tampas@gmail.com>
2017-11-09 19:56:19 -0300

    ajustes en nav, agrego aria

--- src/common/nav.inc.php
+++ src/common/nav.inc.php
@@ -1,6 +1,15 @@
-<nav id="nav" class="mainNav container pRel">
-    <a href="javascript:void(0)" class="hamb pAbs displayNone displayBlockBT alignCenter upperCase"><?php echo _("Men&uacute;"); ?></a>
+<nav id="nav" class="mainNav container pRel" role="navigation">
+    <button class="hamb pAbs displayNone displayBlockBT alignCenter upperCase"><?php echo _("Men&uacute;"); ?></button>
     <ul class="mainMenu displayNoneBT unstyled horizontalAT clearfix">
         <li class="<?php if($navSelected == 1) echo "selected" ?>"><a href="/nuevo/" class="displayBlockBT"><?php echo _("Inicio"); ?></a></li>
+        
+        <li class="nos <?php if($navSelected == 1) echo "selected" ?>">
+            <a href="/nuevo/nosotros/" id="nosoNavItem" aria-haspopup="true" aria-owns="nosSubMenu" aria-controls="nosSubMenu" aria-expanded="false" class="displayBlock upperCase alignCenter">
+                <?php echo _("Nosotros"); ?>
+            </a>
+            <ul id="nosSubMenu" role="group" aria-expanded="false" aria-labelledby="nosNavItem" class="subMenu unstyled">
+                <li><a href="/nuevo/nosotros/historia.php" class="displayBlock"><?php echo _("Historia"); ?></a></li>
+            </ul>
+        </li>
     </ul>
 </nav>
