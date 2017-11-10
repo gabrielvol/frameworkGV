6d66da842712da8c6421c8f84d36e66fef38c1d9
Gabriel Volonte <tampas@gmail.com>
2017-11-10 17:27:21 -0300

    ajusto aria expanded en nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,19 @@
 $(document).ready(function () {
-//Hamburger Menu
+    // Acciones del menú hamburguesa
     $(".hamb").click(function () {
         $(this).closest("#nav").toggleClass('clicked');
         $(this).closest("#nav").find(".mainMenu").toggleClass('displayNoneBT');
-    });
-    
+    });    
+    // Oculto menú al dar clic en algún item
     $('.mainMenu a').click(function () {
         $(this).closest(".mainMenu").addClass('displayNoneBT');
+    });
+    
+    // Acciones al hover en un item con submenú
+    $('.hasSub').hover(function () {
+        $(this).closest("#header").find(".bgMenu").toggleClass('displayNone');
+        $(this).find(".topLevelItem").attr('aria-expanded', function (i, attr) {
+            return attr === 'true' ? 'false' : 'true';
+        });
     });
 });
