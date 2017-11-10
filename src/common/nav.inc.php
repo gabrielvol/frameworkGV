b3afb2dfea48903ec5612507ca6d8fb6d26939c4
Gabriel Volonte <tampas@gmail.com>
2017-11-10 16:27:01 -0300

    agrego aria para menú principal del sitio

--- src/common/nav.inc.php
+++ src/common/nav.inc.php
@@ -1,4 +1,4 @@
-<nav id="nav" class="mainNav container pRel" role="navigation">
+<nav id="nav" class="mainNav container pRel" role="navigation" aria-label="<?php echo _("Men&uacute; principal del sitio"); ?>">
     <button class="hamb pAbs displayNone displayBlockBT alignCenter upperCase"><?php echo _("Men&uacute;"); ?></button>
     <ul class="mainMenu displayNoneBT unstyled horizontalAT clearfix">
         <li class="<?php if($navSelected == 1) echo "selected" ?>"><a href="/nuevo/" class="displayBlockBT"><?php echo _("Inicio"); ?></a></li>
