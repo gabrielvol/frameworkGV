3e7951efceb89137843c34cafdddeb24254310c1
Gabriel Volonte <tampas@gmail.com>
2017-11-19 16:47:36 -0300

    js para topper

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('#header').addClass('topper');
+    } else {
+        $('#header').removeClass('topper');
+    }
+});
+
 $(document).ready(function () {
     // Acciones del menú hamburguesa
     $(".hamb").click(function () {
@@ -7,8 +15,7 @@ $(document).ready(function () {
     // Oculto menú al dar clic en algún item
     $('.mainMenu a').click(function () {
         $(this).closest(".mainMenu").addClass('displayNoneBT');
-    });
-    
+    });    
     // Acciones al hover en un item con submenú
     $('.hasSub').hover(function () {
         $(this).closest("#header").find(".bgMenu").toggleClass('displayNone');
