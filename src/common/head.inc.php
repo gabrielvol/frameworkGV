d5db8f968811558b673a6ebdc137f946207774d1
Gabriel Volonte <tampas@gmail.com>
2017-11-21 00:52:48 -0300

    ajustes en head

--- src/common/head.inc.php
+++ src/common/head.inc.php
@@ -2,8 +2,7 @@
 <html lang="es-AR">
 <head>
 <meta charset="utf-8">
-<meta http-equiv="x-ua-compatible" content="ie=edge,chrome=1">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="x-ua-compatible" content="ie=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="robots" content="index,follow">
 <meta name="generator" content="NetBeans IDE">
