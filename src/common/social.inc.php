a81758e45eea35d4f982c177a0f8eaa605c947e1
Gabriel Volonte <tampas@gmail.com>
2017-11-28 15:20:11 -0300

    agrego hover grow big

--- src/common/social.inc.php
+++ src/common/social.inc.php
@@ -1,36 +1,36 @@
 <ul class="social horizontal clearfix">
     <li class="fb">
-        <a href="/nuevo/" class="fb displayBlock hoverGrow mhaBT indentedText">
+        <a href="/nuevo/" class="fb displayBlock hoverGrowBig mhaBT indentedText">
             <?php echo _("Seguinos en Facebook"); ?>
             <img src="/img/soc_fb.svg" class="svg displayBlock" alt="<?php echo _('&Iacute;cono de Facebook'); ?>.">
         </a></li>
     <li class="tw">
-        <a href="/nuevo/" class="tw displayBlock hoverGrow mhaBT indentedText">
+        <a href="/nuevo/" class="tw displayBlock hoverGrowBig mhaBT indentedText">
             <?php echo _("Seguinos en Twitter"); ?>
             <img src="/img/soc_tw.svg" class="svg displayBlock" alt="<?php echo _('&Iacute;cono de Twitter'); ?>.">
         </a></li>
     <li class="ig">
-        <a href="/nuevo/" class="ig displayBlock hoverGrow mhaBT indentedText">
+        <a href="/nuevo/" class="ig displayBlock hoverGrowBig mhaBT indentedText">
             <?php echo _("Seguinos en Instagram"); ?>
             <img src="/img/soc_ig.svg" class="svg displayBlock" alt="<?php echo _('&Iacute;cono de Instagram'); ?>.">
         </a></li>
     <li class="pi">
-        <a href="/nuevo/" class="pi displayBlock hoverGrow mhaBT indentedText">
+        <a href="/nuevo/" class="pi displayBlock hoverGrowBig mhaBT indentedText">
             <?php echo _("Seguinos en Pinterest"); ?>
             <img src="/img/soc_pi.svg" class="svg displayBlock" alt="<?php echo _('&Iacute;cono de Pinterest'); ?>.">
         </a></li>
     <li class="yt">
-        <a href="/nuevo/" class="yt displayBlock hoverGrow mhaBT indentedText">
+        <a href="/nuevo/" class="yt displayBlock hoverGrowBig mhaBT indentedText">
             <?php echo _("Mir&aacute; Nuestro Canal en YouTube"); ?>
             <img src="/img/soc_yt.svg" class="svg displayBlock" alt="<?php echo _('&Iacute;cono de YouTube'); ?>.">
         </a></li>
     <li class="li">
-        <a href="/nuevo/" class="li displayBlock hoverGrow mhaBT indentedText">
+        <a href="/nuevo/" class="li displayBlock hoverGrowBig mhaBT indentedText">
             <?php echo _("Mir&aacute; nuestro perfil en LinkedIn"); ?>
             <img src="/img/soc_li.svg" class="svg displayBlock" alt="<?php echo _('&Iacute;cono de LinkedIn'); ?>.">
         </a></li>
     <li class="sp">
-        <a href="/nuevo/" class="sp displayBlock hoverGrow mhaBT indentedText">
+        <a href="/nuevo/" class="sp displayBlock hoverGrowBig mhaBT indentedText">
             <?php echo _("Escuchanos en Spotify"); ?>
             <img src="/img/soc_sp.svg" class="svg displayBlock" alt="<?php echo _('&Iacute;cono de Spotify'); ?>.">
         </a></li>
