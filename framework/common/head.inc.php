aa889601d22419c8095b1a1e041e906b1a17425a
Gabriel Volonte <tampas@gmail.com>
2017-12-01 18:42:15 -0300

    gulp for framework

--- framework/common/head.inc.php
+++ framework/common/head.inc.php
@@ -2,8 +2,7 @@
 <html lang="es-AR">
 <head>
 <meta charset="utf-8">
-<meta http-equiv="x-ua-compatible" content="ie=edge,chrome=1">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="x-ua-compatible" content="ie=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="robots" content="index,follow">
 <meta name="generator" content="NetBeans IDE">
@@ -11,10 +10,9 @@
 <meta name="geo.region" content="AR">
 
 <title><?php echo $title ?></title>
-<meta name="description" content="<?php echo $mdesc ?>"/>
 
-<link href="http://www.gabrielvolonte.com.ar/css/styles.css" rel="stylesheet">
+<link href="/framekwork/css/main.css" rel="stylesheet">
 
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-<script type="text/javascript" src="/nuevo/js/modernizr2.8.3.js"></script>
-<script type="text/javascript" src="/nuevo/js/html5shiv.min.js"></script>
+<script src="/framekwork/js/jQuery3.2.1.min.js" type="text/javascript"></script>
+<script src="/js/modernizr2.8.3.js" type="text/javascript"></script>
+<script src="/js/html5shiv.min.js" type="text/javascript"></script>
--- gulpfile_custom.js
+++ gulpfile_custom.js
@@ -4,9 +4,6 @@ var gulp         = require('gulp'),
     cssnano      = require('gulp-cssnano'),
     autoprefixer = require('gulp-autoprefixer');
 
-/* =============================================================================
-   Regular Sass
-   ========================================================================== */
     // Static Server + watching scss files
 gulp.task('serve', ['stylesheets'], function() {
     gulp.watch("./etc/css/custom/*.scss", ['stylesheets']);
@@ -32,25 +29,3 @@ gulp.task('stylesheets', ['sass'], function() {
 });
 
 gulp.task('default', ['serve']);
-
-/* =============================================================================
-   Framework Sass
-   ========================================================================== */
-    // Compile sass into CSS
-gulp.task('sassFW', function () {
-    return gulp.src('./framework/css/scss/styles.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./framework/compilation/'));
-});
-
-    // CSSnano / PostCSS / Autoprefixer
-gulp.task('compileFW', ['sassFW'], function() {
-    gulp.src("./framework/css/compilation/styles.css")
-        .pipe(cssnano([,
-            postcss(),
-            autoprefixer({browsers: ['last 3 versions']})
-        ]))
-        .pipe(gulp.dest("./framework/css/"));
-});
-
-gulp.task('framework', ['compileFW']);
