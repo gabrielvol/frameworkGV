73f9e8c4e08045bdc5eac2777604d4fe54edf3e0
Gabriel Volonte <tampas@gmail.com>
2018-02-21 16:23:02 -0500

    Saco puntos dobles en alt

--- src/common/lang.inc.php
+++ src/common/lang.inc.php
@@ -1,20 +1,23 @@
 <?php
-    $text_es = "Navegar el sitio en idioma espa&ntilde;ol.";
-    $text_en = "Switch to English version.";
-    $text_pt = "Navegar no site em Portugu&ecirc;s.";
+    $text_es = "Navegar el sitio en idioma espa&ntilde;ol";
+    $text_en = "Switch to English version";
+    $text_pt = "Navegar no site em Portugu&ecirc;s";
 ?>
 
 <ul class="lang pRel horizontal clearfix">
     <li>
         <a href="/nuevo/?l=es_AR" class="displayBlock mask">
             <img src="/nuevo/img/fl_ar.svg" alt="<?php echo $text_es; ?>.">
-        </a></li>
+        </a>
+    </li>
     <li>
         <a href="/nuevo/?l=en_GB" class="displayBlock mask">
             <img src="/nuevo/img/fl_uk.svg" alt="<?php echo $text_en; ?>.">
-        </a></li>
+        </a>
+    </li>
     <li>
         <a href="/nuevo/?l=pt_BR" class="displayBlock mask">
             <img src="/nuevo/img/fl_br.svg" alt="<?php echo $text_pt; ?>.">
-        </a></li>
+        </a>
+    </li>
 </ul>
