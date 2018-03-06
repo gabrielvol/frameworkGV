9c4b5fb297a7e468e55c5306cfb983ee43359d13
Gabriel Volonte <tampas@gmail.com>
2018-03-06 13:29:24 -0500

    Ajustes de idioma

--- etc/js/_modifyHtmlLang.js
+++ etc/js/_modifyHtmlLang.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-    //modify html lang attr
-    $(".it a").click(function () {
-        $('html').attr('lang', 'it_IT');
-    });
-    $(".es a").click(function () {
+// Modify html lang attr
+    $(".ar a").click(function () {
         $('html').attr('lang', 'es_AR');
     });
-    $(".en a").click(function () {
+    $(".br a").click(function () {
+        $('html').attr('lang', 'pt_BR');
+    });
+    $(".gb a").click(function () {
         $('html').attr('lang', 'en_GB');
     });
 });
