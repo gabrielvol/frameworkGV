fee606ea561ef373f4ac412432043fc8b68d3f52
Gabriel Volonte <tampas@gmail.com>
2018-03-08 15:36:13 -0500

    Ajustes de accesibilidad en nav

--- src/common/nav.inc.php
+++ src/common/nav.inc.php
@@ -2,30 +2,22 @@
     <button class="hamb openNav pAbs displayNone displayBlockBT alignCenter upperCase" aria-pressed="false"><?php echo _("Men&uacute;"); ?></button>
     <button class="hamb closeNav pAbs indentedText"><?php echo _("Cerrar"); ?></button>
     <ul class="mainMenu unstyled horizontalAT clearfix">
-        <li class="<?php if($navCurrent == 1) { echo "current"; } ?>">
-            <a href="/nuevo/" class="displayBlockBT" <?php if($navCurrent == 1) { echo 'aria-current="page"'; } ?>>
+        <li>
+            <a href="/nuevo/" class="displayBlockBT" <?php if($navCurrent == 0) { echo 'aria-current="page"'; } ?>>
                 <?php echo _("Inicio"); ?></a>
         </li>
-        
-        <li class="nos hasSub pRel <?php if($navCurrent == 1) { echo "current"; } ?>">
-            <a href="/nuevo/nosotros/" id="nosNavItem" aria-haspopup="true" aria-owns="nosSubMenu" aria-controls="nosSubMenu" aria-expanded="false" class="topLevelItem displayBlock" <?php if($navCurrent == 1) { echo 'aria-current="page"'; } ?>>
-                <?php echo _("Nosotros"); ?>
-            </a>
-            <ul id="nosSubMenu" role="group" aria-expanded="false" aria-labelledby="nosNavItem" class="subMenu unstyled">
-                <li class="<?php if($navCurrentInt == 1) { echo "current"; } ?>">
-                    <a href="/nuevo/nosotros/historia.php" class="displayBlock" <?php if($navCurrentInt == 1) { echo 'aria-current="page"'; } ?>>
-                        <?php echo _("Historia"); ?></a>
-                </li>
-            </ul>
+        <li class="<?php if($navCurrent == 1) { echo "current"; } ?>">
+            <a href="/nuevo/" class="displayBlockBT" <?php if($navCurrent == 1) { echo 'aria-current="page"'; } ?>>
+                <?php echo _("seccion"); ?></a>
         </li>
         
-        <li class="nos hasSub <?php if($navCurrent == 1) { echo "current"; } ?>">
-            <p id="nosNavItem" aria-haspopup="true" aria-owns="nosSubMenu" aria-controls="nosSubMenu" aria-expanded="false" class="topLevelItem" role="button">
+        <li class="nos hasSub <?php if($navCurrent == 100) { echo "current"; } ?>">
+            <p class="topLevelItem" id="nosNavItem" aria-haspopup="true" aria-owns="nosSubMenu" aria-controls="nosSubMenu" role="button" aria-pressed="false">
                 <?php echo _("Nosotros"); ?>
             </p>
             <ul id="nosSubMenu" role="group" aria-expanded="false" aria-labelledby="nosNavItem" class="subMenu unstyled">
-                <li class="<?php if($navCurrentInt == 1) { echo "current"; } ?>">
-                    <a href="/nuevo/nosotros/historia.php" class="displayBlock" <?php if($navCurrentInt == 1) { echo 'aria-current="page"'; } ?>>
+                <li class="<?php if($navCurrentInt == 100) { echo "current"; } ?>">
+                    <a href="/nuevo/nosotros/historia.php" class="displayBlock" <?php if($navCurrentInt == 100) { echo 'aria-current="page"'; } ?>>
                         <?php echo _("Historia"); ?></a>
                 </li>
             </ul>
--- src/js/_main.js
+++ src/js/_main.js
@@ -8,47 +8,52 @@ $(window).scroll(function () {
 
 $(document).ready(function () {
     // Hamburger Drawer
-    $(".openNav").click(function () {
+    $('.openNav').click(function () {
         $(this).addClass('clicked');
-        $(this).closest("#nav").find(".mainMenu").addClass('open');
-        $(this).closest("#header").find(".social").addClass('clicked');
-        $(this).closest("#nav").find(".modalNav").show( "2000", "swing", function() {
-            $(this).closest("#nav").find(".closeNav").addClass('clicked');
-            $(this).closest("#header").find(".logoH").addClass('clicked');
+        $(this).closest('#nav').find('.mainMenu').addClass('open');
+        $(this).closest('#header').find('.social').addClass('clicked');
+        $(this).closest('#nav').find('.modalNav').show( '2000', 'swing', function() {
+            $(this).closest('#nav').find('.closeNav').addClass('clicked');
+            $(this).closest('#header').find('.logoH').addClass('clicked');
         });
+        $(this).attr('aria-pressed', 'true');
     });
 
     $('.closeNav').click(function () {
         $(this).removeClass('clicked');
-        $(this).closest("#header").find(".social").removeClass('clicked');
-        $(this).closest("#header").find(".logoH").removeClass('clicked');
-        $(this).closest("#nav").find(".modalNav").hide("fast", "swing");
-        $(this).closest("#nav").find(".mainMenu").removeClass('open');
-        $(this).closest("#nav").find(".subMenu").removeClass('open');
-        $(this).closest("#nav").find(".topLevelItem").removeClass('open');
-        $(this).closest("#nav").find(".openNav").removeClass('clicked');
-        $(this).closest("#nav").find(".openNav").attr('aria-pressed', function (i, attr) {
-            return attr === 'true' ? 'false' : 'true';
-        });
+        $(this).closest('#header').find('.social').removeClass('clicked');
+        $(this).closest('#header').find('.logoH').removeClass('clicked');
+        $(this).closest('#nav').find('.modalNav').hide('fast', 'swing');
+        $(this).closest('#nav').find('.mainMenu').removeClass('open');
+        $(this).closest('#nav').find('.subMenu').removeClass('open');
+        $(this).closest('#nav').find('.subMenu').attr('aria-expanded', 'false');
+        $(this).closest('#nav').find('.topLevelItem').removeClass('open');
+        $(this).closest('#nav').find('.openNav').removeClass('clicked');
+        $(this).closest('#nav').find('.openNav').attr('aria-pressed', 'false');
     });
 
     $('.modalNav').click(function () {
-        $(this).closest("#nav").find(".closeNav").removeClass("clicked");
-        $(this).closest("#header").find(".social").removeClass('clicked');
-        $(this).closest("#header").find(".logoH").removeClass('clicked');
-        $(this).hide("fast");
-        $(this).closest("#nav").find(".mainMenu").removeClass('open');
-        $(this).closest("#nav").find(".subMenu").removeClass('open');
-        $(this).closest("#nav").find(".topLevelItem").removeClass('open');
-        $(this).closest("#nav").find(".openNav").removeClass('clicked');
-        $(this).closest("#nav").find(".openNav").attr('aria-pressed', function (i, attr) {
-            return attr === 'true' ? 'false' : 'true';
-        });
+        $(this).closest('#nav').find('.closeNav').removeClass('clicked');
+        $(this).closest('#header').find('.social').removeClass('clicked');
+        $(this).closest('#header').find('.logoH').removeClass('clicked');
+        $(this).hide('fast');
+        $(this).closest('#nav').find('.mainMenu').removeClass('open');
+        $(this).closest('#nav').find('.subMenu').removeClass('open');
+        $(this).closest('#nav').find('.subMenu').attr('aria-expanded', 'false');
+        $(this).closest('#nav').find('.topLevelItem').removeClass('open');
+        $(this).closest('#nav').find('.openNav').removeClass('clicked');
+        $(this).closest('#nav').find('.openNav').attr('aria-pressed', 'false');
     });
 
     $('.topLevelItem').click(function () {
         $(this).toggleClass('open');
-        $(this).closest('.hasSub').find(".subMenu").toggleClass('open');
+        $(this).closest('.hasSub').find('.subMenu').toggleClass('open');
+        $(this).attr('aria-pressed', function (i, attr) {
+            return attr === 'true' ? 'false' : 'true';
+        });
+        $(this).closest('.hasSub').find('.subMenu').attr('aria-expanded', function (i, attr) {
+            return attr === 'true' ? 'false' : 'true';
+        });
     });
 
 //    $('.topLevelItem').hover(function () {
