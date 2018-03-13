683c8824a60ff2a7d847b265feebef920dd031b3
Gabriel Volonte <tampas@gmail.com>
2018-03-13 14:28:47 -0500

    Quito scrollKeep

---
+++ etc/js/scrollKeep.js
@@ -0,0 +1,23 @@
+;(function($){  
+    /**
+     * Store scroll position for and set it after reload
+     *
+     * @return {boolean} [loacalStorage is available]
+     */
+    $.fn.scrollPosReaload = function(){
+        if (localStorage) {
+            var posReader = localStorage["posStorage"];
+            if (posReader) {
+                $(window).scrollTop(posReader);
+                localStorage.removeItem("posStorage");
+            }
+            $(this).click(function(e) {
+                localStorage["posStorage"] = $(window).scrollTop();
+            });
+
+            return true;
+        }
+
+        return false;
+    }  
+}(jQuery));  
