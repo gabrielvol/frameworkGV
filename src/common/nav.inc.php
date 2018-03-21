3d40e8da56ea9da67df25f50318cdc8d581229c2
Gabriel Volonte <tampas@gmail.com>
2018-03-21 17:19:33 -0500

    Ajuste en nav

--- src/common/nav.inc.php
+++ src/common/nav.inc.php
@@ -3,21 +3,21 @@
     <button class="hamb navClose pAbs indentedText"><?php echo _("Cerrar"); ?></button>
     <ul class="mainMenu unstyled horizontalCenterAT clearfix">
         <li>
-            <a href="/nuevo/" class="displayBlockBT" <?php if($navCurrent == 0) { echo 'aria-current="page"'; } ?>>
+            <a href="/nuevo/" class="displayBlockBT borderBox" <?php if($navCurrent == 0) { echo 'aria-current="page"'; } ?>>
                 <?php echo _("Inicio"); ?></a>
         </li>
-        <li class="<?php if($navCurrent == 1) { echo "current"; } ?>">
-            <a href="/nuevo/" class="displayBlockBT" <?php if($navCurrent == 1) { echo 'aria-current="page"'; } ?>>
+        <li class="<?php if($navCurrent == 1) { echo 'current'; } ?>">
+            <a href="/nuevo/" class="displayBlockBT borderBox" <?php if($navCurrent == 1) { echo 'aria-current="page"'; } ?>>
                 <?php echo _("seccion"); ?></a>
         </li>
         
-        <li class="nos hasSub pRel <?php if($navCurrent == 2) { echo "current"; } ?>">
-            <p class="topLevelItem" id="nosNavItem" aria-haspopup="true" aria-owns="nosSubMenu" aria-controls="nosSubMenu" role="button" aria-pressed="false">
+        <li class="nos hasSub pRel <?php if($navCurrent == 2) { echo 'current'; } ?>">
+            <p class="topLevelItem borderBox" id="nosNavItem" aria-haspopup="true" aria-owns="nosSubMenu" aria-controls="nosSubMenu" role="button" aria-pressed="false">
                 <?php echo _("Nosotros"); ?>
             </p>
             <ul id="nosSubMenu" class="subMenu unstyled" role="group" aria-expanded="false" aria-labelledby="nosNavItem">
-                <li class="<?php if($navCurrentInt == 200) { echo "current"; } ?>">
-                    <a href="/nuevo/nosotros/historia.php" class="displayBlock" <?php if($navCurrentInt == 200) { echo 'aria-current="page"'; } ?>>
+                <li class="<?php if($navCurrentInt == 200) { echo 'current'; } ?>">
+                    <a href="/nuevo/nosotros/historia.php" class="displayBlock borderBox" <?php if($navCurrentInt == 200) { echo 'aria-current="page"'; } ?>>
                         <?php echo _("Historia"); ?></a>
                 </li>
             </ul>
