d46f95e4a2d6071bb86eeb6a8c026dfd34c71617
Gabriel Volonte <tampas@gmail.com>
2018-03-22 14:15:18 -0500

    Separo lista de nav

---
+++ src/common/nav.list.inc.php
@@ -0,0 +1,20 @@
+<li>
+    <a href="/nuevo/" class="displayBlockBT borderBox" <?php if($navCurrent == 0) { echo 'aria-current="page"'; } ?>>
+        <?php echo _("Inicio"); ?></a>
+</li>
+<li class="<?php if($navCurrent == 1) { echo 'current'; } ?>">
+    <a href="/nuevo/" class="displayBlockBT borderBox" <?php if($navCurrent == 1) { echo 'aria-current="page"'; } ?>>
+        <?php echo _("seccion"); ?></a>
+</li>
+
+<li class="nos hasSub pRel <?php if($navCurrent == 2) { echo 'current'; } ?>">
+    <p class="topLevelItem borderBox" id="nosNavItem" aria-haspopup="true" aria-owns="nosSubMenu" aria-controls="nosSubMenu" role="button" aria-pressed="false">
+        <?php echo _("Nosotros"); ?>
+    </p>
+    <ul id="nosSubMenu" class="subMenu unstyled" role="group" aria-expanded="false" aria-labelledby="nosNavItem">
+        <li class="<?php if($navCurrentInt == 200) { echo 'current'; } ?>">
+            <a href="/nuevo/nosotros/historia.php" class="displayBlock borderBox" <?php if($navCurrentInt == 200) { echo 'aria-current="page"'; } ?>>
+                <?php echo _("Historia"); ?></a>
+        </li>
+    </ul>
+</li>
