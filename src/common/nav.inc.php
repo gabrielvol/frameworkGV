4148d7369fb8f6ff220bc4cc6053f26a53a3818a
Gabriel Volonte <tampas@gmail.com>
2018-04-05 14:45:44 -0500

    Ajuste en navDrawer, subMenu

--- src/common/nav.inc.php
+++ src/common/nav.inc.php
@@ -1,13 +1,13 @@
 <nav id="nav" class="navMain container pRel" aria-label="<?php echo _("Men&uacute; principal del sitio"); ?>.">
     <button class="hamb navOpen pAbs displayNone displayBlockBT alignCenter upperCase" aria-pressed="false"><?php echo _("Men&uacute;"); ?></button>
     <button class="hamb navClose pAbs indentedText"><?php echo _("Cerrar"); ?></button>
-    <ul class="mainMenu unstyled horizontalCenterAT clearfix displayNoneBT">
+    <ul class="mainMenu displayNoneBT unstyled horizontalCenterAT clearfix">
         <?php            
             include('nav.list.inc.php');
         ?>
     </ul>
     
-    <ul class="mainMenu unstyled clearfix displayNoneAT">
+    <ul class="mainMenu displayNoneAT unstyled clearfix">
         <?php            
             if ($seccionConSub == 1) {
                 echo '<li class="volver pRel hasSub hideAT">
