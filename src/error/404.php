cae7fddb178d66e6a9646d2b5440ba738662c37e
Gabriel Volonte <tampas@gmail.com>
2018-05-05 17:32:40 -0300

    Error page 404

---
+++ src/error/404.php
@@ -0,0 +1,26 @@
+<?php
+    $error = 1;
+    
+    $title = "Error | ";
+    //$titleEN = "UntitledPage";
+    $mdesc = "";
+    
+    $navCurrent = 0;
+    
+    //Gettext variable, debe ir antes del head
+    $idioma = $_GET['l'];
+    
+    include($_SERVER['DOCUMENT_ROOT'] . '/nuevo/common/head.inc.php');
+    include($_SERVER['DOCUMENT_ROOT'] . '/nuevo/common/gettext.inc.php');
+?>
+</head>
+<body>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/nuevo/common/header.inc.php'); ?>
+    <div id="main" class="errorPage pvl" role="main">
+        <h1 class="greyD alignCenter"><?php echo _("P&aacute;gina no encontrada"); ?>.</h1>
+        <p class="errorType pbl grey alignCenter"><?php echo _("Error 404"); ?>.</p>
+        <p class="black alignCenter"><?php echo _("Puede continuar navegando el sitio yendo a la"); ?> <a href="/nuevo/" class="black underline"><?php echo _("p&aacute;gina inicial del sitio"); ?></a><?php echo _(", o a nuestra"); ?> <a href="/nuevo/contacto.php" class="black underline"><?php echo _("p&aacute;gina de contacto"); ?></a>.</p>
+    </div>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/nuevo/common/footer.inc.php'); ?>
+</body>
+</html>
