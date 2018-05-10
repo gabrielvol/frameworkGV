3d82cf1d4ec401da78a530838214e2d4a51f06b5
Gabriel Volonte <tampas@gmail.com>
2018-05-10 17:45:57 -0300

    Creo

---
+++ src/gracias.php
@@ -0,0 +1,21 @@
+<?php
+    $int = 1;
+    $gracias=1;
+    
+    $title = "Gracias";
+    $mdesc = "";
+    
+    $navCurrent = 0;
+    
+    include($_SERVER['DOCUMENT_ROOT'] . '/common/head.inc.php');
+?>
+</head>
+<body>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/common/header.inc.php'); ?>
+    <div id="main" class="int gracias" role="main">
+        <h1 class="alignCenter">Gracias</h1>
+        <p class="alignCenter">Los datos se han enviado correctamente, nos comunicaremos a la brevedad. Muchas Gracias</p>
+    </div>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/common/footer.inc.php'); ?>
+</body>
+</html>
