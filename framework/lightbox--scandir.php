9bd537c2117ff7923d38177f88a0ff4dd16c84e6
Gabriel Volonte <tampas@gmail.com>
2018-05-12 17:45:49 -0300

    Corrección

--- framework/lightbox--scandir.php
+++ framework/lightbox--scandir.php
@@ -34,8 +34,8 @@
             ?>
         </ul>
     </div>
-    <script type="text/javascript" src="/framework/js/lightbox.min.js"></script>
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/framework/common/footer.inc.php'); ?>
+    <script type="text/javascript" src="/framework/js/lightbox.min.js"></script>
     <script type="text/javascript">
         lightbox.option({
             'resizeDuration': 1000,
