b95d7f2bfbda55e6eac311d3fa40c4ba2e9c4b4e
Gabriel Volonte <tampas@gmail.com>
2018-05-13 19:53:17 -0300

    Pop accesible

--- src/common/footer.inc.php
+++ src/common/footer.inc.php
@@ -31,6 +31,16 @@
         
     </div>
 </div>
+
+<div class="popup statusDiv borderBox pal" role="alertdialog" aria-labelledby="alertOK">
+    <div role="document" tabindex="0">
+        <button class="closePop pAbs hoverGrow indentedText">Cerrar</button>
+        <h2 id="alertOK" class="mbm alignCenter"></h2>
+        <p class="">Textoooooo.</p>
+        <p class="">Textoooooo <a href="#">aquí</a>.</p>
+        <button class="popOKdialog bgWhite mtl mha arial bold black alignCenter upperCase">OK</button>
+    </div>
+</div>
 <div class="modalBG displayNone"></div>
 
 <script src="/nuevo/js/_main.js"></script>
