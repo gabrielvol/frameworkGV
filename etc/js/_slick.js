9a81aaa6ec961e4dd02d0cb4f6e35ce9738cddb4
Gabriel Volonte <tampas@gmail.com>
2018-05-22 16:38:52 -0300

    Acomodo comas

--- etc/js/_slick.js
+++ etc/js/_slick.js
@@ -1,53 +1,53 @@
 $(document).ready(function () {
     //Slick Slider
     $('.slick').slick({
-        accessibility: true,
-        infinite: true,
+         accessibility: true
+        ,infinite: true
         
-        dots: true,
-        arrows: false,            
-        fade: true,
+        ,dots: true
+        ,arrows: false
+        ,fade: true
         
-        pauseOnHover: false,
-        pauseOnFocus: false,
-        swipeToSlide: true,
-        focusOnSelect: true,
+        ,pauseOnHover: false
+        ,pauseOnFocus: false
+        ,swipeToSlide: true
+        ,focusOnSelect: true
 
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        lazyLoad: 'ondemand',
+        ,slidesToShow: 3
+        ,slidesToScroll: 3
+        ,lazyLoad: 'ondemand'
 
-        autoplay: true,
-        autoplaySpeed: 5000,
-        speed: 500,
+        ,autoplay: true
+        ,autoplaySpeed: 5000
+        ,speed: 500
 
-        centerMode: true,
-        centerPadding: '60px',
-
-        variableWidth: true,
-        adaptiveHeight: true,
+        ,centerMode: true
+        ,centerPadding: '60px'
         
-        asNavFor: '.sliderNav',
+        ,variableWidth: true
+        ,adaptiveHeight: true
+        
+        ,asNavFor: '.sliderNav'
 
-        responsive: [
+        ,responsive: [
             {breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
+                     slidesToShow: 3
+                    ,slidesToScroll: 3
+                    ,infinite: true
+                    ,dots: true
                 }                
-            },
-            {breakpoint: 600,
+            }
+            ,{breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
+                     slidesToShow: 2
+                    ,slidesToScroll: 2
                 }
-            },
-            {breakpoint: 480,
+            }
+            ,{breakpoint: 480,
                 settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
+                     slidesToShow: 1
+                    ,slidesToScroll: 1
                 }
             }
         ]
