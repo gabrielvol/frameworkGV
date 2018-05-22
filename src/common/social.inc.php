1367fc09d96f4517d10a9c2d9a1f9ddc1a287db6
Gabriel Volonte <tampas@gmail.com>
2018-05-22 14:00:59 -0300

    Accesibilidad en redes

--- src/common/social.inc.php
+++ src/common/social.inc.php
@@ -2,43 +2,43 @@
     <li class="fb">
         <a href="/nuevo/" class="fb displayBlock hoverGrowBig mhaBT indentedText">
             <?php echo _("Seguinos en Facebook"); ?>
-            <img src="/img/soc_fb.svg" class="svg displayBlock" alt="<?php echo _('Facebook'); ?>.">
+            <img src="/img/soc_fb.svg" class="svg displayBlock" alt="<?php echo _('Ir a nuestro perfil en Facebook'); ?>.">
         </a>
     </li>
     <li class="tw">
         <a href="/nuevo/" class="tw displayBlock hoverGrowBig mhaBT indentedText">
             <?php echo _("Seguinos en Twitter"); ?>
-            <img src="/img/soc_tw.svg" class="svg displayBlock" alt="<?php echo _('Twitter'); ?>.">
+            <img src="/img/soc_tw.svg" class="svg displayBlock" alt="<?php echo _('Ir a nuestro perfil en Twitter'); ?>.">
         </a>
     </li>
     <li class="ig">
         <a href="/nuevo/" class="ig displayBlock hoverGrowBig mhaBT indentedText">
             <?php echo _("Seguinos en Instagram"); ?>
-            <img src="/img/soc_ig.svg" class="svg displayBlock" alt="<?php echo _('Instagram'); ?>.">
+            <img src="/img/soc_ig.svg" class="svg displayBlock" alt="<?php echo _('Ir a nuestro perfil en Instagram'); ?>.">
         </a>
     </li>
     <li class="pi">
         <a href="/nuevo/" class="pi displayBlock hoverGrowBig mhaBT indentedText">
             <?php echo _("Seguinos en Pinterest"); ?>
-            <img src="/img/soc_pi.svg" class="svg displayBlock" alt="<?php echo _('Pinterest'); ?>.">
+            <img src="/img/soc_pi.svg" class="svg displayBlock" alt="<?php echo _('Ir a nuestro perfil en Pinterest'); ?>.">
         </a>
     </li>
     <li class="yt">
         <a href="/nuevo/" class="yt displayBlock hoverGrowBig mhaBT indentedText">
             <?php echo _("Mir&aacute; Nuestro Canal en YouTube"); ?>
-            <img src="/img/soc_yt.svg" class="svg displayBlock" alt="<?php echo _('YouTube'); ?>.">
+            <img src="/img/soc_yt.svg" class="svg displayBlock" alt="<?php echo _('Ir a nuestro canal de YouTube'); ?>.">
         </a>
     </li>
     <li class="li">
         <a href="/nuevo/" class="li displayBlock hoverGrowBig mhaBT indentedText">
             <?php echo _("Mir&aacute; nuestro perfil en LinkedIn"); ?>
-            <img src="/img/soc_li.svg" class="svg displayBlock" alt="<?php echo _('LinkedIn'); ?>.">
+            <img src="/img/soc_li.svg" class="svg displayBlock" alt="<?php echo _('Ir a nuestro perfil en LinkedIn'); ?>.">
         </a>
     </li>
     <li class="sp">
         <a href="/nuevo/" class="sp displayBlock hoverGrowBig mhaBT indentedText">
             <?php echo _("Escuchanos en Spotify"); ?>
-            <img src="/img/soc_sp.svg" class="svg displayBlock" alt="<?php echo _('Spotify'); ?>.">
+            <img src="/img/soc_sp.svg" class="svg displayBlock" alt="<?php echo _('Ir a nuestro perfil en Spotify'); ?>.">
         </a>
     </li>
 </ul>
