91c575b429a011db23756b4aa6f300895825be3b
Gabriel Volonte <tampas@gmail.com>
2018-06-01 16:43:51 -0300

    Elimino sugenrencias de form

--- src/contacto.php
+++ src/contacto.php
@@ -32,126 +32,108 @@
 
             <label for="nombre">
                 <span class="labelName"><?php echo _('Nombre'); ?></span>
-                <span class="sug"><?php echo _('Ingrese su nombre'); ?>.</span>
                 <input type="text" name="nombre" id="nombre" class="nombre <?php echo $invalidFieldNombre ?>" value="<?php echo $_POST['nombre']; ?>" placeholder="<?php echo _(''); ?>" <?php echo $autofocusNombre ?>>
                 <span class="formInputErrorMsg <?php echo $errorFormClassNombre ?>"><?php echo $errorBelowInputNombre ?></span>               
             </label>
 
             <label for="apellido">
                 <span class="labelName"><?php echo _('Apellido'); ?></span>
-                <span class="sug"><?php echo _('Ingrese su apellido'); ?>.</span>
                 <input type="text" name="apellido" id="apellido" class="apellido <?php echo $invalidFieldApellido ?>" value="<?php echo $_POST['apellido']; ?>" placeholder="<?php echo _(''); ?>" <?php echo $autofocusApellido ?>>
                 <span class="formInputErrorMsg <?php echo $errorFormClassApellido ?>"><?php echo $errorBelowInputApellido ?></span>             
             </label>
 
             <label for="nombreApe">
                 <span class="labelName"><?php echo _('Nombre y Apellido'); ?></span>
-                <span class="sug"><?php echo _('Ingrese su nombre y apellido'); ?>.</span>
                 <input type="text" name="nombreApe" id="nombreApe" class="nombreApe <?php echo $invalidFieldNombreApe ?>" value="<?php echo $_POST['nombreApe']; ?>" placeholder="<?php echo _(''); ?>" <?php echo $autofocusNombreApe ?>>
                 <span class="formInputErrorMsg <?php echo $errorFormClassNombreApe ?>"><?php echo $errorBelowInputNombreApe ?></span>             
             </label>
 
             <label for="direccion">
                 <span class="labelName"><?php echo _('Direcci&oacute;n'); ?></span>
-                <span class="sug"><?php echo _('Ingrese su direcci&oacute;n'); ?>.</span>
                 <input type="text" name="direccion" id="direccion" class="direccion <?php echo $invalidFieldDireccion ?>" value="<?php echo $_POST['direccion']; ?>" placeholder="<?php echo _(''); ?>" <?php echo $autofocusDireccion ?>>
                 <span class="formInputErrorMsg <?php echo $errorFormClassDireccion ?>"><?php echo $errorBelowInputDireccion ?></span>             
             </label>
 
             <label for="domicilio">
                 <span class="labelName"><?php echo _('Domicilio'); ?></span>
-                <span class="sug"><?php echo _('Ingrese su domicilio'); ?>.</span>
                 <input type="text" name="domicilio" id="domicilio" class="domicilio <?php echo $invalidFieldDomicilio ?>" value="<?php echo $_POST['domicilio']; ?>" placeholder="<?php echo _(''); ?>" <?php echo $autofocusDomicilio ?>>
                 <span class="formInputErrorMsg <?php echo $errorFormClassDomicilio ?>"><?php echo $errorBelowInputDomicilio ?></span>             
             </label>
 
             <label for="localidad">
                 <span class="labelName"><?php echo _('Localidad'); ?></span>
-                <span class="sug"><?php echo _('Ingrese su localidad'); ?>.</span>
                 <input type="text" name="localidad" id="localidad" class="localidad <?php echo $invalidFieldLocalidad ?>" value="<?php echo $_POST['localidad']; ?>" placeholder="<?php echo _(''); ?>" <?php echo $autofocusLocalidad ?>>
                 <span class="formInputErrorMsg <?php echo $errorFormClassLocalidad ?>"><?php echo $errorBelowInputLocalidad ?></span>             
             </label>
 
             <label for="codigopostal">
                 <span class="labelName"><?php echo _('C&oacute;digo postal'); ?></span>
-                <span class="sug"><?php echo _('Ingrese su c&oacute;digo postal'); ?>.</span>
                 <input type="text" name="codigopostal" id="codigopostal" class="codigopostal <?php echo $invalidFieldCodigoPostal ?>" value="<?php echo $_POST['codigopostal']; ?>" placeholder="<?php echo _('Ej.:'); ?> B1846DWF, 1832" <?php echo $autofocusCodigoPostal ?>>
                 <span class="formInputErrorMsg <?php echo $errorFormClassCodigoPostal ?>"><?php echo $errorBelowInputCodigoPostal ?></span>             
             </label>
 
             <label for="provincia">
                 <span class="labelName"><?php echo _('Provincia'); ?></span>
-                <span class="sug"><?php echo _('Ingrese su provincia'); ?>.</span>
                 <input type="text" name="provincia" id="provincia" class="provincia <?php echo $invalidFieldProvincia ?>" value="<?php echo $_POST['provincia']; ?>" placeholder="<?php echo _(''); ?>" <?php echo $autofocusProvincia ?>>
                 <span class="formInputErrorMsg <?php echo $errorFormClassProvincia ?>"><?php echo $errorBelowInputProvincia ?></span>             
             </label>
 
             <label for="pais">
                 <span class="labelName"><?php echo _('Pa&iacute;s'); ?></span>
-                <span class="sug"><?php echo _('Ingrese su pa&iacute;s'); ?>.</span>
                 <input type="text" name="pais" id="pais" class="pais <?php echo $invalidFieldPais ?>" value="<?php echo $_POST['pais']; ?>" placeholder="<?php echo _(''); ?>" <?php echo $autofocusPais ?>>
                 <span class="formInputErrorMsg <?php echo $errorFormClassPais ?>"><?php echo $errorBelowInputPais ?></span>             
             </label>
 
             <label for="telefono">
                 <span class="labelName"><?php echo _('Tel&eacute;fono'); ?></span>
-                <span class="sug"><?php echo _('Ingrese su n&ucaute;mero de tel&eacute;fono'); ?>.</span>
                 <input type="tel" name="telefono" id="telefono" class="telefono <?php echo $invalidFieldTelefono ?>" value="<?php echo $_POST['telefono']; ?>" placeholder="<?php echo _(''); ?>" <?php echo $autofocusTelefono ?>>
                 <span class="formInputErrorMsg <?php echo $errorFormClassTelefono ?>"><?php echo $errorBelowInputTelefono ?></span>             
             </label>
 
             <label for="email">
                 <span class="labelName"><?php echo _('Correo electr&oacute;nico'); ?></span>
-                <span class="sug"><?php echo _('Ingrese su correo electr&oacute;nico'); ?>.</span>
                 <input type="email" name="email" id="email" class="email <?php echo $invalidFieldEmail ?>" value="<?php echo $_POST['email']; ?>" placeholder="<?php echo _(''); ?>" <?php echo $autofocusEmail ?>>
                 <span class="formInputErrorMsg <?php echo $errorFormClassEmail ?>"><?php echo $errorBelowInputEmail ?></span>             
             </label> 
 
             <label for="email">
                 <span class="labelName"><?php echo _('E-mail'); ?></span>
-                <span class="sug"><?php echo _('Ingrese su e-mail'); ?>.</span>
                 <input type="email" name="email" id="email" class="email <?php echo $invalidFieldEmail ?>" value="<?php echo $_POST['email']; ?>" placeholder="<?php echo _(''); ?>" <?php echo $autofocusEmail ?>>
                 <span class="formInputErrorMsg <?php echo $errorFormClassEmail ?>"><?php echo $errorBelowInputEmail ?></span>             
             </label>               
 
             <label for="website">
                 <span class="labelName"><?php echo _('Sitio web'); ?></span>
-                <span class="sug">Debe ingresar su sitio web comenzando con <span>http://www</span>.</span>
                 <input type="url" name="website" id="website" class="website <?php echo $invalidFieldWebsite ?>" value="<?php echo $_POST['website']; ?>" placeholder="<?php echo _('Debe comenzar con http://www'); ?>" <?php echo $autofocusWebsite ?>>
                 <span class="formInputErrorMsg <?php echo $errorFormClassWebsite ?>"><?php echo $errorBelowInputWebsite ?></span>             
             </label>          
 
             <label for="facebook">
                 <span class="labelName"><?php echo _('Perfil de Facebook'); ?></span>
-                <span class="sug">Debe ingresar su sitio web comenzando con <span>http://www.facebook.com/</span>.</span>
                 <input type="url" name="facebook" id="facebook" class="facebook <?php echo $invalidFieldFacebook ?>" value="<?php echo $_POST['facebook']; ?>" placeholder="<?php echo _('Debe comenzar con http://www.facebook.com/'); ?>" <?php echo $autofocusFacebook ?>>
                 <span class="formInputErrorMsg <?php echo $errorFormClassFacebook ?>"><?php echo $errorBelowInputFacebook ?></span>             
             </label>
 
             <label for="empresa">
                 <span class="labelName"><?php echo _('Empresa'); ?></span>
-                <span class="sug"><?php echo _('Ingrese su empresa'); ?>.</span>
                 <input type="text" name="empresa" id="empresa" class="empresa <?php echo $invalidFieldEmpresa ?>" value="<?php echo $_POST['empresa']; ?>" placeholder="<?php echo _(''); ?>" <?php echo $autofocusEmpresa ?>>
                 <span class="formInputErrorMsg <?php echo $errorFormClassEmpresa ?>"><?php echo $errorBelowInputEmpresa ?></span>             
             </label>
 
             <label for="rubro">
                 <span class="labelName"><?php echo _('Rubro'); ?></span>
-                <span class="sug"><?php echo _('Ingrese su rubro'); ?>.</span>
                 <input type="text" name="rubro" id="rubro" class="rubro <?php echo $invalidFieldRubro ?>" value="<?php echo $_POST['rubro']; ?>" placeholder="<?php echo _(''); ?>" <?php echo $autofocusRubro ?>>
                 <span class="formInputErrorMsg <?php echo $errorFormClassRubro ?>"><?php echo $errorBelowInputRubro ?></span>             
             </label>
 
             <label for="cargo">
                 <span class="labelName"><?php echo _('Cargo'); ?></span>
-                <span class="sug"><?php echo _('Ingrese su cargo'); ?>.</span>
                 <input type="text" name="cargo" id="cargo" class="cargo <?php echo $invalidFieldCargo ?>" value="<?php echo $_POST['cargo']; ?>" placeholder="<?php echo _(''); ?>" <?php echo $autofocusCargo ?>>
                 <span class="formInputErrorMsg <?php echo $errorFormClassCargo ?>"><?php echo $errorBelowInputCargo ?></span>             
             </label>
 
             <label for="fecha">
                 <span class="labelName"><?php echo _('Fecha'); ?></span>
-                <span class="sug"><?php echo _('Indique la fecha deseada'); ?>.</span>
                 <input type="date" name="fecha" id="fecha" class="fecha <?php echo $invalidFieldFecha ?>" value="<?php echo $_POST['fecha']; ?>" placeholder="<?php echo _('Seleccione una fecha en el calendario'); ?>" <?php echo $autofocusFecha ?>>
                 <span class="formInputErrorMsg <?php echo $errorFormClassFecha ?>"><?php echo $errorBelowInputFecha ?></span>             
             </label>
