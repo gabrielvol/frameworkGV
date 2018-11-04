f2e573073df855129b3edb02e03105617f40a0a6
Gabriel Volonte <tampas@gmail.com>
2018-11-04 18:15:03 -0300

    Variables Gettext

--- src/common/gettext.inc.php
+++ src/common/gettext.inc.php
@@ -11,10 +11,17 @@
             break;
     }
     // Define el idioma
+    putenv("LANGUAGE=$idioma");
     putenv("LANG=$idioma");
+    putenv("LC_ALL=$idioma");
+    putenv("LC_LANG=$idioma");
+    putenv("LC_LANGUAGE=$idioma");
     setlocale(LC_ALL, $idioma);
 
     // Define la ubicación de los ficheros de traducción
-    bindtextdomain("messages", "locale");
-    textdomain("messages");
+    $locale_filename = "messages";
+    $locale_dir = "./locale";
+    bindtextdomain($locale_filename, $locale_dir);
+    bind_textdomain_codeset($locale_filename, "UTF-8");
+    textdomain($locale_filename);
 ?>
