e399c4168af89ca819dfe747f2298b1ea5b9c2a5
Gabriel Volonte <tampas@gmail.com>
2019-01-14 20:01:19 -0300

    Twitter

--- src/common/variables.inc.php
+++ src/common/variables.inc.php
@@ -2,6 +2,7 @@
     $url = "http://wwwsitecomar";
     $urlActual = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     
+    $urlTW = "https://www.twitter.com/";
     $urlFB = "https://www.facebook.com/usuario";
     $urlIG = "https://www.instagram.com/usuario";
     $urlLI = "https://www.linkedin.com/usuario";
