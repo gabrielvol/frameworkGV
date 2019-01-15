5d6a582d23d82853cb2693ea8ee9696cb4de6393
Gabriel Volonte <tampas@gmail.com>
2019-01-15 13:28:51 -0300

    Separo popups de Footer

--- src/common/footer.inc.php
+++ src/common/footer.inc.php
@@ -28,26 +28,8 @@
         </ul>
     </div> 
 </footer>
-
-<!--Popup-->
-<div class="modal popup displayNone">
-    <button type="button" class="closePop">Cerrar</button>
-    <button type="button" class="closePop"><?php echo _("Cerrar"); ?></button>
-    <div class="modalWrap">
-        
-    </div>
-</div>
-
-<div class="popup statusDiv borderBox pal" role="alertdialog" aria-labelledby="alertOK">
-    <div role="document" tabindex="0">
-        <button type="button" class="closePop pAbs hoverGrow indentedText">Cerrar</button>
-        <h2 id="alertOK" class="mbm alignCenter"></h2>
-        <p class="">Textoooooo.</p>
-        <p class="">Textoooooo <a href="#">aquí</a>.</p>
-        <button type="button" class="popOKdialog bgWhite mtl mha arial bold black alignCenter upperCase">OK</button>
-    </div>
-</div>
-<div class="modalBG displayNone"></div>
+    
+<?php include('popups.inc.php'); ?>
 
 <script src="/nuevo/js/slick.js"></script>
 <!--<script src="/nuevo/js/lightbox.min.js"></script>-->
---
+++ src/common/popups.inc.php
@@ -0,0 +1,18 @@
+<div class="modal popup displayNone">
+    <button type="button" class="closePop">Cerrar</button>
+    <button type="button" class="closePop"><?php echo _("Cerrar"); ?></button>
+    <div class="modalWrap">
+
+    </div>
+</div>
+
+<div class="popup statusDiv borderBox pal" role="alertdialog" aria-labelledby="alertOK">
+    <div role="document" tabindex="0">
+        <button type="button" class="closePop pAbs hoverGrow indentedText">Cerrar</button>
+        <h2 id="alertOK" class="mbm alignCenter"></h2>
+        <p class="">Textoooooo.</p>
+        <p class="">Textoooooo <a href="#">aquí</a>.</p>
+        <button type="button" class="popOKdialog bgWhite mtl mha arial bold black alignCenter upperCase">OK</button>
+    </div>
+</div>
+<div class="modalBG displayNone"></div>
