87eeb35dabb46d9b09e970a50cb73df1295e2880
Gabriel Volonte <tampas@gmail.com>
2019-04-17 13:42:21 -0300

    scrollKeep.js

--- src/common/footer.inc.php
+++ src/common/footer.inc.php
@@ -33,11 +33,9 @@
     
 <?php include('popups.inc.php'); ?>
 
-<script src="/nuevo/js/scrollKeep.js"></script>
 <!--<script src="/nuevo/js/slick.js"></script>-->
 <!--<script src="/nuevo/js/lightbox.min.js"></script>-->
 
 <script src="/nuevo/js/_navDrawer.js"></script>
-<script src="/nuevo/js/_scrollKeep.js"></script>
 <!--<script src="/nuevo/js/_topper.js"></script>-->
 <!--<script src="/nuevo/js/_slick.js"></script>-->
