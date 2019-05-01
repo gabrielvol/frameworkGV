daaac3e74e04908566e4d2d489e93c617c757aa4
Gabriel Volonte <tampas@gmail.com>
2019-04-30 21:05:17 -0300

    Breadcrumbs

---
+++ src/common/breadcrumbs.inc.php
@@ -0,0 +1,87 @@
+<ul class="breadcrumbs unstyled clearfix">
+    <li>Productos</li>
+    <?php        if($navCurrentIntA == 1100): ?>
+    <li><a href="/nuevor/productos/lamparas/">Lámparas</a></li>
+    
+    <?php endif; if($navCurrentIntB == 1110): ?>
+    <li><a href="/nuevor/productos/lamparas/halogenas/">Lámparas Halógenas</a></li>
+    <?php endif; if($navCurrentIntB == 1120): ?>
+    <li><a href="/nuevor/productos/lamparas/led/">Lámparas Led</a></li>
+    <?php endif; if($navCurrentIntB == 1130): ?>
+    <li><a href="/nuevor/productos/lamparas/ledaltapotencia/">Lámparas Led Alta Potencia</a></li>
+    
+    <?php endif; if($navCurrentIntC == 1111): ?>
+    <li>HA55</li>
+    <?php endif; if($navCurrentIntC == 1112): ?>
+    <li>HA53</li>
+    <?php endif; if($navCurrentIntC == 1113): ?>
+    <li>HA51</li>
+    
+    <?php endif; if($navCurrentIntC == 1121): ?>
+    <li>JYA5A5508</li>
+    <?php endif; if($navCurrentIntC == 1122): ?>
+    <li>JYA5A5510</li>
+    
+    <?php endif; if($navCurrentIntC == 1131): ?>
+    <li>TALBH350</li>
+    <?php endif; if($navCurrentIntC == 1132): ?>
+    <li>TALBH340</li>
+    <?php endif; if($navCurrentIntC == 1133): ?>
+    <li>TALBH330</li>
+    <?php endif; if($navCurrentIntC == 1134): ?>
+    <li>TALBH320</li>    
+    
+    <?php endif; if($navCurrentIntA == 1200): ?>
+    <li><a href="/nuevor/productos/plafones/">Plafones</a></li>
+    
+    <?php endif; if($navCurrentIntB == 1210): ?>
+    <li><a href="/nuevor/productos/plafones/deembutir/">De Embutir</a></li>
+    <?php endif; if($navCurrentIntB == 1220): ?>
+    <li><a href="/nuevor/productos/plafones/deaplicar/">De Aplicar</a></li>
+    
+    <?php endif; if($navCurrentIntC == 1211): ?>
+    <li>TALPT124STALPT124R</li>
+    <?php endif; if($navCurrentIntC == 1212): ?>
+    <li>TALPT118STALPT124R</li>
+    <?php endif; if($navCurrentIntC == 1213): ?>
+    <li>TALPT112STALPT124R</li>
+    <?php endif; if($navCurrentIntC == 1214): ?>
+    <li>TALPT16STALPT124R</li>
+    
+    <?php endif; if($navCurrentIntC == 1221): ?>
+    <li>TALPI124STALPI124R</li>
+    <?php endif; if($navCurrentIntC == 1222): ?>
+    <li>TALPI118STALPI118R</li>
+    <?php endif; if($navCurrentIntC == 1223): ?>
+    <li>TALPI112STALPI112R</li>
+    <?php endif; if($navCurrentIntC == 1224): ?>
+    <li>TALPI16STALPI16R</li> 
+    
+    <?php endif; if($navCurrentIntA == 1300): ?>
+    <li><a href="/nuevor/productos/tubosled/">Tubos Led</a></li>   
+    
+    <?php endif; if($navCurrentIntC == 1301): ?>
+    <li>TL600</li>
+    <?php endif; if($navCurrentIntC == 1302): ?>
+    <li>TL1200</li>    
+    
+    <?php endif; if($navCurrentIntA == 1400): ?>
+    <li><a href="/nuevor/productos/listontubosled/">Listón para Tubo Led</a></li>   
+    
+    <?php endif; if($navCurrentIntC == 1401): ?>
+    <li>LL600</li>
+    <?php endif; if($navCurrentIntC == 1402): ?>
+    <li>LL1200</li>
+    
+    <?php endif; if($navCurrentIntA == 1500): ?>
+    <li><a href="/nuevor/productos/listontubosfluorescente/">Listón para Tubo Fluorescente</a></li>
+    
+    <?php endif; if($navCurrentIntC == 1501): ?>
+    <li>LFC120</li>
+    <?php endif; if($navCurrentIntC == 1502): ?>
+    <li>LFC130</li>
+    <?php endif; if($navCurrentIntC == 1503): ?>
+    <li>LFC140</li>    
+    
+    <?php endif; ?>
+</ul>
