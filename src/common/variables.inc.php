dfdd4212c6a6b0e70836dc27bee57b75509698ee
Gabriel Volonte <tampas@gmail.com>
2019-05-04 16:35:01 -0300

    WhatsApp WA me

--- src/common/variables.inc.php
+++ src/common/variables.inc.php
@@ -7,13 +7,14 @@
     $dateThisYear = (int)date('Y');
     
     // Social
-    $urlTW = "https://www.twitter.com/";
-    $urlFB = "https://www.facebook.com/usuario";
-    $urlIG = "https://www.instagram.com/usuario";
-    $urlLI = "https://www.linkedin.com/usuario";
-    $urlWA = "https://api.whatsapp.com/send?phone=5491100001111";
-    $urlEM = "correo@servidor.com";
-    $urlTO = "mailto:".$urlEM;
-    $urlGM = "https://goo.gl/maps/aaaaaaaaaaaa";
-    $urlYT = "https://www.youtube.com/user/ffffffffffffff";    
+    $urlTW      = "https://www.twitter.com/";
+    $urlFB      = "https://www.facebook.com/usuario";
+    $urlIG      = "https://www.instagram.com/usuario";
+    $urlLI      = "https://www.linkedin.com/usuario";
+    $urlWA      = "https://api.whatsapp.com/send?phone=5491100001111";
+    $urlWAme    = "https://wa.me/5491100001111?text=I'm%20interested%20in%20your%20car%20for%20sale";
+    $urlEM      = "correo@servidor.com";
+    $urlTO      = "mailto:".$urlEM;
+    $urlGM      = "https://goo.gl/maps/aaaaaaaaaaaa";
+    $urlYT      = "https://www.youtube.com/user/ffffffffffffff";    
 ?>
