f0b73a950e4edcd6c6b32c33aa6e66c7c6c1645d
Gabriel Volonte <tampas@gmail.com>
2019-05-04 18:24:20 -0300

    Breadcrumbs

--- src/common/breadcrumbs.inc.php
+++ src/common/breadcrumbs.inc.php
@@ -1,87 +1,89 @@
-<ul class="breadcrumbs unstyled clearfix">
-    <li>Productos</li>
-    <?php        if($navCurrentIntA == 1100): ?>
-    <li><a href="/nuevor/productos/lamparas/">Lámparas</a></li>
-    
-    <?php endif; if($navCurrentIntB == 1110): ?>
-    <li><a href="/nuevor/productos/lamparas/halogenas/">Lámparas Halógenas</a></li>
-    <?php endif; if($navCurrentIntB == 1120): ?>
-    <li><a href="/nuevor/productos/lamparas/led/">Lámparas Led</a></li>
-    <?php endif; if($navCurrentIntB == 1130): ?>
-    <li><a href="/nuevor/productos/lamparas/ledaltapotencia/">Lámparas Led Alta Potencia</a></li>
-    
-    <?php endif; if($navCurrentIntC == 1111): ?>
-    <li>HA55</li>
-    <?php endif; if($navCurrentIntC == 1112): ?>
-    <li>HA53</li>
-    <?php endif; if($navCurrentIntC == 1113): ?>
-    <li>HA51</li>
-    
-    <?php endif; if($navCurrentIntC == 1121): ?>
-    <li>JYA5A5508</li>
-    <?php endif; if($navCurrentIntC == 1122): ?>
-    <li>JYA5A5510</li>
-    
-    <?php endif; if($navCurrentIntC == 1131): ?>
-    <li>TALBH350</li>
-    <?php endif; if($navCurrentIntC == 1132): ?>
-    <li>TALBH340</li>
-    <?php endif; if($navCurrentIntC == 1133): ?>
-    <li>TALBH330</li>
-    <?php endif; if($navCurrentIntC == 1134): ?>
-    <li>TALBH320</li>    
-    
-    <?php endif; if($navCurrentIntA == 1200): ?>
-    <li><a href="/nuevor/productos/plafones/">Plafones</a></li>
-    
-    <?php endif; if($navCurrentIntB == 1210): ?>
-    <li><a href="/nuevor/productos/plafones/deembutir/">De Embutir</a></li>
-    <?php endif; if($navCurrentIntB == 1220): ?>
-    <li><a href="/nuevor/productos/plafones/deaplicar/">De Aplicar</a></li>
-    
-    <?php endif; if($navCurrentIntC == 1211): ?>
-    <li>TALPT124STALPT124R</li>
-    <?php endif; if($navCurrentIntC == 1212): ?>
-    <li>TALPT118STALPT124R</li>
-    <?php endif; if($navCurrentIntC == 1213): ?>
-    <li>TALPT112STALPT124R</li>
-    <?php endif; if($navCurrentIntC == 1214): ?>
-    <li>TALPT16STALPT124R</li>
-    
-    <?php endif; if($navCurrentIntC == 1221): ?>
-    <li>TALPI124STALPI124R</li>
-    <?php endif; if($navCurrentIntC == 1222): ?>
-    <li>TALPI118STALPI118R</li>
-    <?php endif; if($navCurrentIntC == 1223): ?>
-    <li>TALPI112STALPI112R</li>
-    <?php endif; if($navCurrentIntC == 1224): ?>
-    <li>TALPI16STALPI16R</li> 
-    
-    <?php endif; if($navCurrentIntA == 1300): ?>
-    <li><a href="/nuevor/productos/tubosled/">Tubos Led</a></li>   
-    
-    <?php endif; if($navCurrentIntC == 1301): ?>
-    <li>TL600</li>
-    <?php endif; if($navCurrentIntC == 1302): ?>
-    <li>TL1200</li>    
-    
-    <?php endif; if($navCurrentIntA == 1400): ?>
-    <li><a href="/nuevor/productos/listontubosled/">Listón para Tubo Led</a></li>   
-    
-    <?php endif; if($navCurrentIntC == 1401): ?>
-    <li>LL600</li>
-    <?php endif; if($navCurrentIntC == 1402): ?>
-    <li>LL1200</li>
-    
-    <?php endif; if($navCurrentIntA == 1500): ?>
-    <li><a href="/nuevor/productos/listontubosfluorescente/">Listón para Tubo Fluorescente</a></li>
-    
-    <?php endif; if($navCurrentIntC == 1501): ?>
-    <li>LFC120</li>
-    <?php endif; if($navCurrentIntC == 1502): ?>
-    <li>LFC130</li>
-    <?php endif; if($navCurrentIntC == 1503): ?>
-    <li>LFC140</li>    
-    
-    <?php endif; ?>
-</ul>
+<nav class="breadcrumbs">
+    <ul class="unstyled clearfix">
+        <li>Productos</li>
+        <?php        if($navCurrentIntA == 1100): ?>
+        <li class="cat"><a href="/clientes/everpower/productos/lamparas/">Lámparas</a></li>
+
+        <?php endif; if($navCurrentIntB == 1110): ?>
+        <li class="cat"><a href="/clientes/everpower/productos/lamparas/halogenas/">Lámparas Halógenas</a></li>
+        <?php endif; if($navCurrentIntB == 1120): ?>
+        <li class="cat"><a href="/clientes/everpower/productos/lamparas/led/">Lámparas Led</a></li>
+        <?php endif; if($navCurrentIntB == 1130): ?>
+        <li class="cat"><a href="/clientes/everpower/productos/lamparas/led-alta-potencia/">Lámparas Led Alta Potencia</a></li>
+
+        <?php endif; if($navCurrentIntC == 1111): ?>
+        <li class="item">HA55</li>
+        <?php endif; if($navCurrentIntC == 1112): ?>
+        <li class="item">HA53</li>
+        <?php endif; if($navCurrentIntC == 1113): ?>
+        <li class="item">HA51</li>
+
+        <?php endif; if($navCurrentIntC == 1121): ?>
+        <li class="item">JY-A5-A55-08</li>
+        <?php endif; if($navCurrentIntC == 1122): ?>
+        <li class="item">JY-A5-A55-10</li>
+
+        <?php endif; if($navCurrentIntC == 1131): ?>
+        <li class="item">TALBH3-50</li>
+        <?php endif; if($navCurrentIntC == 1132): ?>
+        <li class="item">TALBH3-40</li>
+        <?php endif; if($navCurrentIntC == 1133): ?>
+        <li class="item">TALBH3-30</li>
+        <?php endif; if($navCurrentIntC == 1134): ?>
+        <li class="item">TALBH3-20</li>    
+
+        <?php endif; if($navCurrentIntA == 1200): ?>
+        <li class="cat"><a href="/clientes/everpower/productos/plafones/">Plafones</a></li>
+
+        <?php endif; if($navCurrentIntB == 1210): ?>
+        <li class="cat"><a href="/clientes/everpower/productos/plafones/de-embutir/">De Embutir</a></li>
+        <?php endif; if($navCurrentIntB == 1220): ?>
+        <li class="cat"><a href="/clientes/everpower/productos/plafones/de-aplicar/">De Aplicar</a></li>
+
+        <?php endif; if($navCurrentIntC == 1211): ?>
+        <li class="item">TALPT1-24S / TALPT1-24R</li>
+        <?php endif; if($navCurrentIntC == 1212): ?>
+        <li class="item">TALPT1-18S / TALPT1-24R</li>
+        <?php endif; if($navCurrentIntC == 1213): ?>
+        <li class="item">TALPT1-12S / TALPT1-24R</li>
+        <?php endif; if($navCurrentIntC == 1214): ?>
+        <li class="item">TALPT1-6S / TALPT1-24R</li>
+
+        <?php endif; if($navCurrentIntC == 1221): ?>
+        <li class="item">TALPI1-24S / TALPI1-24R</li>
+        <?php endif; if($navCurrentIntC == 1222): ?>
+        <li class="item">TALPI1-18S / TALPI1-18R</li>
+        <?php endif; if($navCurrentIntC == 1223): ?>
+        <li class="item">TALPI1-12S / TALPI1-12R</li>
+        <?php endif; if($navCurrentIntC == 1224): ?>
+        <li class="item">TALPI1-6S / TALPI1-6R</li> 
+
+        <?php endif; if($navCurrentIntA == 1300): ?>
+        <li class="cat"><a href="/clientes/everpower/productos/tubos-led/">Tubos Led</a></li>   
+
+        <?php endif; if($navCurrentIntC == 1301): ?>
+        <li class="item">TL600</li>
+        <?php endif; if($navCurrentIntC == 1302): ?>
+        <li class="item">TL1200</li>    
+
+        <?php endif; if($navCurrentIntA == 1400): ?>
+        <li class="cat"><a href="/clientes/everpower/productos/listones-tubos-led/">Listones para Tubos Led</a></li>   
+
+        <?php endif; if($navCurrentIntC == 1401): ?>
+        <li class="item">LL600</li>
+        <?php endif; if($navCurrentIntC == 1402): ?>
+        <li class="item">LL1200</li>
+
+        <?php endif; if($navCurrentIntA == 1500): ?>
+        <li class="cat"><a href="/clientes/everpower/productos/listones-tubos-fluorescentes/">Listones para Tubos Fluorescentes</a></li>
+
+        <?php endif; if($navCurrentIntC == 1501): ?>
+        <li class="item">LFC120</li>
+        <?php endif; if($navCurrentIntC == 1502): ?>
+        <li class="item">LFC130</li>
+        <?php endif; if($navCurrentIntC == 1503): ?>
+        <li class="item">LFC140</li>    
+
+        <?php endif; ?>
+    </ul>
+</nav>
