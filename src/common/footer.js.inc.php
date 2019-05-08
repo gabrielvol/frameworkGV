cf71656dc9e2b3e613facfcc462421cf9e791363
Gabriel Volonte <tampas@gmail.com>
2019-05-07 21:38:55 -0300

    _lightbox.js

--- src/common/footer.js.inc.php
+++ src/common/footer.js.inc.php
@@ -1,10 +1,12 @@
 <!--<script src="/nuevo/js/slick.js"></script>-->
+<!--<script src="/nuevo/js/_slick.js"></script>-->
+
 <!--<script src="/nuevo/js/lightbox.min.js"></script>-->
+<!--<script src="/nuevo/js/_lightbox.js"></script>-->
 
 <script src="/nuevo/js/_global.js"></script>
 <script src="/nuevo/js/_navDrawer.js"></script>
 <!--<script src="/nuevo/js/_topper.js"></script>-->
-<!--<script src="/nuevo/js/_slick.js"></script>-->
 
 <?php if($navCurrent == 0): // Contacto ?> 
 <!--<script src="/nuevo/js/scrollKeep.js"></script>-->
@@ -12,7 +14,7 @@
 <!--<script src="/nuevo/js/_forms.js"></script>-->
 
 <?php endif; if($navCurrent == 0): ?>
-<!--<script src="/clientes/nuevo/js/scrollKeep.js"></script>-->
-<!--<script src="/clientes/nuevo/js/_scrollKeep.js"></script>-->
+<!--<script src="/clientes/nuevo/js/aaaaaa.js"></script>-->
+<!--<script src="/clientes/nuevo/js/_aaaaaa.js"></script>-->
 
 <?php endif; ?>
