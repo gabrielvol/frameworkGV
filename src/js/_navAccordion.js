33ba3eddc4cc321a04b31f90f3ee00f282e98437
Gabriel Volonte <tampas@gmail.com>
2019-05-07 23:05:36 -0300

    Nav Accordion .js, clean

--- src/js/_navAccordion.js
+++ src/js/_navAccordion.js
@@ -3,25 +3,13 @@ $(document).ready(function () {
         $(this).addClass('clicked');
         $(this).attr('aria-pressed', 'true');
         
-//        $(this).closest('#header').find('.social').addClass('clicked');
         $(this).closest('#nav').find('.mainMenu').addClass('open');
-//        $(this).closest('#nav').find('.modalNav').show( '2000', 'swing', function() {
-//            $(this).closest('#header').find('.logoH').addClass('clicked');
-//            $(this).closest('#header').find('.lang').addClass('clicked');
-//            $(this).closest('#nav').find('.navClose').addClass('clicked');
-//        });
         $(this).closest('#nav').find('.navClose').addClass('clicked');
     });
 
     $('.liNoLink').click(function () {
         $(this).closest('#nav').find('.navClose').removeClass('clicked');
-//        $(this).closest('#header').find('.logoH').removeClass('clicked');
-//        $(this).closest('#header').find('.socHeader').removeClass('clicked');
-//        $(this).closest('#header').find('.lang').removeClass('clicked');
-//        $(this).closest('#nav').find('.modalNav').hide('fast', 'swing');
         $(this).closest('#nav').find('.mainMenu').removeClass('open');
-//        $(this).closest('#nav').find('.subMenu').removeClass('open');
-//        $(this).closest('#nav').find('.subMenu').attr('aria-expanded', 'false');
         $(this).closest('#nav').find('.topLevelItem').removeClass('open');
         $(this).closest('#nav').find('.topLevelItem').attr('aria-pressed', function (i, attr) {
             return attr === 'true' ? 'false' : 'true';
@@ -32,13 +20,7 @@ $(document).ready(function () {
 
     $('.navClose').click(function () {
         $(this).removeClass('clicked');
-//        $(this).closest('#header').find('.logoH').removeClass('clicked');
-//        $(this).closest('#header').find('.social').removeClass('clicked');
-//        $(this).closest('#header').find('.lang').removeClass('clicked');
-//        $(this).closest('#nav').find('.modalNav').hide('fast', 'swing');
         $(this).closest('#nav').find('.mainMenu').removeClass('open');
-//        $(this).closest('#nav').find('.subMenu').removeClass('open');
-//        $(this).closest('#nav').find('.subMenu').attr('aria-expanded', 'false');
         $(this).closest('#nav').find('.topLevelItem').removeClass('open');
         $(this).closest('#nav').find('.topLevelItem').attr('aria-pressed', function (i, attr) {
             return attr === 'true' ? 'false' : 'true';
@@ -49,20 +31,12 @@ $(document).ready(function () {
 
     $('.topLevelItem').click(function () {
         $(this).toggleClass('open');
-//        $(this).closest('.hasSub').find('.subMenu').toggleClass('open');
         $(this).attr('aria-pressed', function (i, attr) {
             return attr === 'true' ? 'false' : 'true';
         });
         $(this).closest('.hasSub').find('.subMenu').attr('aria-expanded', function (i, attr) {
             return attr === 'true' ? 'false' : 'true';
         });
-//        $(this).closest('.hasSub').find('.subMenu').toggleClass('open');
-    });
-       
-    $('.hasSub p').click(function () {
-//        $(this).closest('.hasSub').toggleClass('openSubTTLO');
-////        $(this).closest('.mainMenu').toggleClass('openSubTTLO');
-//        $(this).closest('.hasSub').find('.subMenu').toggleClass('openSubTTLO');
     });
        
     $('.mainNav > li').click(function () {
