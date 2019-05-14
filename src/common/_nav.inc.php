40607e73320281084f1ace04ec676b010c6a8818
Gabriel Volonte <tampas@gmail.com>
2019-05-13 22:19:14 -0300

    Media queries independientes para Nav

--- src/common/_nav.inc.php
+++ src/common/_nav.inc.php
@@ -1,13 +1,13 @@
 <nav id="nav" class="navMain pRel" aria-label="<?php echo _("Men&uacute; principal del sitio"); ?>.">
-    <button type="button" class="hamb navOpen pAbs displayNone displayBlockBT alignCenter upperCase" aria-pressed="false"><?php echo _("Men&uacute;"); ?></button>
+    <button type="button" class="hamb navOpen pAbs displayBlockBT alignCenter upperCase" aria-pressed="false"><?php echo _("Men&uacute;"); ?></button>
     <button type="button" class="hamb navClose pAbs indentedText"><?php echo _("Cerrar"); ?></button>
-    <ul class="mainMenu displayNoneBT unstyled clearfix horizontalCenterAT">
+    <ul class="mainMenu navAT unstyled clearfix">
         <?php include('nav.list.inc.php'); ?>
     </ul>   
     
     <?php
         if($navDrawer == 1){
-            echo '<ul class="mainMenu displayNoneAT unstyled clearfix">';
+            echo '<ul class="mainMenu navDrawerMainUL unstyled clearfix">';
 
             if ($seccionConSub == 1) {
                 echo '<li class="backMenu pRel hasSub hideAT">
