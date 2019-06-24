629c67b5cf83d047b43f04429abd8d2d55d081b4
Gabriel Volonte <tampas@gmail.com>
2019-06-24 15:24:43 -0300

    Agrego variables para descarga de archivos

--- src/common/_variables.inc.php
+++ src/common/_variables.inc.php
@@ -54,5 +54,19 @@
     // NAVIGATION
     $navDrawer_act              = 1;
     $navAcc_act                 = 0;
-    $navHorizontalCenter_AT_act = 0;
+    $navHorizontalCenter_AT_act = 0;   
+    
+    // -------------------------------------------------------------------------
+    // FILE DOWNLOADING 
+    
+    // section_section   
+    $file_pdf01 = 'href="/files/archivopdf01.pdf" download="titulo_pricipal-titulo_secundario.pdf"';
+    
+    $file_href______pdf02 = 'href="/files/archivopdf02.pdf"';
+    $file_download__pdf02 = 'download="titulo_pricipal-titulo_secundario.pdf"';
+    
+    // section_section -- Gettext
+    $file_href______pdf03 = '/files/archivopdf02.pdf';
+    $file_download__pdf03 = 'titulo_pricipal-titulo_secundario.pdf';
+    $file_hrefDownload_pdf03 = 'href="'.$file_href______pdf03.'" download="'.$file_download__pdf03.'"';
 ?>
