d8776855f664ef1e71b3443ca66a887e936e0a29
Gabriel Volonte <tampas@gmail.com>
2019-06-26 19:52:35 -0300

    Fix

--- src/common/_google.youTube.inc.php
+++ src/common/_google.youTube.inc.php
@@ -1,3 +1,3 @@
-<div class="youTubeVideo">
+<div class="gooYTVideo">
     <iframe src="https://www.youtube.com/embed/aaa222bbbccc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" class="displayBlock" allowfullscreen></iframe>
 </div>
