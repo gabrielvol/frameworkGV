8c1685d8bf01363c7da6fadb33893f29f407328a
Gabriel Volonte <tampas@gmail.com>
2019-07-22 16:45:45 -0300

    Company Address en dos partes

--- src/common/_variables.inc.php
+++ src/common/_variables.inc.php
@@ -21,8 +21,14 @@
     $site_desc_global_GB  = ""; 
     
         //-- Address
-    $company_address_main       = "aaaaaaaaaaaaaaaaaaaaaaaaaa";
-    $company_address_main_GB    = "bbbbbbbbbbbbbbbbbbbbbbbbbb  Autonomous City of Buenos Aires";
+    $company_address_main_a       = "aaaaaaaaaaaaaaaaaaaaaaaaaa";
+    $company_address_main_a_GB    = "bbbbbbbbbbbbbbbbbbbbbbbbbb  Autonomous City of Buenos Aires";
+    
+    $company_address_main_b       = "aaaaaaaaaaaaaaaaaaaaaaaaaa";
+    $company_address_main_b_GB    = "bbbbbbbbbbbbbbbbbbbbbbbbbb  Autonomous City of Buenos Aires";
+    
+    $company_address_main_full    = $company_address_main_a.' '.$company_address_main_b;
+    $company_address_main_full_GB = $company_address_main_a_GB.' '.$company_address_main_b_GB;
     
         //-- Tels
     $company_tel_main              = "12345678";
