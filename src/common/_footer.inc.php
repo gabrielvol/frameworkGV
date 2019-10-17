cb05272b18c4f079afcecc3220c3667935237e38
Gabriel Volonte <tampas@gmail.com>
2019-10-17 10:40:54 -0300

    Reescribo clases en Footer

--- src/common/_footer.inc.php
+++ src/common/_footer.inc.php
@@ -3,15 +3,15 @@
         
     <img src="/nuevo/img/logo.svg" class="logoF displayBlock" alt="<?php echo $company_name_full ?>.">
     
-    <div class="logoFooter borderBox">
+    <div class="logo_footer borderBox">
         <img src="/nuevo/img/logo.svg" class="logoF displayBlock" alt="<?php echo $company_name_full ?>.">
     </div>
-    <div class="txtFooter borderBox">
-        <p class="gooAddress">
+    <div class="txt_footer borderBox">
+        <p class="address_footer_goo">
             <a href="<?php echo $social_googleMap_url ?>" target="_blank"><?php if($gettext_idioma == 'pt_BR'){ echo $company_address_main_full_PT; } else if($gettext_idioma == 'en_GB') { echo $company_address_main_full_GB; } else { echo $company_address_main_full; } ?></a>
         </p>
-        <p><?php echo _("Tel&eacute;fono"); ?>: <?php echo $company_tel_main_formatted ?></p>
-        <p><?php echo _("Correo electr&oacute;nico"); ?>: <a href="<?php echo $social_mailto_address_A ?>"><?php echo $social_email_address_A ?></a></p>
+        <p class="tel_footer"><?php echo _("Tel&eacute;fono"); ?>: <?php echo $company_tel_main_formatted ?></p>
+        <p class="mail_footer"><?php echo _("Correo electr&oacute;nico"); ?>: <a href="<?php echo $social_mailto_address_A ?>"><?php echo $social_email_address_A ?></a></p>
     </div>  
     
     <nav class="navFooter pRel" aria-label="<?php echo _("Men&uacute; principal del sitio"); ?>.">
@@ -22,7 +22,7 @@
     
     <?php include('social.inc.php'); ?>
     
-    <p class="gooAddress">
+    <p class="address_footer_goo">
         <a href="<?php echo $social_googleMap_url ?>" target="_blank"><?php if($gettext_idioma == 'pt_BR'){ echo $company_address_main_full_PT; } else if($gettext_idioma == 'en_GB') { echo $company_address_main_full_GB; } else { echo $company_address_main_full; } ?></a>
     </p>
     
@@ -30,9 +30,9 @@
     
     <p><?php echo _("Correo electr&oacute;nico"); ?>: <a href="<?php echo $social_mailto_address_A ?>"><?php echo $social_email_address_A ?></a></p>
     
-    <p class="footerCopy">&copy; <?php echo $dateThisYear." - ".$company_name_full ?></p>
+    <p class="copy_footer">&copy; <?php echo $dateThisYear." - ".$company_name_full ?></p>
     
-    <p class="footerCopy">
+    <p class="copy_footer">
         &copy;
         <?php
             $fromYear = 2010;
@@ -40,11 +40,11 @@
         ?>
     </p>
     
-    <div class="footerLegal">
+    <div class="legal_footer">
         <ul class="horizontal clearfix containerAT">
-            <li class="copy">&copy; <?php echo $dateThisYear." - ".$company_name_full ?></li>
-            <li class="tos"><a href="/legal/" class="displayBlock"><?php echo _("Privacidad y Condiciones"); ?></a></li>
-            <li class="stu"><?php echo _("Web Design By"); ?><a href="https://www.instagram.com/xxxxxx/" class="displayBlock alignCenterBT">Estudio De Diseño</a></li>
+            <li class="legal_copy">&copy; <?php echo $dateThisYear." - ".$company_name_full ?></li>
+            <li class="legal_tos"><a href="/legal/" class="displayBlock"><?php echo _("Privacidad y Condiciones"); ?></a></li>
+            <li class="legal_studio"><?php echo _("Web Design By"); ?><a href="https://www.instagram.com/xxxxxx/" class="displayBlock alignCenterBT">Estudio De Diseño</a></li>
         </ul>
     </div> 
 </footer>
