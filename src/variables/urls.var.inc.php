0cc88620ce96a7a58c7a84c88b53faadf64a3246
Gabriel Volonte <tampas@gmail.com>
2019-10-21 08:59:59 -0300

    Fix

--- src/variables/urls.var.inc.php
+++ src/variables/urls.var.inc.php
@@ -5,12 +5,14 @@
     $url_global     = "http://www.".$domain_global;
     $url_openGraph  = "http://www.".$domain_global;
     
-    $url-seccion        = "/clientes/charly/seccion.php";
-    $url_contacto       = "/clientes/charly/contacto.php";
+    $url-seccion        = "/nuevo/seccion.php";
+    $url_index          = "/nuevo/";
+    $url_contacto       = "/nuevo/contacto.php";
     $url_contacto_full  = $url_global . $url_contacto;
     
-    $href-seccion  = 'href="'.$url-seccion.'"';
+    $href_index    = 'href="'.$url_index.'"';
     $href_contacto = 'href="'.$url_contacto.'"';
+    $href-seccion  = 'href="'.$url-seccion.'"';
     
     $url_actual     = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 ?>
