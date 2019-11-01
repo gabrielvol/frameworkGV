d4ea7683f0c3d7e6a120fbe03ac1858f2d072fb1
Gabriel Volonte <tampas@gmail.com>
2019-11-01 19:59:17 -0300

    soc_ => social_

--- src/common/_social.inc.php
+++ src/common/_social.inc.php
@@ -1,93 +1,93 @@
 <ul class="social horizontal clearfix">
-    <li class="soc_fb">
+    <li class="social_fb">
         <a href="<?php echo $social_facebook_url ?>" class="fb displayBlock hoverGrowL indentedText">
             <?php echo _("Seguinos en Facebook"); ?>
-            <img src="/nuevo/img/soc_fb.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en Facebook'); ?>.">
+            <img src="/nuevo/img/social_fb.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en Facebook'); ?>.">
         </a>
     </li>
-    <li class="soc_sk">
+    <li class="social_sk">
         <a href="/nuevo/" class="tw displayBlock hoverGrowL indentedText">
             <?php echo _("Contactanos en Skype"); ?>
-            <img src="/nuevo/img/soc_sk.svg" class="displayBlock" alt="<?php echo _('Comunicate con nosotros por Skype'); ?>.">
+            <img src="/nuevo/img/social_sk.svg" class="displayBlock" alt="<?php echo _('Comunicate con nosotros por Skype'); ?>.">
         </a>
     </li>
-    <li class="soc_tw">
+    <li class="social_tw">
         <a href="/nuevo/" class="tw displayBlock hoverGrowL indentedText">
             <?php echo _("Seguinos en Twitter"); ?>
-            <img src="/nuevo/img/soc_tw.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en Twitter'); ?>.">
+            <img src="/nuevo/img/social_tw.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en Twitter'); ?>.">
         </a>
     </li>
-    <li class="soc_vi">
+    <li class="social_vi">
         <a href="/nuevo/" class="tw displayBlock hoverGrowL indentedText">
             <?php echo _("Mir&aacute; Nuestro Canal en Vimeo"); ?>
-            <img src="/nuevo/img/soc_vi.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro canal de Vimeo'); ?>.">
+            <img src="/nuevo/img/social_vi.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro canal de Vimeo'); ?>.">
         </a>
     </li>
-    <li class="soc_ig">
+    <li class="social_ig">
         <a href="<?php echo $social_instagram_url ?>" class="ig displayBlock hoverGrowL indentedText">
             <?php echo _("Seguinos en Instagram"); ?>
-            <img src="/nuevo/img/soc_ig.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en Instagram'); ?>.">
+            <img src="/nuevo/img/social_ig.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en Instagram'); ?>.">
         </a>
     </li>
-    <li class="soc_pi">
+    <li class="social_pi">
         <a href="/nuevo/" class="pi displayBlock hoverGrowL indentedText">
             <?php echo _("Seguinos en Pinterest"); ?>
-            <img src="/nuevo/img/soc_pi.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en Pinterest'); ?>.">
+            <img src="/nuevo/img/social_pi.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en Pinterest'); ?>.">
         </a>
     </li>
-    <li class="soc_yt">
+    <li class="social_yt">
         <a href="/nuevo/" class="yt displayBlock hoverGrowL indentedText">
             <?php echo _("Mir&aacute; Nuestro Canal en YouTube"); ?>
-            <img src="/nuevo/img/soc_yt.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro canal de YouTube'); ?>.">
+            <img src="/nuevo/img/social_yt.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro canal de YouTube'); ?>.">
         </a>
     </li>
-    <li class="soc_li">
+    <li class="social_li">
         <a href="<?php echo $social_linkedin_url ?>" class="li displayBlock hoverGrowL indentedText">
             <?php echo _("Mir&aacute; nuestro perfil en LinkedIn"); ?>
-            <img src="/nuevo/img/soc_li.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en LinkedIn'); ?>.">
+            <img src="/nuevo/img/social_li.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en LinkedIn'); ?>.">
         </a>
     </li>
-    <li class="soc_sp">
+    <li class="social_sp">
         <a href="/nuevo/" class="sp displayBlock hoverGrowL indentedText">
             <?php echo _("Escuchanos en Spotify"); ?>
-            <img src="/nuevo/img/soc_sp.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en Spotify'); ?>.">
+            <img src="/nuevo/img/social_sp.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en Spotify'); ?>.">
         </a>
     </li>
-    <li class="soc_sc">
+    <li class="social_sc">
         <a href="/nuevo/" class="sp displayBlock hoverGrowL indentedText">
             <?php echo _("Nuestro perfil en Snapchat"); ?>
-            <img src="/nuevo/img/soc_sc.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en Snapchat'); ?>.">
+            <img src="/nuevo/img/social_sc.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en Snapchat'); ?>.">
         </a>
     </li>
-    <li class="soc_wa">
+    <li class="social_wa">
         <a href="<?php echo $social_whatsapp_chatOpen ?>" class="wa conTel displayBlock">11 0000 2222</a> 
         
         <a href="<?php echo $social_whatsapp_chatOpen ?>" class="wa displayBlock hoverGrowL indentedText"><?php echo _('Nuestro contacto en WhatsApp'); ?>.</a> 
         
         <a href="<?php echo $social_whatsapp_chatOpen ?>" class="wa displayBlock hoverGrowL indentedText">
             <?php echo _("Nuestro contacto en WhatsApp"); ?>
-            <img src="/nuevo/img/soc_wa.svg" class="displayBlock" alt="<?php echo _('Nuestro contacto en WhatsApp'); ?>.">
+            <img src="/nuevo/img/social_wa.svg" class="displayBlock" alt="<?php echo _('Nuestro contacto en WhatsApp'); ?>.">
         </a>
     </li>
-    <li class="soc_em">
+    <li class="social_em">
         <a href="<?php echo $social_mailto_address_A ?>" class="em displayBlock hoverGrowL indentedText">
             <?php echo _("Cont&aacute;ctenos por Correo Electr&oacute;nico"); ?>
-            <img src="/nuevo/img/soc_em.svg" class="displayBlock" alt="<?php echo _('Cont&aacute;ctenos por Correo Electr&oacute;nico'); ?>.">
+            <img src="/nuevo/img/social_em.svg" class="displayBlock" alt="<?php echo _('Cont&aacute;ctenos por Correo Electr&oacute;nico'); ?>.">
         </a>
     </li>
-    <li class="soc_em">
+    <li class="social_em">
         <a href="<?php echo $social_mailto_address_A ?>" class="displayBlock"><?php echo $social_email_address_A ?></a>
     </li>
-    <li class="soc_ta">
+    <li class="social_ta">
         <a href="/nuevo/" class="ta displayBlock hoverGrowL indentedText">
             <?php echo _("Nuestro perfil en TripAdvisor"); ?>
-            <img src="/nuevo/img/soc_ta.svg" class="displayBlock" alt="<?php echo _('Nuestro perfil en TripAdvisor'); ?>.">
+            <img src="/nuevo/img/social_ta.svg" class="displayBlock" alt="<?php echo _('Nuestro perfil en TripAdvisor'); ?>.">
         </a>
     </li>
-    <li class="soc_go">
+    <li class="social_go">
         <a href="/nuevo/" class="go displayBlock hoverGrowL indentedText">
             <?php echo _("Nuestro perfil en Gu&iacute;a Oleo"); ?>
-            <img src="/nuevo/img/soc_go.svg" class="displayBlock" alt="<?php echo _('Nuestro perfil en Gu&iacute;a Oleo'); ?>.">
+            <img src="/nuevo/img/social_go.svg" class="displayBlock" alt="<?php echo _('Nuestro perfil en Gu&iacute;a Oleo'); ?>.">
         </a>
     </li>
 </ul>
