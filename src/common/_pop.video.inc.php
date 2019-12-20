f7fed7662658eaf47f8818db4c6aa4c1fea4be2a
Gabriel Volonte <tampas@gmail.com>
2019-12-20 16:35:29 -0300

    Fix

--- src/common/_pop.video.inc.php
+++ src/common/_pop.video.inc.php
@@ -2,7 +2,7 @@
     <button type="button" class="pop_close pAbs indentedText"><?php echo _("Cerrar"); ?></button>
     <div class="pop_innerContainer">
         <div class="gooYTVideo">
-            <iframe src="<?php echo $youTubeV__noticiaA_src_iframe ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" class="displayBlock" allowfullscreen></iframe>
+            <iframe src="<?php echo $youTubeV__noticiaA_src_iframe ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" class="displayBlock mha" allowfullscreen></iframe>
         </div>
     </div>
 </div>
