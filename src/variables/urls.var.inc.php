2898565590524d09848307ae37135a71b9ff0a55
Gabriel Volonte <tampas@gmail.com>
2020-01-11 17:24:07 -0300

    Fix placeholders

--- src/variables/urls.var.inc.php
+++ src/variables/urls.var.inc.php
@@ -8,14 +8,22 @@
     $domain_sec     = 'sitiocomar';
     $url_sec        = 'http://www.'. $domain_sec;
     
-    $url-seccion        = '/nuevo/seccion.php';
     $url_index          = '/nuevo/';
+    // $url-seccion        = '/nuevo/seccion.php';
+    // $url-seccion        = '/nuevo/seccion.php';
+    // $url-seccion        = '/nuevo/seccion.php';
+    // $url-seccion        = '/nuevo/seccion.php';
+    // $url-seccion        = '/nuevo/seccion.php';
     $url_contacto       = '/nuevo/contacto.php';
     $url_contacto_full  = $url_global . $url_contacto;
     
-    $href-seccion  = 'href="'. $url .'"';
     $href_index    = 'href="'. $url_index .'"';
     $href_contacto = 'href="'. $url_contacto .'"';
+    // $href-seccion  = 'href="'. $url .'"';
+    // $href-seccion  = 'href="'. $url .'"';
+    // $href-seccion  = 'href="'. $url .'"';
+    // $href-seccion  = 'href="'. $url .'"';
+    // $href-seccion  = 'href="'. $url .'"';
     
     $url_actual     = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 ?>
