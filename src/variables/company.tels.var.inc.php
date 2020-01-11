9ccfeb9768448ccc17ca658370c3f76d001a136c
Gabriel Volonte <tampas@gmail.com>
2020-01-11 16:15:48 -0300

    Fix teléfonos en /variables/

--- src/variables/company.tels.var.inc.php
+++ src/variables/company.tels.var.inc.php
@@ -15,16 +15,45 @@
     $company_tel_sec_a             = '9999';
     $company_tel_sec_b             = '9999';
     
+    $company_fax_a                 = '2222';
+    $company_fax_b                 = '3333';
+    
+    // $company_tel_NOMBRE_a             = '3278';
+    // $company_tel_NOMBRE_b             = '9435';
+    
+    
+        //-- Main
     $company_tel_main              = $company_tel_main_a . $company_tel_main_b;
     $company_tel_main_formatted    = $tel_formatted_prefix .' '. $company_tel_main_a .'-'. $company_tel_main_b;
     $company_tel_main_code_country = '+'. $tel_code_country .' '. $tel_code_area .' '. $company_tel_main;
     
     $company_tel_main_whatsapp     = $tel_prefix_watsapp . $company_tel_main;
-    $social_whatsapp_chatOpen      = 'https://api.whatsapp.com/send?phone='. $company_tel_main_whatsapp;
-    $social_whatsapp_WAme_url      = 'https://wa.me/'. $company_tel_main_whatsapp .'?text=I’m%20interested%20in%20your%20car%20for%20sale';
+    $social_whatsapp_main_chatOpen = 'https://api.whatsapp.com/send?phone='. $company_tel_main_whatsapp;
+    $social_whatsapp_main_WAme_url      = 'https://wa.me/'. $company_tel_main_whatsapp .'?text=I’m%20interested%20in%20your%20car%20for%20sale';
     
+    
+        //-- Sec
     $company_tel_sec               = $company_tel_sec_a . $company_tel_sec_b;
     $company_tel_sec_formatted     = $tel_formatted_prefix. ' '. $company_tel_sec_a. '-' .$company_tel_sec_b;
     $company_tel_sec_code_country  = '+'. $tel_code_country .' '. $tel_code_area .' '. $company_tel_sec;
-    $company_tel_sec_whatsapp      = $tel_prefix_watsapp . $company_tel_sec;
+    
+    
+        //-- Fax
+    $company_fax                   = $company_fax_a . $company_fax_b;
+    $company_fax_formatted         = $tel_formatted_prefix .' '. $company_fax_a .'-'. $company_fax_b;
+    $company_fax_code_country      = '+'. $tel_code_country .' '. $tel_code_area .' '. $company_fax;  
+    
+    
+        //-- NOMBRE
+    // $company_tel_NOMBRE               = $company_tel_NOMBRE_a . $company_tel_NOMBRE_b;
+    // $company_tel_NOMBRE_formatted     = $tel_formatted_prefix .' '. $company_tel_NOMBRE_a .'-'. $company_tel_NOMBRE_b;
+    // $company_tel_NOMBRE_code_country  = '+'. $tel_code_country .' '. $tel_code_area .' '. $company_tel_NOMBRE;
+    
+    // $company_tel_NOMBRE_whatsapp      = $tel_prefix_watsapp . $company_tel_NOMBRE;
+    // $social_whatsapp_NOMBRE_chatOpen  = 'https://api.whatsapp.com/send?phone='. $company_tel_NOMBRE_whatsapp;
+    // $social_whatsapp_NOMBRE_WAme_url  = 'https://wa.me/'. $company_tel_NOMBRE_whatsapp .'?text=I’m%20interested%20in%20your%20car%20for%20sale';
+    
+    
+        //-- Misc    
+    $social_whatsapp_chatOpen = $social_whatsapp_main_chatOpen;
 ?>
