a3fd84de09ae619c1ed723843e1892235dcb90ac
Gabriel Volonte <tampas@gmail.com>
2020-01-11 15:19:05 -0300

    Fix

--- src/variables/company.tels.var.inc.php
+++ src/variables/company.tels.var.inc.php
@@ -17,7 +17,7 @@
     
     $company_tel_main              = $company_tel_main_a . $company_tel_main_b;
     $company_tel_main_formatted    = $tel_formatted_prefix .' '. $company_tel_main_a .'-'. $company_tel_main_b;
-    $company_tel_main_code_country = '+'. $tel_code_country .' '.$tel_code_area.' '. $company_tel_main;
+    $company_tel_main_code_country = '+'. $tel_code_country .' '. $tel_code_area .' '. $company_tel_main;
     
     $company_tel_main_whatsapp     = $tel_prefix_watsapp . $company_tel_main;
     $social_whatsapp_chatOpen      = 'https://api.whatsapp.com/send?phone='. $company_tel_main_whatsapp;
