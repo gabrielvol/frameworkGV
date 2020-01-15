c458da00b42f2caf693a1ceb926812863c81be14
Gabriel Volonte <tampas@gmail.com>
2020-01-15 19:33:16 -0300

    Ajustes en pop, styles media y pop video

--- src/common/_pop.video.inc.php
+++ src/common/_pop.video.inc.php
@@ -1,8 +1,8 @@
-<div class="pop_main pop_video pop_noticiaA boxShadow displayNone">
+<div class="pop_main pop_video pop-xxx displayNone">
     <button type="button" class="pop_close pAbs indentedText"><?php echo _("Cerrar"); ?></button>
     <div class="pop_innerContainer">
         <div class="gooYTVideo">
-            <iframe src="<?php echo $youTubeV__noticiaA_src_iframe ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" class="displayBlock mha" allowfullscreen></iframe>
+            <iframe src="<?php echo $youTubeV__xxxx_src_iframe ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" class="displayBlock mha" allowfullscreen></iframe>
         </div>
     </div>
 </div>
