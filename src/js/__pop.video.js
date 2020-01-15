b140b26cbf5de3ca45d754a16ed870d6c8785abc
Gabriel Volonte <tampas@gmail.com>
2020-01-15 18:08:46 -0300

    Fix

---
+++ src/js/__pop.video.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {        
+    $('.pop_open-xxxxx').click(function () {
+        $('.modal_video, .pop-xxxxx').addClass('displayBlock');
+        $('.modal_video, .pop-xxxxx').removeClass('displayNone');
+    });
+        
+    $('.pop_open-xxxxx').click(function () {
+        $('.modal_video, .pop-xxxxx').addClass('displayBlock');
+        $('.modal_video, .pop-xxxxx').removeClass('displayNone');
+    });
+    
+    
+    
+    
+    $('.modal_video, .pop_close').click(function () {        
+        $('.modal_video, .pop_video').addClass('displayNone');
+        $('.modal_video, .pop_video').removeClass('displayBlock');
+    });
+});
