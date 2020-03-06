178f5025e62a7c70a695c599785ebc96491b4e81
Gabriel Volonte <tampas@gmail.com>
2020-03-06 20:01:00 -0300

    Variables para files

--- src/variables/_files.var.inc.php
+++ src/variables/_files.var.inc.php
@@ -1,8 +1,8 @@
 <?php
-    /* // Download anchor attributes ---------------------------------------- */   
+    /* // Download anchor attributes ---------------------------------------- */
     
-    $file_href______pdf_01 = 'href="/files/archivopdf02.pdf"';
-    $file_download__pdf_01 = 'download="titulo_pricipal-titulo_secundario.pdf"';
-    
-    $file_pdf01 = $file_href______pdf_01 . $file_download__pdf_01;
+    // Título
+    // $file_href______pdf_01 = 'href="/files/archivopdf02.pdf"';
+    // $file_download__pdf_01 = 'download="titulo_pricipal-titulo_secundario.pdf"';    
+    // $file_pdf01 = $file_href______pdf_01 . $file_download__pdf_01;
 ?>
