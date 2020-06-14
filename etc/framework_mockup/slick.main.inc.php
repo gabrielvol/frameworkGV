5b667567c9900ceeb5334bcf3ee0073e5ee126aa
Gabriel Volonte <tampas@gmail.com>
2020-06-14 20:16:40 -0300

    :art: :nail_care: Reestructuración CSS / Cosmetic

---
+++ etc/framework_mockup/slick.main.inc.php
@@ -0,0 +1,70 @@
+<?php
+    $slider_class = 'sliderSeccion';
+
+    $slider_foto_01 = '01';
+    $slider_foto_02 = '02';
+    $slider_foto_03 = '03';
+    $slider_foto_04 = '04';
+    $slider_foto_05 = '05';
+    $slider_foto_06 = '06';
+    $slider_foto_07 = '07';
+    $slider_foto_08 = '08';
+    $slider_foto_09 = '09';
+    $slider_foto_10 = '10';
+    $slider_foto_11 = '11';
+    $slider_foto_12 = '12';
+    $slider_foto_13 = '13';
+    $slider_foto_14 = '14';
+    $slider_foto_15 = '15';
+    $slider_foto_16 = '16';
+    $slider_foto_17 = '17';
+    $slider_foto_18 = '18';
+    $slider_foto_19 = '19';
+    $slider_foto_20 = '20';
+    
+    include($_SERVER['DOCUMENT_ROOT'] . '/nuevo/common/slick.main.inc.php');
+?>                
+
+<ul class="slick slick_main mask horizontal clearfix <?php if(isset($slider_class)) { echo $slider_class; } ?>">
+    <?php        if(isset($slider_foto_01)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_01; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_02)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_02; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_03)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_03; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_04)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_04; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_05)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_05; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_06)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_06; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_07)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_07; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_08)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_08; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_09)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_09; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_10)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_10; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_11)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_11; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_12)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_12; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_13)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_13; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_14)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_14; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_15)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_15; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_16)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_16; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_17)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_17; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_18)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_18; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_19)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_19; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; if(isset($slider_foto_20)): // Section ?>
+    <li><img src="/web2020/img/slide_home_<?php echo $slider_foto_20; ?>.jpg" alt="" aria-hidden="true" class="displayBlock fullWidth"></li>
+    <?php endif; ?>
+</ul>
--- src/common/_social.inc.php
+++ src/common/_social.inc.php
@@ -1,77 +1,75 @@
 <ul class="social unstyled displayFlex">
     <li class="social_fb">
         <a href="<?php echo $social_facebook_url ?>" class="fb displayBlock hoverGrowL indentedText">
-            <?php echo _("Seguinos en Facebook"); ?>
+            <?php echo _("Seguinos en Facebook"); ?>.
             <img src="/nuevo/img/social_fb.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en Facebook'); ?>.">
         </a>
     </li>
     <li class="social_sk">
         <a href="<?php echo $social_skype_user ?>" class="sk displayBlock hoverGrowL indentedText">
-            <?php echo _("Contactanos en Skype"); ?>
+            <?php echo _("Contactanos en Skype"); ?>.
             <img src="/nuevo/img/social_sk.svg" class="displayBlock" alt="<?php echo _('Comunicate con nosotros por Skype'); ?>.">
         </a>
     </li>
     <li class="social_tw">
         <a href="<?php echo $social_twitter_url ?>" class="tw displayBlock hoverGrowL indentedText">
-            <?php echo _("Seguinos en Twitter"); ?>
+            <?php echo _("Seguinos en Twitter"); ?>.
             <img src="/nuevo/img/social_tw.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en Twitter'); ?>.">
         </a>
     </li>
     <li class="social_vi">
         <a href="<?php echo $social_vimeo_url ?>" class="vi displayBlock hoverGrowL indentedText">
-            <?php echo _("Mir&aacute; Nuestro Canal en Vimeo"); ?>
+            <?php echo _("Mir&aacute; Nuestro Canal en Vimeo"); ?>.
             <img src="/nuevo/img/social_vi.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro canal de Vimeo'); ?>.">
         </a>
     </li>
     <li class="social_ig">
         <a href="<?php echo $social_instagram_url ?>" class="ig displayBlock hoverGrowL indentedText">
-            <?php echo _("Seguinos en Instagram"); ?>
+            <?php echo _("Seguinos en Instagram"); ?>.
             <img src="/nuevo/img/social_ig.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en Instagram'); ?>.">
         </a>
     </li>
     <li class="social_pi">
         <a href="<?php echo $social_pinterest_url ?>" class="pi displayBlock hoverGrowL indentedText">
-            <?php echo _("Seguinos en Pinterest"); ?>
+            <?php echo _("Seguinos en Pinterest"); ?>.
             <img src="/nuevo/img/social_pi.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en Pinterest'); ?>.">
         </a>
     </li>
     <li class="social_yt">
         <a href="<?php echo $social_youTube_url ?>" class="yt displayBlock hoverGrowL indentedText">
-            <?php echo _("Mir&aacute; Nuestro Canal en YouTube"); ?>
+            <?php echo _("Mir&aacute; Nuestro Canal en YouTube"); ?>.
             <img src="/nuevo/img/social_yt.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro canal de YouTube'); ?>.">
         </a>
     </li>
     <li class="social_li">
         <a href="<?php echo $social_linkedin_url ?>" class="li displayBlock hoverGrowL indentedText">
-            <?php echo _("Mir&aacute; nuestro perfil en LinkedIn"); ?>
+            <?php echo _("Mir&aacute; nuestro perfil en LinkedIn"); ?>.
             <img src="/nuevo/img/social_li.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en LinkedIn'); ?>.">
         </a>
     </li>
     <li class="social_sp">
         <a href="<?php echo $social_spotify_url ?>" class="sp displayBlock hoverGrowL indentedText">
-            <?php echo _("Escuchanos en Spotify"); ?>
+            <?php echo _("Escuchanos en Spotify"); ?>.
             <img src="/nuevo/img/social_sp.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en Spotify'); ?>.">
         </a>
     </li>
     <li class="social_sc">
         <a href="<?php echo $social_snapchat_url ?>" class="sp displayBlock hoverGrowL indentedText">
-            <?php echo _("Nuestro perfil en Snapchat"); ?>
+            <?php echo _("Nuestro perfil en Snapchat"); ?>.
             <img src="/nuevo/img/social_sc.svg" class="displayBlock" alt="<?php echo _('Ir a nuestro perfil en Snapchat'); ?>.">
         </a>
     </li>
     <li class="social_wa">
         <a href="<?php echo $company_whatsapp_chatOpen ?>" class="wa conTel displayBlock">11 0000 2222</a> 
         
-        <a href="<?php echo $company_whatsapp_chatOpen ?>" class="wa displayBlock hoverGrowL indentedText"><?php echo _('Nuestro contacto en WhatsApp'); ?>.</a> 
-        
         <a href="<?php echo $company_whatsapp_chatOpen ?>" class="wa displayBlock hoverGrowL indentedText">
-            <?php echo _("Nuestro contacto en WhatsApp"); ?>
+            <?php echo _("Nuestro contacto en WhatsApp"); ?>.
             <img src="/nuevo/img/social_wa.svg" class="displayBlock" alt="<?php echo _('Nuestro contacto en WhatsApp'); ?>.">
         </a>
     </li>
     <li class="social_em">
         <a <?php echo $company_email_href_EMAILA ?> class="em displayBlock hoverGrowL indentedText">
-            <?php echo _("Cont&aacute;ctenos por Correo Electr&oacute;nico"); ?>
+            <?php echo _("Cont&aacute;ctenos por Correo Electr&oacute;nico"); ?>.
             <img src="/nuevo/img/social_em.svg" class="displayBlock" alt="<?php echo _('Cont&aacute;ctenos por Correo Electr&oacute;nico'); ?>.">
         </a>
     </li>
@@ -80,13 +78,13 @@
     </li>
     <li class="social_ta">
         <a href="/nuevo/" class="ta displayBlock hoverGrowL indentedText">
-            <?php echo _("Nuestro perfil en TripAdvisor"); ?>
+            <?php echo _("Nuestro perfil en TripAdvisor"); ?>.
             <img src="/nuevo/img/social_ta.svg" class="displayBlock" alt="<?php echo _('Nuestro perfil en TripAdvisor'); ?>.">
         </a>
     </li>
     <li class="social_go">
         <a href="/nuevo/" class="go displayBlock hoverGrowL indentedText">
-            <?php echo _("Nuestro perfil en Gu&iacute;a Oleo"); ?>
+            <?php echo _("Nuestro perfil en Gu&iacute;a Oleo"); ?>.
             <img src="/nuevo/img/social_go.svg" class="displayBlock" alt="<?php echo _('Nuestro perfil en Gu&iacute;a Oleo'); ?>.">
         </a>
     </li>
