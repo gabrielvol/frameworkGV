f84455a9fa5b2e55370f36a9ad2c0f1576b0da6f
Gabriel Volonte <tampas@gmail.com>
2020-10-25 13:58:00 -0300

    :zap: Agrego p en top level item, ajusto variables de files.var

--- src/var/_files.var.inc.php
+++ src/var/_files.var.inc.php
@@ -2,12 +2,12 @@
     /* // Download anchor attributes ---------------------------------------- */
     
 //    Título
-//    $file_href_-pdf_01       = 'href="/nuevo/files/archivopdf01.pdf"';
-//    $file_download_-pdf_01   = 'download="titulo_pricipal-titulo_secundario.pdf"';    
-//    $file_-pdf_01            = $file_href_-pdf_01 .' '. $file_download_-pdf_01;
+//    $file_href_____-pdf_01    = 'href="/nuevo/files/archivopdf01.pdf"';
+//    $file_download_-pdf_01    = 'download="titulo_pricipal-titulo_secundario.pdf"';    
+//    $file__________-pdf_01    = $file_href_____-pdf_01 .' '. $file_download_-pdf_01;
     
 //    Título
-//    $file_href______pdf_02 = 'href="/files/archivopdf02.pdf"';
-//    $file_download__pdf_02 = 'download="titulo_pricipal-titulo_secundario.pdf"';    
-//    $file_pdf02 = $file_href______pdf_02 . $file_download__pdf_02;
+//    $file_href______pdf_02    = 'href="/nuevo/files/archivopdf02.pdf"';
+//    $file_download__pdf_02    = 'download="titulo_pricipal-titulo_secundario.pdf"';    
+//    $file___________pdf_02    = $file_href______pdf_02 .' '. $file_download__pdf_02;
 ?>
