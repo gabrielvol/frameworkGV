bf6c026d2dd02778ef556eb2cddbf699ba1e102a
Gabriel Volonte <tampas@gmail.com>
2021-01-31 19:42:54 -0300

    :art: Meta tags / Open Graph.

--- src/var/mails.var.inc.php
+++ src/var/mails.var.inc.php
@@ -4,7 +4,7 @@
     $site_email_address_EMAILA   = $site_email_EMAILA .'@'. $domain_global;
     $site_email_mailto_EMAILA    = 'mailto:'. $site_email_address_EMAILA;
     $site_email_href_EMAILA      = 'href="'. $site_email_mailto_EMAILA .'"';
-    $site_email_anchor_EMAILA    = '<a '. $site_email_href_EMAILA .'>'. $site_email_address_EMAILA .'</a>';   
+    $site_email_anchor_EMAILA    = '<a '. $site_email_href_EMAILA .'>'. $site_email_address_EMAILA .'</a>';
     
     $site_email_EMAILB           = 'EMAILB';
     $site_email_address_EMAILB   = $site_email_EMAILB .'@'. $domain_global;
