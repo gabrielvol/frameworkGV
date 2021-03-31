dd160778dcc0dccbee645407fef129998783cde0
Gabriel Volonte <tampas@gmail.com>
2021-03-31 14:07:06 -0300

    :art: :nail_care: :books: Reestructuración CSS / Cosmetic / Documentación

--- src/var/act/header.act.inc.php
+++ src/var/act/header.act.inc.php
@@ -1,4 +1,26 @@
 <?php
-    /* // Header ------------------------------------------------------------ */
-    $logo_bgImage_before               = 0;
+/* =============================================================================
+   Header Activators [/src/var/act/header.act.inc.php]
+   ========================================================================== */
+
+/* // Descripcion ----------------------------------------------------------- */
+// Activadores para Header.
+//
+// #activador #header #logo #before
+
+
+/* // Listado --------------------------------------------------------------- */
+// 01 _ $logo_bgImage_before_act
+//
+//
+// 01 _ $logo_bgImage_before_act
+// Activa los if que se encuentran en `[/src/common/header.inc.php]`
+//
+// Genera un `div.logo_before` que envuelve al logo.
+//
+// VER / SEE `[/etc/css/custom/act/logo_act.scss]`
+
+
+$logo_bgImage_before_act    = 0; // VER / SEE `[/etc/css/custom/act/logo_act.scss]`
+
 ?>
