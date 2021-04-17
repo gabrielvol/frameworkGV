b80ae0032d55716ce9e77d73570eb22211af7a5a
Gabriel Volonte <tampas@gmail.com>
2021-04-17 11:45:46 -0300

    :art: Reestructuro variables para URLS

--- src/var/urls.gettext.var.inc.php
+++ src/var/urls.gettext.var.inc.php
@@ -1,34 +1,49 @@
 <?php
-    /* // Gettext URLS ------------------------------------------------------ */
-
-    $hash_inicio_gettext         = '#main';
+/* // Gettext URLS ---------------------------------------------------------- */
+    
+/* // Hashes gettext -------------------------------------------------------- */
     $hash_inicio_gettext         = '#'. _('inicio');
-//    $hash-PAGINA-gettext         = '#'. _('seccion');
-//    $hash-PAGINA-gettext         = '#'. _('seccion');
-//    $hash-PAGINA-gettext         = '#'. _('seccion');
-    $hash_contacto_gettext       = '#'. _('contacto');  
+    $hash_contacto_gettext       = '#'. _('contacto');
+//    $hash-PAGINA_gettext         = '#'. _('seccion');
+//    $hash-PAGINA_gettext         = '#'. _('seccion');
+//    $hash-PAGINA_gettext         = '#'. _('seccion');
     
+    
+/* // url gettext ----------------------------------------------------------- */ 
+    $url_inicio_hs_gettext      = $url_dir .'/'. $hash_inicio_gettext;
+    $url_contacto_hs_gettext    = $url_dir .'/'. $hash_contacto_gettext; 
+//    $url-PAGINA_hs_gettext      = $url_dir .'/'. $hash_PAGINA_gettext; 
+//    $url-PAGINA_hs_gettext      = $url_dir .'/'. $hash_PAGINA_gettext; 
+//    $url-PAGINA_hs_gettext      = $url_dir .'/'. $hash_PAGINA_gettext;
+    
+//    $url_alianzas_hs_SERVICIOS_gettext     = $url_servicios . $hash_alianzas_gettext;
+//    $url-PAGINA_hs_PAGINA_gettext      = $url_PAGINA . $hash_PAGINA_gettext;
+//    $url-PAGINA_hs_PAGINA_gettext      = $url_PAGINA . $hash_PAGINA_gettext;
+//    $url-PAGINA_hs_PAGINA_gettext      = $url_PAGINA . $hash_PAGINA_gettext;
+  
+    
+/* // global urls / full urls ----------------------------------------------- */ 
+    $url_inicio_hs_full_gettext    = $url_global . $url_inicio_hs_gettext;
+    $url_contacto_hs_full_gettext  = $url_global . $url_contacto_hs_gettext;
+//    $url-PAGINA_hs_full_gettext    = $url_global . $url_PAGINA_gettext;
+//    $url-PAGINA_hs_full_gettext    = $url_global . $url_PAGINA_gettext;
+//    $url-PAGINA_hs_full_gettext    = $url_global . $url_PAGINA_gettext;
 
-    $url_inicio_gettext         = $url_dir .'/'. $hash_inicio_gettext;
-//    $url-PAGINA-gettext         = $url_dir .'/'. $hash-PAGINA-gettext;
-//    $url-PAGINA-gettext         = $url_dir .'/'. $hash-PAGINA-gettext;
-//    $url-PAGINA-gettext         = $url_PAGINA . $hash-PAGINA-gettext;
-//    $url-PAGINA-gettext         = $url_PAGINA . $hash-PAGINA-gettext;
-    $url_contacto_gettext       = $url_dir .'/'. $hash_contacto_gettext;  
+//    $url_inicio_hs_INDEX_full_gettext     = $url_global . $url_index . $hash_inicio_gettext;
+//    $url-PAGINA_hs_PAGINA_full_gettext    = $url_global . $url_PAGINA . $url_PAGINA_gettext;
+//    $url-PAGINA_hs_PAGINA_full_gettext    = $url_global . $url_PAGINA . $url_PAGINA_gettext;
+//    $url-PAGINA_hs_PAGINA_full_gettext    = $url_global . $url_PAGINA . $url_PAGINA_gettext;
     
     
-    $href_inicio_gettext        = 'href="'. $url_inicio_gettext .'"';
-//    $href-PAGINA_gettext       = 'href="'. $url .'"';
-//    $href-PAGINA_gettext       = 'href="'. $url .'"';
-//    $href-PAGINA_gettext       = 'href="'. $url .'"';
-    $href_contacto_gettext      = 'href="'. $url_contacto_gettext .'"';  
+/* // href ------------------------------------------------------------------ */ 
+    $href_inicio_hs_gettext        = 'href="'. $url_inicio_hs_gettext .'"';
+    $href_contacto_hs_gettext      = 'href="'. $url_contacto_hs_gettext .'"';
+//    $href-PAGINA_hs_gettext        = 'href="'. $url_pagina_hs_gettext .'"';
+//    $href-PAGINA_hs_gettext        = 'href="'. $url_pagina_hs_gettext .'"';
+//    $href-PAGINA_hs_gettext        = 'href="'. $url_pagina_hs_gettext .'"';
     
-    
-    $url_inicio_full_gettext    = $url_global . $url_inicio_gettext;
-    $url_inicio_full_gettext    = $url_global . $url_index . $hash_inicio_gettext;
-//    $url_PAGINA_full_gettext  = $url_global . $url_PAGINA_gettext;
-//    $url_PAGINA_full_gettext  = $url_global . $url_PAGINA_gettext;
-//    $url_PAGINA_full_gettext  = $url_global . $url_PAGINA . $hash_PAGINA_gettext;
-//    $url_PAGINA_full_gettext  = $url_global . $url_PAGINA . $hash_PAGINA_gettext;
-    $url_contacto_full_gettext  = $url_global . $url_contacto_gettext;
+//    $href_alianzas_hs_SERVICIOS_gettext   = 'href="'. $url_alianzas_hs_SERVICIOS_gettext .'"';
+//    $href-PAGINA_hs_pagina_gettext        = 'href="'. $url_pagina_hs_PAGINA_gettext .'"';
+//    $href-PAGINA_hs_pagina_gettext        = 'href="'. $url_pagina_hs_PAGINA_gettext .'"';
+//    $href-PAGINA_hs_pagina_gettext        = 'href="'. $url_pagina_hs_PAGINA_gettext .'"';
 ?>
--- src/var/urls.var.inc.php
+++ src/var/urls.var.inc.php
@@ -1,35 +1,71 @@
 <?php
-    /* // URLS -------------------------------------------------------------- */
+/* // Domains --------------------------------------------------------------- */
     $domain_global      = 'sitiocomar';
     $domain_sec         = 'sitiocomar';
     
     $url_global         = 'http://www.'. $domain_global;
     $url_sec            = 'http://www.'. $domain_sec;
     
-    $url_openGraph_global   = $url_global . $url_dir;
+    
+/* // Hashes ---------------------------------------------------------------- */
+    $hash_main          = '#main';
+    $hash_inicio        = '#inicio';
+//    $hash-SECCION       = '#seccion';
+//    $hash-SECCION       = '#seccion';
+//    $hash-SECCION       = '#seccion';
     
     
-    $url_index          = $url_dir .'/';                // $nav_pageCurrent 0
-//    $url-PAGINA         = $url_dir .'/pagina.php';      // $nav_pageCurrent 0
-//    $url-PAGINA         = $url_dir .'/pagina.php';      // $nav_pageCurrent 0
-//    $url-PAGINA         = $url_dir .'/pagina.php';      // $nav_pageCurrent 0
+/* // url ------------------------------------------------------------------- */
+    $url_root           = $url_dir .'/';
+    $url_index          = $url_dir .'/index.php';           // $nav_pageCurrent 1
     $url_contacto       = $url_dir .'/contacto.php';    // $nav_pageCurrent 0
+//    $url-PAGINA         = $url_dir .'/pagina.php';      // $nav_pageCurrent 0
+//    $url-PAGINA         = $url_dir .'/pagina.php';      // $nav_pageCurrent 0
+//    $url-PAGINA         = $url_dir .'/pagina.php';      // $nav_pageCurrent 0
+
+    $url_main_hs    = $url_dir .'/'. $hash_main;
+    $url_inicio_hs  = $url_dir .'/'. $hash_inicio;
+//    $url-SECCION_hs      = $url_dir .'/'. $hash_SECCION;
+//    $url-SECCION_hs      = $url_dir .'/'. $hash_SECCION;
+//    $url-SECCION_hs      = $url_dir .'/'. $hash_SECCION;  
+
+//    $url_quienesSomos_hs_INDEX    = $url_index . $hash_quienesSomos; 
+//    $url-SECCION_hs_PAGINA        = $url_PAGINA . $hash_SECCION;
+//    $url-SECCION_hs_PAGINA        = $url_PAGINA . $hash_SECCION;
+//    $url-SECCION_hs_PAGINA        = $url_PAGINA . $hash_SECCION;
+  
     
-//    $url_inicio         = "/#inicio";
-//    $url-PAGINA         = "/#seccion"; 
-//    $url-PAGINA         = "/#seccion"; 
-//    $url-PAGINA         = "/#seccion"; 
-//    $url_contacto       = "/#contacto";
+/* // global urls / full urls ----------------------------------------------- */
+    $url_global_full        = $url_global . $url_dir;
     
-    $url_contacto_full  = $url_global . $url_contacto;
+    $url_openGraph_global   = $url_global_full;
     
+    $url_contacto_full      = $url_global . $url_contacto;
+//    $url-PAGINA_full        = $url_global . $url_PAGINA;
+//    $url-PAGINA_full        = $url_global . $url_PAGINA;
+//    $url-PAGINA_full        = $url_global . $url_PAGINA;
+    
+    
+/* // href ------------------------------------------------------------------ */ 
     $href_index         = 'href="'. $url_index .'"';
-//    $href-PAGINA        = 'href="'. $url_PAGINA .'"';
-//    $href-PAGINA        = 'href="'. $url_PAGINA .'"';
-//    $href-PAGINA        = 'href="'. $url_PAGINA .'"';
-//    $href-PAGINA        = 'href="'. $url_PAGINA .'"';
-//    $href-PAGINA        = 'href="'. $url_PAGINA .'"';
     $href_contacto      = 'href="'. $url_contacto .'"';
+//    $href-PAGINA        = 'href="'. $url_PAGINA .'"';
+//    $href-PAGINA        = 'href="'. $url_PAGINA .'"';
+//    $href-PAGINA        = 'href="'. $url_PAGINA .'"';
+    
+    $href_main_hs       = 'href="'. $url_main_hs .'"';
+    $href_inicio_hs     = 'href="'. $url_inicio_hs .'"';
+//    $href-SECCION_hs    = 'href="'. $url_SECCION_hs .'"';
+//    $href-SECCION_hs    = 'href="'. $url_SECCION_hs .'"';
+//    $href-SECCION_hs    = 'href="'. $url_SECCION_hs .'"';
+
+//    $href_quienesSomos_hs_INDEX = 'href="'. $url_quienesSomos_hs_INDEX .'"';
+//    $href-SECCION_hs_PAGINA      = 'href="'. $url_SECCION_hs_PAGINA .'"';
+//    $href-SECCION_hs_PAGINA      = 'href="'. $url_SECCION_hs_PAGINA .'"';
+//    $href-SECCION_hs_PAGINA      = 'href="'. $url_SECCION_hs_PAGINA .'"';
+    
+    
+/* // misc / experimental --------------------------------------------------- */ 
     
 //    $url_actual     = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 ?>
