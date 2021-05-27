40c11c5b05b4d5173c59092ea8e1f2a66af34f7e
Gabriel Volonte <tampas@gmail.com>
2021-05-26 21:37:52 -0300

    :pencil2: :zap: Fixes in media, shadows. Nav sub AT.

--- etc/framework_mockup/slider.header.inc.php
+++ etc/framework_mockup/slider.header.inc.php
@@ -1,26 +1,26 @@
 <?php
-    $slider_header_class = 'slider_header';
+    $slider_header_class    = 'slider_header';
 
-    $slider_header_item_01 = '01';
-    $slider_header_item_02 = '02';
-    $slider_header_item_03 = '03';
-    $slider_header_item_04 = '04';
-    $slider_header_item_05 = '05';
-    $slider_header_item_06 = '06';
-    $slider_header_item_07 = '07';
-    $slider_header_item_08 = '08';
-    $slider_header_item_09 = '09';
-    $slider_header_item_10 = '10';
-    $slider_header_item_11 = '11';
-    $slider_header_item_12 = '12';
-    $slider_header_item_13 = '13';
-    $slider_header_item_14 = '14';
-    $slider_header_item_15 = '15';
-    $slider_header_item_16 = '16';
-    $slider_header_item_17 = '17';
-    $slider_header_item_18 = '18';
-    $slider_header_item_19 = '19';
-    $slider_header_item_20 = '20';
+    $slider_header_item_01  = '01';
+    $slider_header_item_02  = '02';
+    $slider_header_item_03  = '03';
+    $slider_header_item_04  = '04';
+    $slider_header_item_05  = '05';
+    $slider_header_item_06  = '06';
+    $slider_header_item_07  = '07';
+    $slider_header_item_08  = '08';
+    $slider_header_item_09  = '09';
+    $slider_header_item_10  = '10';
+    $slider_header_item_11  = '11';
+    $slider_header_item_12  = '12';
+    $slider_header_item_13  = '13';
+    $slider_header_item_14  = '14';
+    $slider_header_item_15  = '15';
+    $slider_header_item_16  = '16';
+    $slider_header_item_17  = '17';
+    $slider_header_item_18  = '18';
+    $slider_header_item_19  = '19';
+    $slider_header_item_20  = '20';
     
     include($_SERVER['DOCUMENT_ROOT'] . $url_dir .'/common/slider.header.inc.php');
 ?>                
