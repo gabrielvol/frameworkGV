eabc4360f76456f01e46a8e9ca8c07a17a500078
Gabriel Volonte <tampas@gmail.com>
2021-06-02 16:29:19 -0300

    :zap: Agrego variables para iframe de Google Maps.

--- src/var/address.var.inc.php
+++ src/var/address.var.inc.php
@@ -1,5 +1,5 @@
 <?php
-    /* // Address ----------------------------------------------------------- */
+    /* // Address PRINCIPAL ------------------------------------------------- */
     $site_address_PRINCIPAL_a         = 'aaaaaaaaaaaaaaaaaaaaaaaaaa';     
     $site_address_PRINCIPAL_b         = 'aaaaaaaaaaaaaaaaaaaaaaaaaa';
     $site_address_PRINCIPAL           = $site_address_PRINCIPAL_a .' '. $site_address_PRINCIPAL_b;   
@@ -8,13 +8,19 @@
     $site_address_PRINCIPAL_b_GB      = 'bbbbbbbbbbbbbbbbbbbbbbbbbb Autonomous City of Buenos Aires';
     $site_address_PRINCIPAL_GB        = $site_address_PRINCIPAL_a_GB .' '. $site_address_PRINCIPAL_b_GB;      
        
-    $site_address_PRINCIPAL_map_url   = 'https_goo_gl_maps_aaaaaaaaaaaa';
-    $site_address_PRINCIPAL_map_href  = 'href="'. $site_address_PRINCIPAL_map_url .'"';
+    $site_address_PRINCIPAL_map_url         = 'https_goo_gl_maps_aaaaaaaaaaaa';
+    $site_address_PRINCIPAL_map_href        = 'href="'. $site_address_PRINCIPAL_map_url .'"';
+    $site_address_PRINCIPAL_map_iframe_src  = 'https_google_com_maps_embed_aaaaaaaaaaaaaaaaa';
+    $site_address_PRINCIPAL_map_iframe      = '<iframe src="'. $site_address_PRINCIPAL_map_iframe_src .'" allowfullscreen="" loading="lazy"></iframe>';
     
+    
+    /* // Address NOMBRE ---------------------------------------------------- */
 //    $site_address_NOMBRE_a       = 'bbbbbbbbbbbbbbbbbbbbbbbbbb';
 //    $site_address_NOMBRE_b       = 'bbbbbbbbbbbbbbbbbbbbbbbbbb';
 //    $site_address_NOMBRE         = $site_address_NOMBRE_a .' '. $site_address_NOMBRE_b;       
        
 //    $site_address_NOMBRE_map_url   = 'https_goo_gl_maps_aaaaaaaaaaaa';
 //    $site_address_NOMBRE_map_href  = 'href="'. $site_address_NOMBRE_map_url .'"';
+//    $site_address_NOMBRE_map_iframe_src  = 'https_google_com_maps_embed_aaaaaaaaaaaaaaaaa';
+//    $site_address_NOMBRE_map_iframe      = '<iframe src="'. $site_address_NOMBRE_map_iframe_src .'" allowfullscreen="" loading="lazy"></iframe>';
 ?>
