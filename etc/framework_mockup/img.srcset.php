a064cb50f17e6282b7053a3289e8163cd2677947
Gabriel Volonte <tampas@gmail.com>
2021-07-01 14:12:16 -0300

    :zap: Img srcset placeholders.

---
+++ etc/framework_mockup/img.srcset.php
@@ -0,0 +1,12 @@
+
+<img src="<?php echo $url_dir; ?>/img/img_NOMBREIMG_1500.jpg" alt=""
+    srcset="<?php echo $url_dir; ?>/img/img_NOMBREIMG_600.jpg 600w, <?php echo $url_dir; ?>/img/img_NOMBREIMG_1000.jpg 1000w, <?php echo $url_dir; ?>/img/img_NOMBREIMG_1500.jpg 1500w" />
+
+<img src="<?php echo $url_dir; ?>/img/img_NOMBREIMG_2000.jpg" alt=""
+    srcset="<?php echo $url_dir; ?>/img/img_NOMBREIMG_600.jpg 600w, <?php echo $url_dir; ?>/img/img_NOMBREIMG_1000.jpg 1000w, <?php echo $url_dir; ?>/img/img_NOMBREIMG_1500.jpg 1500w, <?php echo $url_dir; ?>/img/img_NOMBREIMG_2000.jpg 2000w" />
+
+<img src="<?php echo $url_dir; ?>/img/img_NOMBREIMG_2000.jpg" alt=""
+    srcset="<?php echo $url_dir; ?>/img/img_NOMBREIMG_600.jpg 600w, <?php echo $url_dir; ?>/img/img_NOMBREIMG_1000.jpg 1000w, <?php echo $url_dir; ?>/img/img_NOMBREIMG_1500.jpg 1500w, <?php echo $url_dir; ?>/img/img_NOMBREIMG_2000.jpg 2000w, <?php echo $url_dir; ?>/img/img_NOMBREIMG_2500.jpg 2500w" />
+
+<img src="<?php echo $url_dir; ?>/img/img_NOMBREIMG_2000.jpg" alt=""
+    srcset="<?php echo $url_dir; ?>/img/img_NOMBREIMG_600.jpg 600w, <?php echo $url_dir; ?>/img/img_NOMBREIMG_1000.jpg 1000w, <?php echo $url_dir; ?>/img/img_NOMBREIMG_1500.jpg 1500w, <?php echo $url_dir; ?>/img/img_NOMBREIMG_2000.jpg 2000w, <?php echo $url_dir; ?>/img/img_NOMBREIMG_2500.jpg 2500w, <?php echo $url_dir; ?>/img/img_NOMBREIMG_3000.jpg 3000w" />
