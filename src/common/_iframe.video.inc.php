a2ab2e7bf493f19bd1a715e3ff356beaf255503e
Gabriel Volonte <tampas@gmail.com>
2021-08-17 19:24:00 -0300

    :zap: iframes

--- src/common/_iframe.video.inc.php
+++ src/common/_iframe.video.inc.php
@@ -1,3 +1,3 @@
-<div class="video_mask">
+<div class="iframe_video_mask">
     <iframe src="<?php echo $media__videoA_src_iframe ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" class="displayBlock" allowfullscreen></iframe>
 </div>
--- src/common/_pop.video.inc.php
+++ src/common/_pop.video.inc.php
@@ -1,7 +1,7 @@
 <div class="pop_global pop_video pop-xxx displayNone">
     <button type="button" class="button_pop_close"><?php echo _("Cerrar Ventana"); ?></button>
     <div class="pop_innerContainer">
-        <div class="video_mask">
+        <div class="iframe_video_mask">
             <iframe src="<?php echo $media__NOMBRE_src_iframe ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" class="displayBlock mha" allowfullscreen></iframe>
         </div>
     </div>
