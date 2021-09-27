f76d94f0242dfc2b2c5eaf5798cf86db84346fa7
Gabriel Volonte <tampas@gmail.com>
2021-09-27 18:48:43 -0300

    :zap: Nav Language.

--- src/common/_nav.language.inc.php
+++ src/common/_nav.language.inc.php
@@ -31,72 +31,73 @@
     $gettext_switchMsg_ZH       = "sinTraduccion";
     $gettext_languageName_ZH    = "sinTraduccion";
 ?>
-
-<ul class="nav_language_ul unstyled displayFlex">
-    <li>
-        <a href="<?php echo $url_entorno; ?>/?l=es_AR" hreflang="es" class="hover_grow_M_ani">
-            <img src="<?php echo $url_entorno; ?>/img/flag_ar.svg" alt="<?php echo $gettext_switchMsg_AR; ?>." />
-            <span class="visuallyHidden" lang="es"><?php echo $gettext_languageName_AR; ?></span>
-        </a>
-    </li>
-    <li>
-        <a href="<?php echo $url_entorno; ?>/?l=pt_BR" hreflang="pt" class="hover_grow_M_ani">
-            <img src="<?php echo $url_entorno; ?>/img/flag_br.svg" alt="<?php echo $gettext_switchMsg_BR; ?>." />
-            <span class="visuallyHidden" lang="pt"><?php echo $gettext_languageName_BR; ?></span>
-        </a>
-    </li>
-    <li>
-        <a href="<?php echo $url_entorno; ?>/?l=de_DE" hreflang="de" class="hover_grow_M_ani">
-            <img src="<?php echo $url_entorno; ?>/img/flag_de.svg" alt="<?php echo $gettext_switchMsg_DE; ?>." />
-            <span class="visuallyHidden" lang="de"><?php echo $gettext_languageName_DE; ?></span>
-        </a>
-    </li>
-    <li class="flag_alignedLeft">
-        <a href="<?php echo $url_entorno; ?>/?l=es_ES" hreflang="es" class="hover_grow_M_ani">
-            <img src="<?php echo $url_entorno; ?>/img/flag_es.svg" alt="<?php echo $gettext_switchMsg_ES; ?>." />
-            <span class="visuallyHidden" lang="es"><?php echo $gettext_languageName_ES; ?></span>
-        </a>
-    </li>
-    <li>
-        <a href="<?php echo $url_entorno; ?>/?l=fr_FR" hreflang="fr" class="hover_grow_M_ani">
-            <img src="<?php echo $url_entorno; ?>/img/flag_fr.svg" alt="<?php echo $gettext_switchMsg_FR; ?>." />
-            <span class="visuallyHidden" lang="fr"><?php echo $gettext_languageName_FR; ?></span>
-        </a>
-    </li>
-    <li>
-        <a href="<?php echo $url_entorno; ?>/?l=en_GB" hreflang="en" class="hover_grow_M_ani">
-            <img src="<?php echo $url_entorno; ?>/img/flag_gb.svg" alt="<?php echo $gettext_switchMsg_GB; ?>." />
-            <span class="visuallyHidden" lang="en"><?php echo $gettext_languageName_GB; ?></span>
-        </a>
-    </li>
-    <li>
-        <a href="<?php echo $url_entorno; ?>/?l=ja_JP" hreflang="ja" class="hover_grow_M_ani">
-            <img src="<?php echo $url_entorno; ?>/img/flag_jp.svg" alt="<?php echo $gettext_switchMsg_JP; ?>." />
-            <span class="visuallyHidden" lang="ja"><?php echo $gettext_languageName_JP; ?></span>
-        </a>
-    </li>
-    <li class="flag_alignedLeft">
-        <a href="<?php echo $url_entorno; ?>/?l=en_US" hreflang="en" class="hover_grow_M_ani">
-            <img src="<?php echo $url_entorno; ?>/img/flag_us.svg" alt="<?php echo $gettext_switchMsg_US; ?>." />
-            <span class="visuallyHidden" lang="en"><?php echo $gettext_languageName_US; ?></span>
-        </a>
-    </li>
-    <li class="flag_alignedLeft"> <!-- gettext_alt -->
-        <a href="<?php echo $url_entorno; ?>/?l=en" hreflang="en" class="hover_grow_M_ani">
-            <img src="<?php echo $url_entorno; ?>/img/flag_us.svg" alt="<?php echo $gettext_switchMsg_US; ?>." />
-            <span class="visuallyHidden" lang="en"><?php echo $gettext_languageName_US; ?></span>
-        </a>
-    </li>
-    <li class="flag_alignedLeft">
-        <a href="<?php echo $url_entorno; ?>/?l=es_UY" hreflang="es" class="hover_grow_M_ani">
-            <img src="<?php echo $url_entorno; ?>/img/flag_uy.svg" alt="<?php echo $gettext_switchMsg_UY; ?>." />
-            <span class="visuallyHidden" lang="es"><?php echo $gettext_languageName_UY; ?></span>
-        </a>
-    </li>
-    <li class="flag_alignedLeft">
-        <a href="<?php echo $url_entorno; ?>/zh" hreflang="fr" class="hover_grow_M_ani">
-            <img src="<?php echo $url_entorno; ?>/img/flag_zh.svg" alt="<?php echo $gettext_switchMsg_ZH; ?>." />
-            <span class="visuallyHidden" lang="zh"><?php echo $gettext_languageName_ZH; ?></span>
-        </a>
-    </li>
-</ul>
+<nav class="nav_language">
+    <ul class="nav_language_ul unstyled displayFlex">
+        <li>
+            <a href="<?php echo $url_entorno; ?>/?l=es_AR" hreflang="es" class="hover_grow_M_ani">
+                <img src="<?php echo $url_entorno; ?>/img/flag_ar.svg" alt="<?php echo $gettext_switchMsg_AR; ?>." />
+                <span class="visuallyHidden" lang="es"><?php echo $gettext_languageName_AR; ?></span>
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo $url_entorno; ?>/?l=pt_BR" hreflang="pt" class="hover_grow_M_ani">
+                <img src="<?php echo $url_entorno; ?>/img/flag_br.svg" alt="<?php echo $gettext_switchMsg_BR; ?>." />
+                <span class="visuallyHidden" lang="pt"><?php echo $gettext_languageName_BR; ?></span>
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo $url_entorno; ?>/?l=de_DE" hreflang="de" class="hover_grow_M_ani">
+                <img src="<?php echo $url_entorno; ?>/img/flag_de.svg" alt="<?php echo $gettext_switchMsg_DE; ?>." />
+                <span class="visuallyHidden" lang="de"><?php echo $gettext_languageName_DE; ?></span>
+            </a>
+        </li>
+        <li class="flag_alignedLeft">
+            <a href="<?php echo $url_entorno; ?>/?l=es_ES" hreflang="es" class="hover_grow_M_ani">
+                <img src="<?php echo $url_entorno; ?>/img/flag_es.svg" alt="<?php echo $gettext_switchMsg_ES; ?>." />
+                <span class="visuallyHidden" lang="es"><?php echo $gettext_languageName_ES; ?></span>
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo $url_entorno; ?>/?l=fr_FR" hreflang="fr" class="hover_grow_M_ani">
+                <img src="<?php echo $url_entorno; ?>/img/flag_fr.svg" alt="<?php echo $gettext_switchMsg_FR; ?>." />
+                <span class="visuallyHidden" lang="fr"><?php echo $gettext_languageName_FR; ?></span>
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo $url_entorno; ?>/?l=en_GB" hreflang="en" class="hover_grow_M_ani">
+                <img src="<?php echo $url_entorno; ?>/img/flag_gb.svg" alt="<?php echo $gettext_switchMsg_GB; ?>." />
+                <span class="visuallyHidden" lang="en"><?php echo $gettext_languageName_GB; ?></span>
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo $url_entorno; ?>/?l=ja_JP" hreflang="ja" class="hover_grow_M_ani">
+                <img src="<?php echo $url_entorno; ?>/img/flag_jp.svg" alt="<?php echo $gettext_switchMsg_JP; ?>." />
+                <span class="visuallyHidden" lang="ja"><?php echo $gettext_languageName_JP; ?></span>
+            </a>
+        </li>
+        <li class="flag_alignedLeft">
+            <a href="<?php echo $url_entorno; ?>/?l=en_US" hreflang="en" class="hover_grow_M_ani">
+                <img src="<?php echo $url_entorno; ?>/img/flag_us.svg" alt="<?php echo $gettext_switchMsg_US; ?>." />
+                <span class="visuallyHidden" lang="en"><?php echo $gettext_languageName_US; ?></span>
+            </a>
+        </li>
+        <li class="flag_alignedLeft"> <!-- gettext_alt -->
+            <a href="<?php echo $url_entorno; ?>/?l=en" hreflang="en" class="hover_grow_M_ani">
+                <img src="<?php echo $url_entorno; ?>/img/flag_us.svg" alt="<?php echo $gettext_switchMsg_US; ?>." />
+                <span class="visuallyHidden" lang="en"><?php echo $gettext_languageName_US; ?></span>
+            </a>
+        </li>
+        <li class="flag_alignedLeft">
+            <a href="<?php echo $url_entorno; ?>/?l=es_UY" hreflang="es" class="hover_grow_M_ani">
+                <img src="<?php echo $url_entorno; ?>/img/flag_uy.svg" alt="<?php echo $gettext_switchMsg_UY; ?>." />
+                <span class="visuallyHidden" lang="es"><?php echo $gettext_languageName_UY; ?></span>
+            </a>
+        </li>
+        <li class="flag_alignedLeft">
+            <a href="<?php echo $url_entorno; ?>/zh" hreflang="fr" class="hover_grow_M_ani">
+                <img src="<?php echo $url_entorno; ?>/img/flag_zh.svg" alt="<?php echo $gettext_switchMsg_ZH; ?>." />
+                <span class="visuallyHidden" lang="zh"><?php echo $gettext_languageName_ZH; ?></span>
+            </a>
+        </li>
+    </ul>
+</nav>
