6ab9bc78f47b5b05a4429be818e391de6dc6e946
Gabriel Volonte <tampas@gmail.com>
2021-11-15 19:48:03 -0300

    :pencil2: Ajustes para pop de videos, CSS de botones. Fix en colores.

--- src/common/_footer.pop.inc.php
+++ src/common/_footer.pop.inc.php
@@ -1,17 +1,41 @@
 <?php
+    if($page_index == 1){ // Index
+//        include('pop.inc.php');
+    }
+    
+    if($page_int == 1){ // Internas
+//        include('pop.inc.php');
+    }
+    
     if($nav_pageCurrent == 1){ // Home
-        include('pop.inc.php');
+//        include('pop.inc.php');
+    }
+    
+    if($nav_pageCurrent == 2){ // Inicio
+//        include('pop.inc.php');
     }
 
     if($nav_pageCurrent == 3){ // Contacto
 //         include('pop.inc.php');
     }
     
-    if($nav_pageCurrent == 000){ // SECTIONNAME
+    if($nav_pageCurrent == 4){ // Gracias
+//        include('pop.inc.php');
+    }
+    
+    if($nav_pageCurrent == 5){ // Construcción
+//        include('pop.inc.php');
+    }
+    
+    if($nav_pageCurrent == 0){ // SECTIONNAME
+//         include('pop.inc.php');
+    }
+    
+    if($nav_pageCurrent_int == 000){ // SECTION_INTERNA
 //         include('pop.inc.php');
     }
 
-    if($nav_pageCurrent == 000 || $nav_pageCurrent == 000){ // SECTIONNAME / SECTIONNAME
+    if($nav_pageCurrent == 0 || $nav_pageCurrent_int == 000){ // SECTIONNAME / SECTIONNAME
 //         include('pop.inc.php');
     }
 ?>
