95aee57ae81b580a7475e5b1754a0ed1d433931e
Gabriel Volonte <tampas@gmail.com>
2022-04-04 14:22:53 -0300

    :zap: Button, play video. iframe, lazy loading. Blockquote

--- src/common/_iframe.map.inc.php
+++ src/common/_iframe.map.inc.php
@@ -1,5 +1,5 @@
 <div class="iframe_map_mask">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.9143712104897!2d-58.45375948486464!3d-34.581033163805046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5e89de5b86b%3A0xbfbedb68f4d25fec!2sAv.+Federico+Lacroze+3547%2C+C1427EDA+CABA!5e0!3m2!1ses!2sar!4v1542227751856" frameborder="0" allowfullscreen></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.9143712104897!2d-58.45375948486464!3d-34.581033163805046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb5e89de5b86b%3A0xbfbedb68f4d25fec!2sAv.+Federico+Lacroze+3547%2C+C1427EDA+CABA!5e0!3m2!1ses!2sar!4v1542227751856" loading="lazy" frameborder="0" allowfullscreen></iframe>
     
-    <iframe src="<?php echo $site_address_ADDRESSA_map_iframe_src; ?>" frameborder="0" allowfullscreen></iframe>
+    <iframe src="<?php echo $site_address_ADDRESSA_map_iframe_src; ?>" loading="lazy" frameborder="0" allowfullscreen></iframe>
 </div>
