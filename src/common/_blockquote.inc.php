c4095d30e771ea0b537b78aa03165d227606d3c1
Gabriel Volonte <tampas@gmail.com>
2022-04-05 16:25:20 -0300

    :zap: :memo: Blockquote, q, cite.

--- src/common/_blockquote.inc.php
+++ src/common/_blockquote.inc.php
@@ -1,4 +1,8 @@
+<?php /* // REF [32] */ ?>
+
 <blockquote cite="httpswwwsitiowebcom">
     <p>A URL that designates a source document or message for the information quoted. This attribute is intended to point to information explaining the context or the reference for the quote.</p>
     <p>This attribute is intended to point to information explaining the context or the reference for the quote.</p>
 </blockquote>
+
+<p>When Dave asks HAL to open the pod bay door, HAL answers: <q cite="httpswwwimdbcomtitlett0062622quotesqt0396921">I'm sorry, Dave. I'm afraid I can't do that.</q></p>
