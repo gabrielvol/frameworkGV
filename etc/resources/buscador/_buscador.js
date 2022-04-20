0e28bd455de542b89ab1d4605e7036dd9f53b2d4
Gabriel Volonte <tampas@gmail.com>
2022-04-20 13:57:04 -0300

    :zap: Ajustes en buscador jQuery. Agrego submit con tecla enter.

--- etc/resources/buscador/_buscador.js
+++ etc/resources/buscador/_buscador.js
@@ -9,5 +9,19 @@ $(document).ready(function() {
                 $(this).show();
             }
         });
+    });    
+
+    $('.buscador_busquedaMain').keypress((e) => {  
+        if (e.which === 13) {
+            $('.buscador_item').hide();
+
+            var sucursaltxt = $('#buscador_input__busquedaMain').val();
+
+            $('.buscador_item').each(function(){
+                if($(this).text().toUpperCase().indexOf(sucursaltxt.toUpperCase()) != -1){
+                    $(this).show();
+                }
+            });
+        }
     });
 });
--- etc/resources/buscador/buscador.php
+++ etc/resources/buscador/buscador.php
@@ -2,3 +2,11 @@
     <input type="search" id="buscador_input__busquedaMain">
     <input type="submit" id="buscador_submit__busquedaMain" value="Buscar">
 </form>
+    
+<div class="buscador_busquedaMain">
+    <div class="buscador_container_busquedaMain">
+        <input type="search" id="buscador_input__busquedaMain">
+        <button type="button" id="buscador_submit__busquedaMain">Buscar</button>
+    </div>
+    <p class="buscador_sugg_busquedaMain">Ingrese el <span>nombre</span> del ítem y luego presione el botón <span>Buscar</span>.</p>
+</div>
