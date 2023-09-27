b88f1a60a19ef03e695550e4f85f628415d4977f
Gabriel Volonte <tampas@gmail.com>
2023-09-27 11:52:03 -0300

    :iphone: Clase .global_MQ, ajustes en slider

---
+++ etc/resources/slider/slider.responsive.inc.php
@@ -0,0 +1,48 @@
+<div class="global_MQ_B600">
+    <ul class="slick unstyled">
+        <li><img src="<?php echo $dir_env; ?>/img/img_01_0600.jpg" alt="" /></li>
+        <li><img src="<?php echo $dir_env; ?>/img/img_02_0600.jpg" alt="" /></li>
+    </ul>
+</div>
+
+<div class="global_MQ_A600">
+    <ul class="slick unstyled">
+        <li><img src="<?php echo $dir_env; ?>/img/img_01_1000.jpg" alt="" /></li>
+        <li><img src="<?php echo $dir_env; ?>/img/img_02_1000.jpg" alt="" /></li>
+    </ul>
+</div>
+
+<div class="global_MQ_A1000">
+    <ul class="slick unstyled slider_main">
+        <li><img src="<?php echo $dir_env; ?>/img/img_01_1300.jpg" alt="" /></li>
+        <li><img src="<?php echo $dir_env; ?>/img/img_02_1300.jpg" alt="" /></li>
+    </ul>
+</div>
+
+<div class="global_MQ_A1300">
+    <ul class="slick unstyled slider_main">
+        <li><img src="<?php echo $dir_env; ?>/img/img_01_1500.jpg" alt="" /></li>
+        <li><img src="<?php echo $dir_env; ?>/img/img_02_1500.jpg" alt="" /></li>
+    </ul>
+</div>
+
+<div class="global_MQ_A1500">
+    <ul class="slick unstyled slider_main">
+        <li><img src="<?php echo $dir_env; ?>/img/img_01_2000.jpg" alt="" /></li>
+        <li><img src="<?php echo $dir_env; ?>/img/img_02_2000.jpg" alt="" /></li>
+    </ul>
+</div>
+
+<div class="global_MQ_A2000">
+    <ul class="slick unstyled slider_main">
+        <li><img src="<?php echo $dir_env; ?>/img/img_01_2500.jpg" alt="" /></li>
+        <li><img src="<?php echo $dir_env; ?>/img/img_02_2500.jpg" alt="" /></li>
+    </ul>
+</div>
+
+<div class="global_MQ_A2500">
+    <ul class="slick unstyled slider_main">
+        <li><img src="<?php echo $dir_env; ?>/img/img_01_3000.jpg" alt="" /></li>
+        <li><img src="<?php echo $dir_env; ?>/img/img_02_3000.jpg" alt="" /></li>
+    </ul>
+</div>
