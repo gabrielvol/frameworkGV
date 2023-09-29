380724736ec24e9354cb13891a37e47d41a684ce
Gabriel Volonte <tampas@gmail.com>
2023-09-28 21:24:21 -0300

    :pencil2: Fix

--- etc/resources/slider/slider.responsive.inc.php
+++ etc/resources/slider/slider.responsive.inc.php
@@ -1,12 +1,12 @@
 <div class="global_MQ_B600">
-    <ul class="slick unstyled">
+    <ul class="slick unstyled slider_main">
         <li><img src="<?php echo $dir_env; ?>/img/img_01_0600.jpg" alt="" /></li>
         <li><img src="<?php echo $dir_env; ?>/img/img_02_0600.jpg" alt="" /></li>
     </ul>
 </div>
 
 <div class="global_MQ_A600">
-    <ul class="slick unstyled">
+    <ul class="slick unstyled slider_main">
         <li><img src="<?php echo $dir_env; ?>/img/img_01_1000.jpg" alt="" /></li>
         <li><img src="<?php echo $dir_env; ?>/img/img_02_1000.jpg" alt="" /></li>
     </ul>
