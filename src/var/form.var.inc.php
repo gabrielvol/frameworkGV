8803d3c69401e42efc69e95b88fab20d0dce4460
Gabriel Volonte <tampas@gmail.com>
2024-03-11 13:32:12 -0300

    :pencil2: Fixes

--- src/var/form.var.inc.php
+++ src/var/form.var.inc.php
@@ -15,13 +15,13 @@
 if(!empty($dir_env) && $dir_env !== '/stage'):
     // test / maqueta
     $form_recipient__formMain           = 'tampas@gmail.com';
-    $form_recipient_CC__formMain        = 'gabrielvol@protonmail.com';
-    $form_recipient_BCC__formMain       = 'ggvv@hotmail.com.ar';
+    $form_recipient_CC__formMain        = 'ggvv@hotmail.com.ar';
+    $form_recipient_BCC__formMain       = 'gabrielvol@protonmail.com';
 else:
     // produ / stage
-    $form_recipient__formMain           = 'gabrielvol@protonmail.com'; // $site_email_info_address;
-    $form_recipient_CC__formMain        = 'tampas@gmail.com'; // $site_email_info_address; // . ', ' . $site_email_EMAILB_address;
-    $form_recipient_BCC__formMain       = 'ggvv@hotmail.com.ar';
+    $form_recipient__formMain           = 'tampas@gmail.com'; // $site_email_contacto_address;
+    $form_recipient_CC__formMain        = 'ggvv@hotmail.com.ar'; // $site_email_contacto_address; // . ', ' . $site_email_EMAILA_address;
+    $form_recipient_BCC__formMain       = 'gabrielvol@protonmail.com';
     
 endif;
 
@@ -29,37 +29,37 @@ $form_status_recipient__formMain        = $form_recipient__formMain;
 $form_status_recipient_mailto__formMain = 'mailto:' . $form_recipient__formMain;
 
 
-/* // Form Contacto // REF [36] --------------------------------------------- */
+/* // Form Contacto // REF [36] --------------------------------------------- 
 if(!empty($dir_env) && $dir_env !== '/stage'):
     // test / maqueta
     $form_recipient__formContacto           = 'tampas@gmail.com';
-    $form_recipient_CC__formContacto        = 'gabrielvol@protonmail.com';
-    $form_recipient_BCC__formContacto       = 'ggvv@hotmail.com.ar';
+    $form_recipient_CC__formContacto        = 'ggvv@hotmail.com.ar';
+    $form_recipient_BCC__formContacto       = 'gabrielvol@protonmail.com';
 else:
     // produ / stage
-    $form_recipient__formContacto           = 'gabrielvol@protonmail.com'; // $site_email_info_address;
-    $form_recipient_CC__formContacto        = 'tampas@gmail.com'; // $site_email_info_address; // . ', ' . $site_email_EMAILB_address;
-    $form_recipient_BCC__formContacto       = 'ggvv@hotmail.com.ar';
+    $form_recipient__formContacto           = 'tampas@gmail.com'; // $site_email_contacto_address;
+    $form_recipient_CC__formContacto        = 'ggvv@hotmail.com.ar'; // $site_email_contacto_address; // . ', ' . $site_email_EMAILA_address;
+    $form_recipient_BCC__formContacto       = 'gabrielvol@protonmail.com';
 endif;
 
 $form_status_recipient__formContacto        = $form_recipient__formContacto;
 $form_status_recipient_mailto__formContacto = 'mailto:' . $form_recipient__formContacto;
+*/
 
-
-/* // Form Footer // REF [36] ----------------------------------------------- */
+/* // Form Footer // REF [36] ----------------------------------------------- 
 if(!empty($dir_env) && $dir_env !== '/stage'):
     // test / maqueta
     $form_recipient__formFooter           = 'tampas@gmail.com';
-    $form_recipient_CC__formFooter        = 'gabrielvol@protonmail.com';
-    $form_recipient_BCC__formFooter       = 'ggvv@hotmail.com.ar';
+    $form_recipient_CC__formFooter        = 'ggvv@hotmail.com.ar';
+    $form_recipient_BCC__formFooter       = 'gabrielvol@protonmail.com';
 else:
     // produ / stage    
-    $form_recipient__formFooter           = 'gabrielvol@protonmail.com'; // $site_email_info_address;
-    $form_recipient_CC__formFooter        = 'tampas@gmail.com'; // $site_email_info_address; // . ', ' . $site_email_EMAILB_address;
-    $form_recipient_BCC__formFooter       = 'ggvv@hotmail.com.ar';
+    $form_recipient__formFooter           = 'tampas@gmail.com'; // $site_email_contacto_address;
+    $form_recipient_CC__formFooter        = 'ggvv@hotmail.com.ar'; // $site_email_contacto_address; // . ', ' . $site_email_EMAILA_address;
+    $form_recipient_BCC__formFooter       = 'gabrielvol@protonmail.com';
 endif;    
 
 $form_status_recipient__formFooter        = $form_recipient__formFooter;
 $form_status_recipient_mailto__formFooter = 'mailto:' . $form_recipient__formFooter;
-
+*/
 ?>
