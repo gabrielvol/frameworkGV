eac2f7e0e06e7e71181ec38cf75711ae9c259764
Gabriel Volonte <tampas@gmail.com>
2024-03-20 12:32:49 -0300

    :bento: :truck: PHPMailer 5.2 / 6.8

---
+++ etc/resources/PHPMailer/phpmailer.5.2.php
@@ -0,0 +1,30 @@
+<?php 
+require 'PHPMailer/PHPMailerAutoload.php';
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+// Set PHPMailer to use the sendmail transport
+$mail->isSendmail();
+//Set who the message is to be sent from
+$mail->setFrom('form@sitiowebcom', 'Form');
+//Set an alternative reply-to address
+$mail->addReplyTo('form@sitiowebcom', 'Form');
+//Set who the message is to be sent to
+$mail->addAddress('gabrielvol@protonmail.com', 'G V');
+//Set the subject line
+$mail->Subject = 'Contacto PHPMailer 5.2';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+//$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+//Replace the plain text body with one created manually
+$mail->Body    = 'Este es el contenido del mensaje <b>en negrita!</b>';    // Contenido del mensaje (acepta HTML)
+$mail->AltBody = 'This is a plain-text message body';
+//Attach an image file
+//$mail->addAttachment('images/phpmailer_mini.png');
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
---
+++ etc/resources/PHPMailer/phpmailer.6.8.php
@@ -0,0 +1,45 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+ 
+$mail = new PHPMailer(true);
+try {
+    $mail->SMTPDebug = 2;  // Sacar esta línea para no mostrar salida debug
+    $mail->SMTPDebug = SMTP::DEBUG_CONNECTION; 
+    $mail->isSMTP();
+    $mail->Host = 'smtp.sitiowebcom';  // Host de conexión SMTP
+    $mail->SMTPAuth = true;
+    $mail->Username = 'form@sitiowebcom';
+    $mail->Password = 'contrasena';
+
+/*___ Activar seguridad TLS _*/
+    $mail->SMTPSecure = 'ssl';
+/*___ Puerto SMTP _*/
+    $mail->Port = 465;
+
+/*___ Descomentar si el servidor SMTP tiene un certificado autofirmado _*/
+    #$mail->SMTPOptions = ['ssl'=> ['allow_self_signed' => true]];
+/*___ Descomentar si se requiere desactivar cifrado
+    (se suele usar en conjunto con la siguiente línea) _*/
+    #$mail->SMTPSecure = false;
+/*___ Descomentar si se requiere desactivar completamente TLS (sin cifrado) _*/
+    #$mail->SMTPAutoTLS = false;
+ 
+    $mail->setFrom('form@sitiowebcom');		// Mail del remitente
+    $mail->addAddress('gabrielvol@protonmail.com');     // Mail del destinatario
+ 
+    $mail->isHTML(true);
+    $mail->Subject = 'Contacto PHPMailer 6.8';  // Asunto del mensaje
+    $mail->Body    = 'Este es el contenido del mensaje <b>en negrita!</b>';    // Contenido del mensaje (acepta HTML)
+    $mail->AltBody = 'Este es el contenido del mensaje en texto plano';    // Contenido del mensaje alternativo (texto plano)
+ 
+    $mail->send();
+    echo 'El mensaje ha sido enviado';
+} catch (Exception $e) {
+    echo 'El mensaje no se ha podido enviar, error: ', $mail->ErrorInfo;
+}
