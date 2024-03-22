070e8194b51521994ae9545518edcfe94257f9bb
Gabriel Volonte <tampas@gmail.com>
2024-03-22 14:44:12 -0300

    :zap: Tels

--- src/common/_footer.inc.php
+++ src/common/_footer.inc.php
@@ -8,7 +8,7 @@
     </div>
     <div class="footer_txt"> <?php /* // REF [20] */ ?>
         <p class="footer_address"><?php echo _("Direcci&oacute;n"); ?>: <a href="<?php echo $site_address_ADDRESSA_map_url; ?>" target="_blank"><?php echo $site_address_ADDRESSA; ?></a></p>
-        <p class="footer_tel"><?php echo _("Tel&eacute;fono"); ?>: <a <?php echo $site_tel_PHONEMAIN_call_href; ?>> <?php echo $site_tel_PHONEMAIN_formatted; ?></a></p>
+        <p class="footer_tel"><?php echo _("Tel&eacute;fono"); ?>: <a <?php echo $site_tel_XXXX_call_href; ?>> <?php echo $site_tel_XXXX_formatted; ?></a></p>
         <p class="footer_mail"><?php echo _("Correo electr&oacute;nico"); ?>: <a <?php echo $site_email_contacto_href; ?>> <?php echo $site_email_contacto_address; ?></a></p>
     </div>  
     
@@ -24,7 +24,7 @@
         <a href="<?php echo $site_address_ADDRESSA_map_url ?>" target="_blank"><?php echo $site_address_ADDRESSA; ?></a>
     </p>
     
-    <p><?php echo _("Tel&eacute;fono"); ?>: <a <?php echo $site_tel_PHONEMAIN_call_href; ?>> <?php echo $site_tel_PHONEMAIN_formatted; ?></a></p>
+    <p><?php echo _("Tel&eacute;fono"); ?>: <a <?php echo $site_tel_XXXX_call_href; ?>> <?php echo $site_tel_XXXX_formatted; ?></a></p>
     
     <p><?php echo _("Correo electr&oacute;nico"); ?>: <a <?php echo $site_email_contacto_href; ?>><?php echo $site_email_contacto_address; ?></a></p>
     
--- src/var/_tel.var.inc.php
+++ src/var/_tel.var.inc.php
@@ -7,6 +7,7 @@
 
 /* // Tel prefixes ---------------------------------------------------------- */
 $tel_code_country               = '54'; // REF [13]
+$tel_code_country_YYYY          = '52'; // REF [13]
 
 $tel_code_area                  = '11';
 $tel_code_area_int              = '911';
@@ -16,92 +17,80 @@ $tel_prefix_call                = $tel_code_country . $tel_code_area;
 $tel_prefix_formatted           = '(11)';  
     
     
-// $tel_alt_code_area              = '2325';
-// $tel_alt_code_area_int          = '02325';
+// $tel_code_area_YYYY              = '2325';
+// $tel_code_area_YYYY_int          = '02325';
     
-// $tel_alt_prefix_whatsapp        = $tel_code_country . $tel_alt_code_area_int;
-// $tel_alt_prefix_call            = $tel_code_country . $tel_alt_code_area;  
-// $tel_alt_prefix_formatted       = '(02325)';       
+// $tel_prefix_YYYY_whatsapp        = $tel_code_country_YYYY . $tel_code_area_YYYY_int;
+// $tel_prefix_YYYY_call            = $tel_code_country_YYYY . $tel_code_area_YYYY;  
+// $tel_prefix_YYYY_formatted       = '(02325)';       
     
     
     
-/* // Telephone number PRINCIPAL -------------------------------------------- */
-$site_tel_PHONEMAIN_a                  = '0000';
-$site_tel_PHONEMAIN_b                  = '0000';
+/* // Telephone number XXXX ------------------------------------------------- */
+$site_tel_XXXX_a                  = '0000';
+$site_tel_XXXX_b                  = 'XXXX';
 
-$site_tel_PHONEMAIN                    = $site_tel_PHONEMAIN_a . $site_tel_PHONEMAIN_b;
-$site_tel_PHONEMAIN_formatted          = $tel_prefix_formatted . ' ' . $site_tel_PHONEMAIN_a . '-' . $site_tel_PHONEMAIN_b;
+$site_tel_XXXX                    = $site_tel_XXXX_a . $site_tel_XXXX_b;
+$site_tel_XXXX_formatted          = $tel_prefix_formatted . ' ' . $site_tel_XXXX_a . '-' . $site_tel_XXXX_b;
 
-$site_tel_PHONEMAIN_whatsapp           = $tel_prefix_whatsapp . $site_tel_PHONEMAIN;
-$site_tel_PHONEMAIN_whatsapp_formatted = '+' . $tel_code_country . ' ' . $tel_code_area_int . ' ' . $site_tel_PHONEMAIN_a . ' ' . $site_tel_PHONEMAIN_b;
-$site_tel_PHONEMAIN_whatsapp_chatOpen  = 'https://wa.me/' . $site_tel_PHONEMAIN_whatsapp . '?text=Hola,%20me%20interesa%20la%20propuesta!';
-$site_tel_PHONEMAIN_whatsapp_chatOpen_api = 'https://api.whatsapp.com/send?phone=' . $site_tel_PHONEMAIN_whatsapp . '?text=Hola,%20me%20interesa%20la%20propuesta!';
-$site_tel_PHONEMAIN_whatsapp_chatOpen_web = 'https://web.whatsapp.com/send?phone=' . $site_tel_PHONEMAIN_whatsapp . '?text=Hola,%20me%20interesa%20la%20propuesta!';
+$site_tel_XXXX_whatsapp           = $tel_prefix_whatsapp . $site_tel_XXXX;
+$site_tel_XXXX_whatsapp_formatted = '+' . $tel_code_country . ' ' . $tel_code_area_int . ' ' . $site_tel_XXXX_a . ' ' . $site_tel_XXXX_b;
+$site_tel_XXXX_whatsapp_chatOpen  = 'https://wa.me/' . $site_tel_XXXX_whatsapp . '?text=Hola,%20me%20interesa%20la%20propuesta!';
+$site_tel_XXXX_whatsapp_chatOpen_api = 'https://api.whatsapp.com/send?phone=' . $site_tel_XXXX_whatsapp . '?text=Hola,%20me%20interesa%20la%20propuesta!';
+$site_tel_XXXX_whatsapp_chatOpen_web = 'https://web.whatsapp.com/send?phone=' . $site_tel_XXXX_whatsapp . '?text=Hola,%20me%20interesa%20la%20propuesta!';
 
-$site_tel_PHONEMAIN_call               = 'tel:+' . $tel_prefix_call . $site_tel_PHONEMAIN;
-$site_tel_PHONEMAIN_call_href          = 'href="tel:+' . $tel_prefix_call . $site_tel_PHONEMAIN . '"';
-$site_tel_PHONEMAIN_call_anchor        = '<a ' . $site_tel_PHONEMAIN_call_href . '>' . $site_tel_PHONEMAIN . '</a>'; 
+$site_tel_XXXX_call               = 'tel:+' . $tel_prefix_call . $site_tel_XXXX;
+$site_tel_XXXX_call_href          = 'href="tel:+' . $tel_prefix_call . $site_tel_XXXX . '"';
+$site_tel_XXXX_call_anchor        = '<a ' . $site_tel_XXXX_call_href . '>' . $site_tel_XXXX . '</a>'; 
     
     
         
 /* // Telephone number 0800 ------------------------------------------------- */
-$site_tel_PHONE0800_a                  = '0800';
-$site_tel_PHONE0800_b                  = '111';
-$site_tel_PHONE0800_c                  = '9999';
+$site_tel_0800_a                  = '0800';
+$site_tel_0800_b                  = '111';
+$site_tel_0800_c                  = '9999';
     
-$site_tel_PHONE0800                    = $site_tel_PHONE0800_a . $site_tel_PHONE0800_b . $site_tel_PHONE0800_c;
-$site_tel_PHONE0800_formatted          = $site_tel_PHONE0800_a . '-' . $site_tel_PHONE0800_b . '-' . $site_tel_PHONE0800_c;
+$site_tel_0800                    = $site_tel_0800_a . $site_tel_0800_b . $site_tel_0800_c;
+$site_tel_0800_formatted          = $site_tel_0800_a . '-' . $site_tel_0800_b . '-' . $site_tel_0800_c;
        
-$site_tel_PHONE0800_call               = 'wtai://wp/mc;' . $site_tel_PHONE0800;
-$site_tel_PHONE0800_call_href          = 'href="' . $site_tel_PHONE0800_call . '"';
-$site_tel_PHONE0800_call_anchor        = '<a ' . $site_tel_PHONE0800_call_href . '>' . $site_tel_PHONE0800_formatted . '</a>'; 
+$site_tel_0800_call               = 'wtai://wp/mc;' . $site_tel_0800;
+$site_tel_0800_call_href          = 'href="' . $site_tel_0800_call . '"';
+$site_tel_0800_call_anchor        = '<a ' . $site_tel_0800_call_href . '>' . $site_tel_0800_formatted . '</a>';
+    
+    
+    
+/* // Telephone number ZZZZ ------------------------------------------------- */
+// $site_tel_ZZZZ_a                  = '0000';
+// $site_tel_ZZZZ_b                  = 'ZZZZ';
+
+// $site_tel_ZZZZ                    = $site_tel_ZZZZ_a . $site_tel_ZZZZ_b;
+// $site_tel_ZZZZ_formatted          = $tel_prefix_formatted . ' ' . $site_tel_ZZZZ_a . '-' . $site_tel_ZZZZ_b;
+
+// $site_tel_ZZZZ_whatsapp           = $tel_prefix_whatsapp . $site_tel_ZZZZ;
+// $site_tel_ZZZZ_whatsapp_formatted = '+' . $tel_code_country . ' ' . $tel_code_area_int . ' ' . $site_tel_ZZZZ_a . ' ' . $site_tel_ZZZZ_b;
+// $site_tel_ZZZZ_whatsapp_chatOpen  = 'https://wa.me/' . $site_tel_ZZZZ_whatsapp . '?text=Hola,%20me%20interesa%20la%20propuesta!';
+// $site_tel_ZZZZ_whatsapp_chatOpen_api = 'https://api.whatsapp.com/send?phone=' . $site_tel_ZZZZ_whatsapp . '?text=Hola,%20me%20interesa%20la%20propuesta!';
+// $site_tel_ZZZZ_whatsapp_chatOpen_web = 'https://web.whatsapp.com/send?phone=' . $site_tel_ZZZZ_whatsapp . '?text=Hola,%20me%20interesa%20la%20propuesta!';
+
+// $site_tel_ZZZZ_call               = 'tel:+' . $tel_prefix_call . $site_tel_ZZZZ;
+// $site_tel_ZZZZ_call_href          = 'href="tel:+' . $tel_prefix_call . $site_tel_ZZZZ . '"';
+// $site_tel_ZZZZ_call_anchor        = '<a ' . $site_tel_ZZZZ_call_href . '>' . $site_tel_ZZZZ . '</a>';  
     
     
         
-/* // Telephone number SECUNDARIO ------------------------------------------- */
-// $site_tel_PHONEB_a                  = '9999';
-// $site_tel_PHONEB_b                  = '9999';
+/* // Telephone number YYYY ------------------------------------------------- */
+// $site_tel_YYYY_a                     = '9999';
+// $site_tel_YYYY_b                     = '9999';
     
-// $site_tel_PHONEB                    = $site_tel_PHONEB_a . $site_tel_PHONEB_b;
-// $site_tel_PHONEB_formatted          = $tel_prefix_formatted . ' ' . $site_tel_PHONEB_a . '-' . $site_tel_PHONEB_b;
+// $site_tel_YYYY                       = $site_tel_YYYY_a . $site_tel_YYYY_b;
+// $site_tel_YYYY_formatted             = $tel_prefix_YYYY_formatted . ' ' . $site_tel_YYYY_a. '-' .$site_tel_YYYY_b;
     
-// $site_tel_PHONEB_whatsapp           = $tel_prefix_whatsapp . $site_tel_PHONEB;
-// $site_tel_PHONEB_whatsapp_formatted = '+' . $tel_code_country . ' ' . $tel_code_area_int . ' ' . $site_tel_PHONEB_a . ' ' . $site_tel_PHONEB_b;
-// $site_tel_PHONEB_whatsapp_chatOpen  = 'https://api.whatsapp.com/send?phone=' . $site_tel_PHONEB_whatsapp;
-// $site_tel_PHONEB_whatsapp_WAme      = 'https://wa.me/' . $site_tel_PHONEB_whatsapp . '?text=Hola,%20me%20interesa%20la%20propuesta!';
+// $site_tel_YYYY_whatsapp              = $tel_prefix_YYYY_whatsapp . $site_tel_YYYY;
+// $site_tel_YYYY_whatsapp_formatted    = '+' . $tel_code_country_YYYY . ' ' . $tel_code_area_YYYY_int . $site_tel_YYYY_a . ' ' . $site_tel_YYYY_b;
+// $site_tel_YYYY_whatsapp_chatOpen     = 'https://api.whatsapp.com/send?phone=' . $site_tel_YYYY_whatsapp;
+// $site_tel_YYYY_whatsapp_WAme         = 'https://wa.me/' . $site_tel_YYYY_whatsapp . '?text=Hola,%20me%20interesa%20la%20propuesta!';
         
-// $site_tel_PHONEB_call               = 'tel:+' . $tel_prefix_call . $site_tel_PHONEB;
-// $site_tel_PHONEB_call_href          = 'href="tel:+' . $tel_prefix_call . $site_tel_PHONEB . '"';
-// $site_tel_PHONEB_call_anchor        = '<a ' . $site_tel_PHONEB_call_href . '>' . $site_tel_PHONEB . '</a>'; 
-    
-    
-        
-/* // Telephone number ALT -------------------------------------------------- */
-// $site_tel_alt_PHONEALT_a                     = '9999';
-// $site_tel_alt_PHONEALT_b                     = '9999';
-    
-// $site_tel_alt_PHONEALT                       = $site_tel_alt_PHONEALT_a . $site_tel_alt_PHONEALT_b;
-// $site_tel_alt_PHONEALT_formatted             = $tel_alt_prefix_formatted . ' ' . $site_tel_alt_PHONEALT_a. '-' .$site_tel_alt_PHONEALT_b;
-    
-// $site_tel_alt_PHONEALT_whatsapp              = $tel_alt_prefix_whatsapp . $site_tel_alt_PHONEALT;
-// $site_tel_alt_PHONEALT_whatsapp_formatted    = '+' . $tel_alt_code_country . ' ' . $tel_alt_code_area_int . $site_tel_alt_PHONEALT_a . ' ' . $site_tel_alt_PHONEALT_b;
-// $site_tel_alt_PHONEALT_whatsapp_chatOpen     = 'https://api.whatsapp.com/send?phone=' . $site_tel_alt_PHONEALT_whatsapp;
-// $site_tel_alt_PHONEALT_whatsapp_WAme         = 'https://wa.me/' . $site_tel_alt_PHONEALT_whatsapp . '?text=Hola,%20me%20interesa%20la%20propuesta!';
-        
-// $site_tel_alt_PHONEALT_call                  = 'tel:+' . $tel_alt_prefix_call . $site_tel_alt_PHONEALT;
-// $site_tel_alt_PHONEALT_call_href             = 'href="tel:+' . $tel_alt_prefix_call . $site_tel_alt_PHONEALT . '"';
-// $site_tel_alt_PHONEALT_call_anchor           = '<a ' . $site_tel_alt_PHONEALT_call_href . '>' . $site_tel_alt_PHONEALT . '</a>'; 
-    
-    
-    
-/* // Fax number  ----------------------------------------------------------- */
-// $site_fax_a              = '2222';
-// $site_fax_b              = '3333';
-    
-// $site_fax                = $site_fax_a . $site_fax_b;
-// $site_fax_formatted      = $tel_prefix_formatted . ' ' . $site_fax_a . '-' . $site_fax_b;
-        
-// $site_fax_call           = 'tel:+' . $tel_prefix_call . $site_tel_fax;
-// $site_fax_call_href      = 'href="tel:+' . $tel_prefix_call . $site_tel_fax . '"';
-// $site_fax_call_anchor    = '<a ' . $site_fax_call_href . '>' . $site_tel_fax . '</a>'; 
+// $site_tel_YYYY_call                  = 'tel:+' . $tel_prefix_YYYY_call . $site_tel_YYYY;
+// $site_tel_YYYY_call_href             = 'href="tel:+' . $tel_prefix_YYYY_call . $site_tel_YYYY . '"';
+// $site_tel_YYYY_call_anchor           = '<a ' . $site_tel_YYYY_call_href . '>' . $site_tel_YYYY . '</a>'; 
 ?>
