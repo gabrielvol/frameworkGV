cc5d8d659f71c66bd93306b7dc3018e83e401cc4
Gabriel Volonte <tampas@gmail.com>
2024-07-19 15:05:46 -0300

    :zap: Detect User Agent

--- etc/resources/detect_user_agent/4-simbel.php
+++ etc/resources/detect_user_agent/4-simbel.php
@@ -1,7 +1,17 @@
 <?php
-
-$iphone     = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
+    
+// REF [47]
 $android    = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
-$palmpre    = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
 $berry      = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
+$ipad       = strpos($_SERVER['HTTP_USER_AGENT'],"iPad");
+$iphone     = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
 $ipod       = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
+$mobile     = strpos($_SERVER['HTTP_USER_AGENT'],"Mobile");
+$palmpre    = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
+$tablet     = strpos($_SERVER["HTTP_USER_AGENT"],"Tablet");
+
+if($topbar_hide_mobile == 1){
+    if ($android || $berry || $ipad || $iphone || $ipod || $mobile || $palmpre || $tablet == true){
+        echo '<div class="pretopbar"></div>';
+    }
+}
