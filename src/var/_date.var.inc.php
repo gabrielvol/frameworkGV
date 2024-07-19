a45c8a5d3ba077308f5366ccecec36b17238901b
Gabriel Volonte <tampas@gmail.com>
2024-07-19 15:01:14 -0300

    :zap: Date variables, agrego meses

--- src/var/_date.var.inc.php
+++ src/var/_date.var.inc.php
@@ -13,4 +13,17 @@ date_default_timezone_set('America/Argentina/Buenos_Aires');
 $date_year_from     = 2024;
 $date_year_actual   = (int)date('Y');
 
+$date_mes_enero = 'enero';
+$date_mes_febrero = 'febrero';
+$date_mes_marzo = 'marzo';
+$date_mes_abril = 'abril';
+$date_mes_mayo = 'mayo';
+$date_mes_junio = 'junio';
+$date_mes_julio = 'julio';
+$date_mes_agosto = 'agosto';
+$date_mes_septiembre = 'septiembre';
+$date_mes_octubre = 'octubre';
+$date_mes_noviembre = 'noviembre';
+$date_mes_diciembre = 'diciembre';
+
 ?>
