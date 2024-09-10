36ff753ec21a73218e08704e50c95d8826cea03e
Gabriel Volonte <tampas@gmail.com>
2024-09-10 14:12:05 -0300

    :zap: iframe para Google Forms

---
+++ src/common/_iframe.form.inc.php
@@ -0,0 +1,3 @@
+<div class="iframe_form_container">
+    <iframe src="https_docs_google_com_forms_viewform_embedded_true" width="640" height="2000" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+</div>
