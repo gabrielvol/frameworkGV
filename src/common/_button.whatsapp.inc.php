84ee6dc8a1a2294cccc2b02192e25599dc9236d3
Gabriel Volonte <tampas@gmail.com>
2025-06-12 18:36:18 -0300

    Renombro button whatsapp

--- src/common/_button.whatsapp.inc.php
+++
@@ -1,8 +0,0 @@
-<?php /*
-<?php include($_SERVER['DOCUMENT_ROOT'] . $dir_env . '/common/button.whatsapp.inc.php'); ?>
-include($_SERVER['DOCUMENT_ROOT'] . $dir_env . '/common/button.whatsapp.inc.php');
-<?php include('button.whatsapp.inc.php'); ?>
-include('button.whatsapp.inc.php');
-*/ ?>
-
-<a href="<?php echo $site_tel_XXXX_whatsapp_chatOpen; ?>" class="button_whatsApp_anchor" role="button" aria-pressed="false"><span class="button_whatsApp_nFormatted"><?php echo $site_tel_XXXX_whatsapp_formatted; ?></span><span class="button_whatsApp_msg visuallyHidden">Abrir un chat.</span></a>
