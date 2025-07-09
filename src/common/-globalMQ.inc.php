db17fe6b510de5a8faacc2c3de25531bb58e7488
Gabriel Volonte <tampas@gmail.com>
2025-07-09 12:37:16 -0300

    :zap: Responsive images snippets. Javascript para definir --vh native variable para la unidad de viewport height

--- src/common/-globalMQ.inc.php
+++ src/common/-globalMQ.inc.php
@@ -21,3 +21,18 @@
 
 <div class="global_MQ global_MQ_A2000">
 </div>
+
+<div class="responsive-image">
+    <img src="<?php echo $dir_env; ?>/img/image_300.png" class="global_MQ global_MQ_B500" alt="" />
+    <img src="<?php echo $dir_env; ?>/img/image_600.png" class="global_MQ global_MQ_A500 global_MQ_A800 global_MQ_A1000 global_MQ_A1300" alt="" />
+    <img src="<?php echo $dir_env; ?>/img/image_800.png" class="global_MQ global_MQ_A600 global_MQ_A1500 global_MQ_A2000" alt="" />
+</div>
+
+<picture>
+    <source media="(min-width: 1300px)" srcset="<?php echo $dir_env; ?>/img/image_1500.png" />
+    <source media="(min-width: 1000px)" srcset="<?php echo $dir_env; ?>/img/image_1300.png" />
+    <source media="(min-width: 800px)"  srcset="<?php echo $dir_env; ?>/img/image_1000.png" />
+    <source media="(min-width: 600px)"  srcset="<?php echo $dir_env; ?>/img/image_0800.png" />
+    <source media="(min-width: 350px)"  srcset="<?php echo $dir_env; ?>/img/image_0600.png" />
+    <img src="<?php echo $dir_env; ?>/img/image_0350.png" alt="" />
+</picture>
