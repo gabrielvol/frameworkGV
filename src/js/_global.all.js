8878a9155334c40a8b8029ee44c01c8de34d81a1
Gabriel Volonte <tampas@gmail.com>
2025-07-09 12:41:59 -0300

    :zap: Javascript para definir --vh native variable para la unidad de viewport height

--- src/js/_global.all.js
+++ src/js/_global.all.js
@@ -5,6 +5,20 @@
 /* // Site variables -------------------------------------------------------- */
 
 $(document).ready(function () {
+    // Get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    
+    // Set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    
+    // Listen to the resize event
+    window.addEventListener('resize', () => {
+    
+        // Execute the same script as before
+        let vh = window.innerHeight * 0.01;        
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+    
 /* // Separator ------------------------------------------------------------- */
 
 });
