c4dec2a55cc76168e1f27278180ae56a0c2b2523
Gabriel Volonte <tampas@gmail.com>
2025-10-27 19:06:28 -0300

    :zap: :wrench: REF [01] Google Fonts, weights map. gulp

--- src/common/_head.inc.php
+++ src/common/_head.inc.php
@@ -39,7 +39,7 @@
     </style>
 <?php endif; ?>
 
-<?php /* Google Fonts // REF [01] */ ?>
+<?php /* // REF [01] Google Fonts */ ?>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
