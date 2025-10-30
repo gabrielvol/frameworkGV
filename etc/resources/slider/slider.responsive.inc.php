1aff75cfa7b0f7a74f59d0adc80ba2c1c8532c1f
Gabriel Volonte <tampas@gmail.com>
2025-10-30 19:56:37 -0300

    :zap: Slider, reponsive <picture>

--- etc/resources/slider/slider.responsive.inc.php
+++ etc/resources/slider/slider.responsive.inc.php
@@ -1,3 +1,18 @@
+<ul class="slick unstyled slider_main">
+    <li>
+        <picture>
+            <source media="(min-width: 2000px)" srcset="<?php echo $dir_env; ?>/img/slider_header_01_2500.jpg" />
+            <source media="(min-width: 1500px)" srcset="<?php echo $dir_env; ?>/img/slider_header_01_2000.jpg" />
+            <source media="(min-width: 1300px)" srcset="<?php echo $dir_env; ?>/img/slider_header_01_1500.jpg" />
+            <source media="(min-width: 1000px)" srcset="<?php echo $dir_env; ?>/img/slider_header_01_1300.jpg" />
+            <source media="(min-width: 800px)" srcset="<?php echo $dir_env; ?>/img/slider_header_01_1000.jpg" />
+            <source media="(min-width: 600px)" srcset="<?php echo $dir_env; ?>/img/slider_header_01_0800.jpg" />
+            <source media="(min-width: 350px)" srcset="<?php echo $dir_env; ?>/img/slider_header_01_0600.jpg" />
+            <img src="<?php echo $dir_env; ?>/img/slider_header_01_0350.jpg" alt="" />
+        </picture>
+    </li>
+</ul>
+
 <div class="global_MQ_B500">
     <ul class="slick unstyled slider_main">
         <li><img src="<?php echo $dir_env; ?>/img/img_01_0600.jpg" alt="" /></li>
