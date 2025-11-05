2ba2e434d3538bf829f3d9f8d71dfe6f599b7a14
Gabriel Volonte <tampas@gmail.com>
2025-11-04 22:27:44 -0300

    :zap: Footer, Tels, fixes

--- src/var/_tel.var.inc.php
+++ src/var/_tel.var.inc.php
@@ -8,6 +8,7 @@
 /* // Tel prefixes ---------------------------------------------------------- */
 $tel_code_country_AR               = '54'; /* // REF [13] */
 $tel_code_country_MX               = '52'; /* // REF [13] */
+$tel_code_country_NANP             = '1'; /* // REF [13] */ /* NANP North American Numbering Plan */
 $tel_code_country_UY               = '598'; /* // REF [13] */
 
 /* // Tel prefixes Argentina - Buenos Aires -- */
@@ -26,19 +27,33 @@ $tel_prefix_whatsApp_AR_3447       = $tel_code_country_AR . $tel_code_area_AR_WP
 $tel_prefix_call_AR_3447           = $tel_code_country_AR . $tel_code_area_AR_3447;    
 $tel_prefix_formatted_AR_3447      = '(+' . $tel_code_country_AR . '9 ' . $tel_code_area_AR_3447 . ')'; 
 
+/* // Tel prefixes México - CDMX -- */
+$tel_code_area_MX_55              = '55';
+
+$tel_prefix_whatsApp_MX_55        = $tel_code_country_MX . $tel_code_area_MX_55;
+$tel_prefix_call_MX_55            = $tel_prefix_whatsApp_MX_55;    
+$tel_prefix_formatted_MX_55       = '(+ ' . $tel_code_country_MX . ' ' . $tel_code_area_MX_55 . ')'; 
+
 /* // Tel prefixes México - Monterrey -- */
 $tel_code_area_MX_82              = '82';
 
 $tel_prefix_whatsApp_MX_82        = $tel_code_country_MX . $tel_code_area_MX_82;
 $tel_prefix_call_MX_82            = $tel_prefix_whatsApp_MX_82;    
-$tel_prefix_formatted_MX_82       = '(+ ' . $tel_code_country_MX . ' ' . $tel_code_area_MX_82 . ')'; 
+$tel_prefix_formatted_MX_82       = '(+ ' . $tel_code_country_MX . ' ' . $tel_code_area_MX_82 . ')';
 
 /* // Tel prefixes Uruguay - Montevideo -- */
 $tel_code_area_UY_94              = '94';
 
 $tel_prefix_whatsApp_UY_94        = $tel_code_country_UY . $tel_code_area_UY_94;
 $tel_prefix_call_UY_94            = $tel_prefix_whatsApp_UY_94;    
-$tel_prefix_formatted_UY_94       = '(+ ' . $tel_code_country_UY . ' ' . $tel_code_area_UY_94 . ')'; 
+$tel_prefix_formatted_UY_94       = '(+ ' . $tel_code_country_UY . ' ' . $tel_code_area_UY_94 . ')';
+
+/* // Tel prefixes USA - Miami -- */
+$tel_code_area_USA_305              = '305';
+
+$tel_prefix_whatsApp_USA_305        = $tel_code_country_NANP . $tel_code_area_USA_305;
+$tel_prefix_call_USA_305            = $tel_prefix_whatsApp_USA_305;    
+$tel_prefix_formatted_USA_305       = '(+ ' . $tel_code_country_NANP . ' ' . $tel_code_area_USA_305 . ')';
 
    
     
@@ -61,7 +76,7 @@ $site_tel_XXXX_call_anchor        = '<a ' . $site_tel_XXXX_call_href . '>' . $si
     
     
         
-/* // Telephone number YYYY ------------------------------------------------- 
+/* // Telephone number YYYY ------------------------------------------------- *
 $site_tel_YYYY_a                     = '9999';
 $site_tel_YYYY_b                     = '9999';
     
@@ -75,11 +90,12 @@ $site_tel_YYYY_whatsApp_WAme         = 'https://wa.me/' . $site_tel_YYYY_whatsAp
         
 $site_tel_YYYY_call                  = 'tel:+' . $tel_prefix_call_AR_11 . $site_tel_YYYY;
 $site_tel_YYYY_call_href             = 'href="tel:+' . $tel_prefix_call_AR_11 . $site_tel_YYYY . '"';
-$site_tel_YYYY_call_anchor           = '<a ' . $site_tel_YYYY_call_href . '>' . $site_tel_YYYY . '</a>'; */
- 
- 
+$site_tel_YYYY_call_anchor           = '<a ' . $site_tel_YYYY_call_href . '>' . $site_tel_YYYY . '</a>';
+*/
 
-/* // Telephone number AR ER 3447 ZZZZ -------------------------------------- 
+
+
+/* // Telephone number AR ER 3447 ZZZZ -------------------------------------- *
 $site_tel_ZZZZ_a                  = '0000';
 $site_tel_ZZZZ_b                  = 'ZZZZ';
     
@@ -93,8 +109,85 @@ $site_tel_ZZZZ_whatsApp_WAme      = 'https://wa.me/' . $site_tel_ZZZZ_whatsApp .
         
 $site_tel_ZZZZ_call               = 'tel:+' . $tel_prefix_call_AR_3447 . $site_tel_ZZZZ;
 $site_tel_ZZZZ_call_href          = 'href="tel:+' . $tel_prefix_call_AR_3447 . $site_tel_ZZZZ . '"';
-$site_tel_ZZZZ_call_anchor        = '<a ' . $site_tel_ZZZZ_call_href . '>' . $site_tel_ZZZZ . '</a>'; */
-  
+$site_tel_ZZZZ_call_anchor        = '<a ' . $site_tel_ZZZZ_call_href . '>' . $site_tel_ZZZZ . '</a>';
+*/
+ 
+
+
+/* // Telephone number MX CDMX 55 2222 -------------------------------------- *
+$site_tel_2222_a                  = '1111';
+$site_tel_2222_b                  = '2222';
+    
+$site_tel_2222                    = $site_tel_2222_a . $site_tel_2222_b;
+$site_tel_2222_formatted          = $tel_prefix_formatted_MX_55 . ' ' . $site_tel_2222_a. '-' .$site_tel_2222_b;
+    
+$site_tel_2222_whatsApp           = $tel_prefix_whatsApp_MX_55 . $site_tel_2222;
+$site_tel_2222_whatsApp_formatted = '+' . $tel_code_country_MX . ' ' . $tel_code_area_MX_55 . ' ' . $site_tel_2222_a . ' ' . $site_tel_2222_b;
+$site_tel_2222_whatsApp_chatOpen  = 'https://api.whatsapp.com/send?phone=' . $site_tel_2222_whatsApp;
+$site_tel_2222_whatsApp_WAme      = 'https://wa.me/' . $site_tel_2222_whatsApp . '?text=Hola,%20quiero%20hacer%20una%20consulta!';
+        
+$site_tel_2222_call               = 'tel:+' . $tel_prefix_call_MX_55 . $site_tel_2222;
+$site_tel_2222_call_href          = 'href="tel:+' . $tel_prefix_call_MX_55 . $site_tel_2222 . '"';
+$site_tel_2222_call_anchor        = '<a ' . $site_tel_2222_call_href . '>' . $site_tel_2222 . '</a>'; 
+*/
+ 
+ 
+
+/* // Telephone number MX MTY 82 3333 --------------------------------------- *
+$site_tel_3333_a                  = '1111';
+$site_tel_3333_b                  = '3333';
+    
+$site_tel_3333                    = $site_tel_3333_a . $site_tel_3333_b;
+$site_tel_3333_formatted          = $tel_prefix_formatted_MX_81 . ' ' . $site_tel_3333_a. '-' .$site_tel_3333_b;
+    
+$site_tel_3333_whatsApp           = $tel_prefix_whatsApp_MX_81 . $site_tel_3333;
+$site_tel_3333_whatsApp_formatted = '+' . $tel_code_country_MX . ' ' . $tel_code_area_MX_81 . ' ' . $site_tel_3333_a . ' ' . $site_tel_3333_b;
+$site_tel_3333_whatsApp_chatOpen  = 'https://api.whatsapp.com/send?phone=' . $site_tel_3333_whatsApp;
+$site_tel_3333_whatsApp_WAme      = 'https://wa.me/' . $site_tel_3333_whatsApp . '?text=Hola,%20quiero%20hacer%20una%20consulta!';
+        
+$site_tel_3333_call               = 'tel:+' . $tel_prefix_call_MX_81 . $site_tel_3333;
+$site_tel_3333_call_href          = 'href="tel:+' . $tel_prefix_call_MX_81 . $site_tel_3333 . '"';
+$site_tel_3333_call_anchor        = '<a ' . $site_tel_3333_call_href . '>' . $site_tel_3333 . '</a>'; 
+*/
+ 
+ 
+
+/* // Telephone number UY MTV 94 4444 --------------------------------------- *
+$site_tel_4444_a                  = '1111';
+$site_tel_4444_b                  = '4444';
+    
+$site_tel_4444                    = $site_tel_4444_a . $site_tel_4444_b;
+$site_tel_4444_formatted          = $tel_prefix_formatted_UY_94 . ' ' . $site_tel_4444_a. '-' .$site_tel_4444_b;
+    
+$site_tel_4444_whatsApp           = $tel_prefix_whatsApp_UY_94 . $site_tel_4444;
+$site_tel_4444_whatsApp_formatted = '+' . $tel_code_country_UY . ' ' . $tel_code_area_UY_94 . ' ' . $site_tel_4444_a . ' ' . $site_tel_4444_b;
+$site_tel_4444_whatsApp_chatOpen  = 'https://api.whatsapp.com/send?phone=' . $site_tel_4444_whatsApp;
+$site_tel_4444_whatsApp_WAme      = 'https://wa.me/' . $site_tel_4444_whatsApp . '?text=Hola,%20quiero%20hacer%20una%20consulta!';
+        
+$site_tel_4444_call               = 'tel:+' . $tel_prefix_call_UY_94 . $site_tel_4444;
+$site_tel_4444_call_href          = 'href="tel:+' . $tel_prefix_call_UY_94 . $site_tel_4444 . '"';
+$site_tel_4444_call_anchor        = '<a ' . $site_tel_4444_call_href . '>' . $site_tel_4444 . '</a>';
+*/
+
+ 
+
+/* // Telephone number USA Miami 305 6666 ----------------------------------- *
+$site_tel_6666_a                  = '1111';
+$site_tel_6666_b                  = '6666';
+    
+$site_tel_6666                    = $site_tel_6666_a . $site_tel_6666_b;
+$site_tel_6666_formatted          = $tel_prefix_formatted_USA_305 . ' ' . $site_tel_6666_a. '-' .$site_tel_6666_b;
+    
+$site_tel_6666_whatsApp           = $tel_prefix_whatsApp_USA_305 . $site_tel_6666;
+$site_tel_6666_whatsApp_formatted = '+' . $tel_code_country_NANP . ' ' . $tel_code_area_USA_94 . ' ' . $site_tel_6666_a . ' ' . $site_tel_6666_b;
+$site_tel_6666_whatsApp_chatOpen  = 'https://api.whatsapp.com/send?phone=' . $site_tel_6666_whatsApp;
+$site_tel_6666_whatsApp_WAme      = 'https://wa.me/' . $site_tel_6666_whatsApp . '?text=Hola,%20quiero%20hacer%20una%20consulta!';
+        
+$site_tel_6666_call               = 'tel:+' . $tel_prefix_call_USA_305 . $site_tel_6666;
+$site_tel_6666_call_href          = 'href="tel:+' . $tel_prefix_call_USA_305 . $site_tel_6666 . '"';
+$site_tel_6666_call_anchor        = '<a ' . $site_tel_6666_call_href . '>' . $site_tel_6666 . '</a>';
+*/
+
 
         
 /* // Telephone number 0800 ------------------------------------------------- */
