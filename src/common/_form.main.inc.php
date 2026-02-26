ba461931c78d0f4cb0f6dffad4a0db46d0914e7c
Gabriel Volonte <tampas@gmail.com>
2026-02-26 13:53:49 -0300

    :zap: Form select, creo listas para validar options

--- src/common/_form.main.inc.php
+++ src/common/_form.main.inc.php
@@ -108,16 +108,24 @@
         <?php else: ?>
             Género
         <?php endif; ?></span>
-        <select name="data_genero__formMainID" id="data_genero__formMainID" class="data_genero__formMainID" form="formMainID" enterkeyhint="next">
-            <?php if(isset($gettext_idioma) && $gettext_idioma == 'en_GB'): ?>
-                <option value="<?php echo $_POST['data_genero__formMainID']; ?>" selected>Choose an option</option>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-            <?php else: ?>
-                <option value="<?php echo $_POST['data_genero__formMainID']; ?>" selected>Seleccione una opci&oacute;n</option>
-                <option value="Masculino">Masculino</option>
-                <option value="Femenino">Femenino</option>
-            <?php endif; ?>
+        
+        <?php $data_genero_list = [
+                'Femenino'   => _('Femenino'),
+                'Masculino'=> _('Masculino'),
+            ];
+
+            $data_genero_selected = $_POST['data_genero__formMainID'] ?? '';
+            $data_genero_selected = array_key_exists($data_genero_selected, $data_genero_list) ? $data_genero_selected : ''; ?>
+        <select name="data_genero__formMainID" id="data_genero__formMainID" class="data_genero__formMainID <?php echo $form_validation_input_class_data_genero__formMainID; ?>" form="formMainID" enterkeyhint="next">
+            <option value="" disabled <?php echo $data_genero_selected === '' ? 'selected' : ''; ?>>
+                <?php echo _('Seleccione una opci&oacute;n'); ?>
+            </option>
+
+            <?php foreach ($data_genero_list as $data_genero_value => $data_genero_option): ?>
+                <option value="<?php echo $data_genero_value; ?>" <?php echo $data_genero_selected === $data_genero_value ? 'selected' : ''; ?>>
+                    <?php echo $data_genero_option; ?>
+                </option>
+        <?php endforeach; ?>
         </select>
     </label>
 
@@ -265,34 +273,46 @@
         <?php else: ?>
             Provincia
         <?php endif; ?></span>
-        <select name="data_provincia__formMainID" id="data_provincia__formMainID" class="data_provincia__formMainID <?php echo $form_validation_input_class_data_provincia__formMainID; ?>" form="formMainID" enterkeyhint="next">
-            <option value="<?php echo $_POST['data_provincia__formMainID']; ?>" selected><?php echo $_POST['data_provincia__formMainID']; ?></option>
+        
+        <?php $data_provincia_list = [
+            'BuenosAires'       => 'Buenos Aires',
+            'Catamarca'         => 'Catamarca',
+            'Chaco'             => 'Chaco',
+            'Chubut'            => 'Chubut',
+            'CABA'              => 'Ciudad Autónoma de Buenos Aires',
+            'Cordoba'           => 'Córdoba',
+            'Corrientes'        => 'Corrientes',
+            'EntreRios'         => 'Entre Ríos',
+            'Formosa'           => 'Formosa',
+            'Jujuy'             => 'Jujuy',
+            'LaPampa'           => 'La Pampa',
+            'LaRioja'           => 'La Rioja',
+            'Mendoza'           => 'Mendoza',
+            'Misiones'          => 'Misiones',
+            'Neuquen'           => 'Neuquén',
+            'RioNegro'          => 'Río Negro',
+            'Salta'             => 'Salta',
+            'SanJuan'           => 'San Juan',
+            'SanLuis'           => 'San Luis',
+            'SantaCruz'         => 'Santa Cruz',
+            'SantaFe'           => 'Santa Fe',
+            'SantiagoDelEstero' => 'Santiago del Estero',
+            'TierraDelFuego'    => 'Tierra del Fuego',
+            'Tucuman'           => 'Tucumán',
+            ];
 
-            <option value="<?php echo $_POST['data_provincia__formMainID']; ?>" selected><?php if($_POST['data_provincia__formMainID']) { echo $_POST['data_provincia__formMainID']; } else { echo _('Seleccione su provincia'); } ?></option>
-            <option value="CABA"><?php echo _('Ciudad Aut&oacute;noma de Buenos Aires'); ?></option>
-            <option value="Pcia. de Buenos Aires"><?php echo _('Pcia. de Buenos Aires'); ?></option>
-            <option value="Catamarca"><?php echo _('Catamarca'); ?></option>
-            <option value="Chaco"><?php echo _('Chaco'); ?></option>
-            <option value="Chubut"><?php echo _('Chubut'); ?></option>
-            <option value="C&oacute;rdoba"><?php echo _('C&oacute;rdoba'); ?></option>
-            <option value="Corrientes"><?php echo _('Corrientes'); ?></option>
-            <option value="Entre R&iacute;os"><?php echo _('Entre R&iacute;os'); ?></option>
-            <option value="Formosa"><?php echo _('Formosa'); ?></option>
-            <option value="Jujuy"><?php echo _('Jujuy'); ?></option>
-            <option value="La Pampa"><?php echo _('La Pampa'); ?></option>
-            <option value="La Rioja"><?php echo _('La Rioja'); ?></option>
-            <option value="Mendoza"><?php echo _('Mendoza'); ?></option>
-            <option value="Misiones"><?php echo _('Misiones'); ?></option>
-            <option value="Neuqu&eacute;n"><?php echo _('Neuqu&eacute;n'); ?></option>
-            <option value="R&iacute;o Negro"><?php echo _('R&iacute;o Negro'); ?></option>
-            <option value="Salta"><?php echo _('Salta'); ?></option>
-            <option value="San Juan"><?php echo _('San Juan'); ?></option>
-            <option value="San Luis"><?php echo _('San Luis'); ?></option>
-            <option value="Santa Cruz"><?php echo _('Santa Cruz'); ?></option>
-            <option value="Santa Fe"><?php echo _('Santa Fe'); ?></option>
-            <option value="Santiago Del Estero"><?php echo _('Santiago Del Estero'); ?></option>
-            <option value="Tierra del Fuego, Ant&aacute;rtida e Islas del Atl&aacute;ntico Sur"><?php echo _('Tierra del Fuego, Ant&aacute;rtida e Islas del Atl&aacute;ntico Sur'); ?></option>
-            <option value="Tucum&aacute;n"><?php echo _('Tucum&aacute;n'); ?></option>
+            $data_provincia_selected = $_POST['data_provincia__formMainID'] ?? '';
+            $data_provincia_selected = array_key_exists($data_provincia_selected, $data_provincia_list) ? $data_provincia_selected : ''; ?>
+        <select name="data_provincia__formMainID" id="data_provincia__formMainID" class="data_provincia__formMainID <?php echo $form_validation_input_class_data_provincia__formMainID; ?>" form="formMainID" enterkeyhint="next">
+            <option value="" disabled <?php echo $data_provincia_selected === '' ? 'selected' : ''; ?>>
+                <?php echo _('Seleccione su provincia'); ?>
+            </option>
+
+            <?php foreach ($data_provincia_list as $data_provincia_value => $data_provincia_option): ?>
+                <option value="<?php echo $data_provincia_value; ?>" <?php echo $data_provincia_selected === $data_provincia_value ? 'selected' : ''; ?>>
+                    <?php echo $data_provincia_option; ?>
+                </option>
+        <?php endforeach; ?>
         </select>
         <span class="form_validation_span <?php echo $form_validation_span_class_data_provincia__formMainID; ?>"><?php echo $form_validation_span_msg_data_provincia__formMainID; ?></span>
     </label>
@@ -303,13 +323,24 @@
         <?php else: ?>
             ¿Cómo querés que te contactemos?
         <?php endif; ?></span>
-        <select name="data_comoQueres__formMainID" id="data_comoQueres__formMainID" class="data_comoQueres__formMainID <?php echo $form_validation_input_class_data_comoQueres__formMainID; ?>" form="formMainID" enterkeyhint="next">
-            <option value="<?php echo $_POST['data_comoQueres__formMainID']; ?>" selected><?php echo $_POST['data_comoQueres__formMainID']; ?></option>
+        
+        <?php $data_comoQueres_list = [
+                'WhatsApp'=> _('WhatsApp'),
+                'Instagram'   => _('Instagram'),
+            ];
 
-            <option value="<?php echo $_POST['data_comoQueres__formMainID']; ?>" selected><?php if($_POST['data_comoQueres__formMainID']) { echo $_POST['data_comoQueres__formMainID']; } else { echo _('Correo electr&oacute;nico, WhatsAspp o Instagram'); } ?></option>
-            <option value="Correo"><?php echo _('Correo electr&oacute;nico'); ?></option>
-            <option value="WhatsApp"><?php echo _('WhatsApp'); ?></option>
-            <option value="Instagram"><?php echo _('Instagram'); ?></option>
+            $data_comoQueres_selected = $_POST['data_comoQueres__formMainID'] ?? '';
+            $data_comoQueres_selected = array_key_exists($data_comoQueres_selected, $data_comoQueres_list) ? $data_comoQueres_selected : ''; ?>
+        <select name="data_comoQueres__formMainID" id="data_comoQueres__formMainID" class="data_comoQueres__formMainID <?php echo $form_validation_input_class_data_comoQueres__formMainID; ?>" form="formMainID" enterkeyhint="next">
+            <option value="" disabled <?php echo $data_comoQueres_selected === '' ? 'selected' : ''; ?>>
+                <?php echo _('WhatsAspp o Instagram'); ?>
+            </option>
+
+            <?php foreach ($data_comoQueres_list as $data_comoQueres_value => $data_comoQueres_option): ?>
+                <option value="<?php echo $data_comoQueres_value; ?>" <?php echo $data_comoQueres_selected === $data_comoQueres_value ? 'selected' : ''; ?>>
+                    <?php echo $data_comoQueres_option; ?>
+                </option>
+        <?php endforeach; ?>
         </select>
         <span class="form_validation_span <?php echo $form_validation_span_class_data_comoQueres__formMainID; ?>"><?php echo $form_validation_span_msg_data_comoQueres__formMainID; ?></span>
     </label>
@@ -320,13 +351,25 @@
         <?php else: ?>
             GENERICSELECT
         <?php endif; ?></span>
-        <select name="data_GENERICSELECT__formMainID" id="data_GENERICSELECT__formMainID" class="data_GENERICSELECT__formMainID <?php echo $form_validation_input_class_data_GENERICSELECT__formMainID; ?>" form="formMainID" enterkeyhint="next" required>
-            <option value="<?php echo $_POST['data_GENERICSELECT__formMainID']; ?>" selected><?php echo $_POST['data_GENERICSELECT__formMainID']; ?></option>
+        
+        <?php $data_GENERICSELECT_list = [
+                'AAAAAAAAAAAAA'   => _('AAAAAAAAAAAAA'),
+                'BBBBBBBBBBBBB'   => _('BBBBBBBBBBBBB'),
+                'CCCCCCCCCCCCC'=> _('CCCCCCCCCCCCC'),
+            ];
 
-            <option value="<?php echo $_POST['data_GENERICSELECT__formMainID']; ?>" selected><?php if($_POST['data_GENERICSELECT__formMainID']) { echo $_POST['data_GENERICSELECT__formMainID']; } else { echo 'Selecciona una opci&oacute;n por favor'; } ?></option>
-            <option value="AAAAAAAAAAAAA"><?php echo _('AAAAAAAAAAAAA'); ?></option>
-            <option value="BBBBBBBBBBBBB"><?php echo _('BBBBBBBBBBBBB'); ?></option>
-            <option value="CCCCCCCCCCCCC"><?php echo _('CCCCCCCCCCCCC'); ?></option>
+            $data_GENERICSELECT_selected = $_POST['data_GENERICSELECT__formMainID'] ?? '';
+            $data_GENERICSELECT_selected = array_key_exists($data_GENERICSELECT_selected, $data_GENERICSELECT_list) ? $data_GENERICSELECT_selected : ''; ?>
+        <select name="data_GENERICSELECT__formMainID" id="data_GENERICSELECT__formMainID" class="data_GENERICSELECT__formMainID <?php echo $form_validation_input_class_data_GENERICSELECT__formMainID; ?>" form="formMainID" enterkeyhint="next">
+            <option value="" disabled <?php echo $data_GENERICSELECT_selected === '' ? 'selected' : ''; ?>>
+                <?php echo _('Selecciona una opci&oacute;n por favor'); ?>
+            </option>
+
+            <?php foreach ($data_GENERICSELECT_list as $data_GENERICSELECT_value => $data_GENERICSELECT_option): ?>
+                <option value="<?php echo $data_GENERICSELECT_value; ?>" <?php echo $data_GENERICSELECT_selected === $data_GENERICSELECT_value ? 'selected' : ''; ?>>
+                    <?php echo $data_GENERICSELECT_option; ?>
+                </option>
+        <?php endforeach; ?>
         </select>
         <span class="form_validation_span <?php echo $form_validation_span_class_data_GENERICSELECT__formMainID; ?>"><?php echo $form_validation_span_msg_data_GENERICSELECT__formMainID; ?></span>
     </label>
@@ -574,42 +617,6 @@
         <input type="text" name="data_asunto__formMainID" id="data_asunto__formMainID" class="data_asunto__formMainID <?php echo $form_validation_input_class_data_asunto__formMainID; ?>" value="<?php echo $_POST['data_asunto__formMainID']; ?>" form="formMainID" enterkeyhint="next" placeholder="<?php if(isset($gettext_idioma) && $gettext_idioma == 'en_GB') { echo ''; } else { echo ''; } ?>" <?php echo $form_input_autofocus_data_asunto__formMainID; ?>>
         <span class="form_validation_span <?php echo $form_validation_span_class_data_asunto__formMainID; ?>"><?php echo $form_validation_span_msg_data_asunto__formMainID; ?></span>
     </label> 
-                  
-    <label for="data_area__formMainID" class="<?php echo $form_validation_input_class_data_nombre__formMainID; ?>">
-        <span class="form_label_name"><?php if(isset($gettext_idioma) && $gettext_idioma == 'en_GB'): ?>
-            Subject
-        <?php else: ?>
-            Asunto
-        <?php endif; ?></span>
-        <select name="data_area__formMainID" id="data_area__formMainID" class="data_area__formMainID <?php echo $form_validation_input_class_data_area__formMainID; ?>" form="formMainID" enterkeyhint="next">
-            <option value="<?php echo $_POST['data_area__formMainID']; ?>" selected><?php if($data_area__formMainID == $site_email_CONTACTO_address){
-                if(isset($gettext_idioma) && $gettext_idioma == 'en_GB') { echo 'Subject: Technical'; } else { echo 'Asunto: Consulta técnica'; }
-            } elseif($data_area__formMainID == $site_email_EMAILB_address) {
-                if(isset($gettext_idioma) && $gettext_idioma == 'en_GB') { echo 'Subject: Investment'; } else { echo 'Asunto: Inversión'; }
-            } else {
-                if(isset($gettext_idioma) && $gettext_idioma == 'en_GB') { echo 'Please select a subject'; } else { echo 'Por favor seleccione un asunto'; }
-            } ?></option>
-            <option value="<?php echo $site_email_CONTACTO_address; ?>" <?php if($data_area__formMainID == $site_email_CONTACTO_address){ echo 'class="displayNone"'; } ?>><?php if(isset($gettext_idioma) && $gettext_idioma == 'en_GB') { echo 'Subject: Technical'; } else { echo 'Asunto: Consulta técnica'; } ?> - <?php echo $site_email_CONTACTO_address ?></option>
-            <option value="<?php echo $site_email_EMAILB_address; ?>" <?php if($data_area__formMainID == $site_email_EMAILB_address){ echo 'class="displayNone"'; } ?>><?php if(isset($gettext_idioma) && $gettext_idioma == 'en_GB') { echo 'Subject: Investment'; } else { echo 'Asunto: Inversión'; } ?> - <?php echo $site_email_EMAILB_address ?></option>
-        </select>
-        <span class="form_validation_span <?php echo $form_validation_span_class_data_area__formMainID; ?>"><?php echo $form_validation_span_msg_data_area__formMainID; ?></span>  
-    </label>
-
-    <label for="data_area__formMainID" class="<?php echo $form_validation_input_class_data_area__formMainID; ?>">
-        <span class="form_label_name"><?php if(isset($gettext_idioma) && $gettext_idioma == 'en_GB'): ?>
-            Department
-        <?php else: ?>
-            &Aacute;rea a contactar
-        <?php endif; ?></span>
-        <select name="data_area__formMainID" id="data_area__formMainID" class="data_area__formMainID <?php echo $form_validation_input_class_data_area__formMainID; ?>" form="formMainID" enterkeyhint="next">
-            <option value="<?php echo $_POST['data_area__formMainID']; ?>" selected><?php if($_POST['data_area__formMainID']) { echo $_POST['data_area__formMainID']; } else {
-                if(isset($gettext_idioma) && $gettext_idioma == 'en_GB') { echo 'Please select a department'; } else { echo 'Por favor seleccione un área a contactar'; }
-            } ?></option>
-            <option value="<?php echo $site_email_CONTACTO_address; ?>"><?php if(isset($gettext_idioma) && $gettext_idioma == 'en_GB') { echo 'Address: a'; } else { echo 'Address: a'; } ?> - <?php echo $site_email_CONTACTO_address; ?></option>
-            <option value="<?php echo $site_email_EMAILB_address; ?>"><?php if(isset($gettext_idioma) && $gettext_idioma == 'en_GB') { echo 'Address: a'; } else { echo 'Address: a'; } ?> - <?php echo $site_email_EMAILB_address; ?></option>
-        </select>
-        <span class="form_validation_span <?php echo $form_validation_span_class_data_area__formMainID; ?>"><?php echo $form_validation_span_msg_data_area__formMainID; ?></span>  
-    </label>
 
     <?php /* Mensaje con validaci&oacute;n */ ?>
     <label for="data_mensaje__formMainID" class="form_required <?php echo $form_validation_input_class_data_mensaje__formMainID; ?>">
